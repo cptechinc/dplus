b374715aeaf812ba828a30b4096f60e0b0882981
Paul Gomez <pauldro952@outlook.com>
2019-04-15 10:03:56 -0500

    Remove templates\test.php

--- site/templates/test.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	use Map\LogpermTableMap;
-	$q = LogpermQuery::create();
-	$con = Propel\Runtime\Propel::getWriteConnection(LogpermTableMap::DATABASE_NAME);
-	$logperm = $q->findOneBySessionid(session_id());
-	//echo $logperm->loginid;
