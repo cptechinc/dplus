7a74beec5d03f9a65797fe897e5ce365dd106f73
Paul Gomez <pauldro952@outlook.com>
2019-04-15 12:40:53 -0500

    Update templates\redir\user.php

--- site/templates/redir/user.php
+++ site/templates/redir/user.php
@@ -2,7 +2,11 @@
 	// Figure out page request method, then grab needed inputs
 	$requestmethod = $input->requestMethod('POST') ? 'post' : 'get';
 	$action = $input->$requestmethod->text('action');
-	$dplusdb = $modules->get('DplusConnectDatabase')->db_name;
+	$dplusdb = $modules->get('DplusOnlineDatabase')->db_name;
+
+	// Set up filename and sessionID in case this was made through cURL
+	$filename = ($input->$requestmethod->sessionID) ? $input->$requestmethod->text('sessionID') : session_id();
+	$sessionID = ($input->$requestmethod->sessionID) ? $input->$requestmethod->text('sessionID') : session_id();
 
 	switch ($action) {
 		case 'login':
@@ -11,7 +15,17 @@
 				$password = $input->$requestmethod->text('password');
 				$data = array("DBNAME=$dplusdb", 'LOGPERM', "LOGINID=$username", "PSWD=$password");
 				$session->loggingin = true;
-				$session->loc = $config->pages->index.'redir/';
+				$session->loc = $pages->get('/redir/')->url;
 			}
 			break;
+	}
+
+	if (!empty($data)) {
+		write_dplusfile($data, $filename);
+		$http = new WireHttp();
+		$http->get("127.0.0.1/cgi-bin/".$config->cgis['default']."?fname=$filename");
+	}
+
+	if (!empty($session->get('loc')) && !$config->ajax) {
+		header("Location: $session->loc");
 	}
