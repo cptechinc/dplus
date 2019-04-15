3844b24db631bccea25aaf465af27292da0db21a
Paul Gomez <pauldro952@outlook.com>
2019-04-15 12:39:32 -0500

    Login Form, show error and submit url

--- site/templates/login.php
+++ site/templates/login.php
@@ -3,5 +3,6 @@
 		$session->redirect($pages->get('/')->url, $http301 = false);
 	}
 
+	$page->formurl = $page->parent->child('template=redir')->url;
 	$page->body = $config->twig->render('user/login-form.twig', ['page' => $page, 'appconfig' => $appconfig,'login_error' => ($session->loggingin && !$user->isloggedin)]);
 	include __DIR__ . "/blank-page.php";
