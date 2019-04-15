59486df9f159d3f6785ccb32b77d4cbd47800b73
Paul Gomez <pauldro952@outlook.com>
2019-04-15 14:12:29 -0500

    Pagination start

--- site/templates/sales-orders.php
+++ site/templates/sales-orders.php
@@ -1,6 +1,9 @@
 <?php
 	$query = SalesOrderQuery::create();
-	$orders = $query->limit(10)->filterbySalesPerson('RDB')->orderByOehdordrdate('DESC')->find();
+	$query->filterbySalesPerson('RDB');
+	$query->orderByOehdordrdate('DESC');
+	$orders = $query->paginate($input->pageNum, 10);
 
 	$page->body = $config->twig->render('sales-orders/sales-orders-list.twig', ['orders' => $orders, 'orderpage' => $pages->get('pw_template=sales-order-view')->url]);
+	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'pagenbr' => $input->pageNum, 'resultscount'=> $orders->getNbResults()]);
 	include __DIR__ . "/basic-page.php";
