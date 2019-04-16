8ee7b6059ac3fbc829a846ad14489b66ac0daf60
bbullemer <barbara.bullemer@gmail.com>
2019-04-16 13:34:59 -0500

    Search Form
    - created search-form.twig
    - changed design for headings and paginator

--- site/templates/sales-orders.php
+++ site/templates/sales-orders.php
@@ -4,6 +4,7 @@
 	$query->orderByOehdordrdate('DESC');
 	$orders = $query->paginate($input->pageNum, 10);
 
-	$page->body = $config->twig->render('sales-orders/sales-orders-list.twig', ['orders' => $orders, 'orderpage' => $pages->get('pw_template=sales-order-view')->url]);
+	$page->body = $config->twig->render('sales-orders/search-form.twig', []);
+	$page->body .= $config->twig->render('sales-orders/sales-orders-list.twig', ['orders' => $orders, 'orderpage' => $pages->get('pw_template=sales-order-view')->url]);
 	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'pagenbr' => $input->pageNum, 'resultscount'=> $orders->getNbResults()]);
 	include __DIR__ . "/basic-page.php";
