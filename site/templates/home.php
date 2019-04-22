26c91ea5ab77c28fe30a3c4b56aa010cf092b9ed
Paul Gomez <pauldro952@outlook.com>
2019-04-22 10:10:01 -0500

    CRLF -> LF

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,8 +1,3 @@
 <?php
-	if (empty(wire('dplusdata'))) {
-		$page->headline = $page->title = "Cannot Connect to Dplus Database";
-		$page->body  = "Error has been logged";
-	} else {
-		$page->body = $config->twig->render('dplus-menu/menu-list.twig', ['items' => $pages->get('/')->find('template=dplus-menu|warehouse-menu')]);
-		include __DIR__ . "/basic-page.php";
-	}
+	$page->body = $config->twig->render('dplus-menu/menu-search-page.twig', ['items' => $pages->get('/')->children('template=dplus-menu|warehouse-menu')]);
+	include __DIR__ . "/basic-page.php";
