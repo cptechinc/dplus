45a8c609e7e27ae2bf103c70fdd755b62e6524fa
bbullemer <barbara.bullemer@gmail.com>
2019-04-22 13:59:01 -0500

    Update sales-orders.php
    - removed count display

--- site/templates/sales-orders.php
+++ site/templates/sales-orders.php
@@ -75,5 +75,4 @@
 	$page->body = $config->twig->render('sales-orders/search-form.twig', ['page' => $page, 'input' => $input]);
 	$page->body .= $config->twig->render('sales-orders/sales-orders-list.twig', ['orders' => $orders, 'orderpage' => $pages->get('pw_template=sales-order-view')->url]);
 	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'pagenbr' => $input->pageNum, 'resultscount'=> $orders->getNbResults()]);
-	$page->body .= "Count: " . $count . "<br>";
 	include __DIR__ . "/basic-page.php";
