832df37ebb264fd50389dc5eb8ca60540f46194f
Paul Gomez <pauldro952@outlook.com>
2019-04-22 09:33:53 -0500

    Added javascript ncessesary for binr

--- site/templates/whse-binr.php
+++ site/templates/whse-binr.php
@@ -1,5 +1,7 @@
 <?php
 	$whsesession = WhsesessionQuery::create()->findOneBySessionid(session_id());
+	$warehouse = WarehouseQuery::create()->findOneByWhseid($whsesession->whseid);
+	$config->scripts->append(hash_templatefile('scripts/warehouse/binr.js'));
 	$binID = '';
 
 	if ($input->get->scan) {
@@ -16,7 +18,7 @@
 			$session->redirect($url , $http301 = false);
 		} else {
 			if (InvsearchQuery::create()->countDistinctItemid(session_id(), $binID) == 1) {
-				$item = InvsearchQuery::create()->findByItemid(session_id(), $binID);
+				$item = InvsearchQuery::create()->findOneBySessionidBin(session_id(), $binID);
 				$pageurl = $page->fullURL->getUrl();
 				$url = $page->parent('template=warehouse-menu,name=binr')->child('template=redir')->url."?action=search-item-bins&itemID=$item->itemid&page=$pageurl";
 				$session->redirect($url, $http301 = false);
@@ -40,22 +42,42 @@
 			$item = $resultscount == 1 ? InvsearchQuery::create()->findOneByItemid(session_id(), $itemID, $binID) : false;
 		}
 
-
 		if ($resultscount == 1) {
 			if (!empty($session->get('binr'))) {
-				// TODO
-				$page->body = __DIR__."/results-screen.php";
-			} else {
+				$nexturl = new Purl\Url($page->fullURL->getUrl());
 
-				$page->body = $config->twig->render('warehouse/binr/binr-form.twig', ['session' => $session, 'page' => $page, 'whsesession' => $whsesession, 'item' => $item]);
+				if ($input->get->tobin || $input->get->frombin) {
+					$nexturl->query->remove('itemID');
+				}
+
+				$page->body = $config->twig->render('warehouse/binr/binr-result.twig', ['session' => $session, 'page' => $page, 'whsesession' => $whsesession, 'item' => $item, 'url' => $nexturl]);
+				$session->remove('binr');
+			} else {
+				$inventory = InvsearchQuery::create();
+				$currentbins = BininfoQuery::create()->filterByItem(session_id(), $item)->select_bin_qty()->find();
+
+				// 1. Binr form
+				$page->formurl = $page->parent('template=warehouse-menu')->child('template=redir')->url;
+				$page->body = $config->twig->render('warehouse/binr/binr-form.twig', ['session' => $session, 'page' => $page, 'whsesession' => $whsesession, 'item' => $item, 'inventory' => $inventory]);
+
+				// 2. Choose From Bin Modal
+				$page->body .= $config->twig->render('warehouse/binr/from-bins-modal.twig', ['item' => $item, 'bins' => $currentbins]);
+
+				// 3. Choose To Bin Modals
+				$page->body .= $config->twig->render('warehouse/binr/to-bins-modal.twig', ['currentbins' => $currentbins, 'warehouse' => $warehouse, 'session' => $session, 'item' => $item, 'inventory' => $inventory]);
+				$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
 			}
 		} else {
 			$items = InvsearchQuery::create()->findBySessionid(session_id());
-			$page->body = $config->twig->render('warehouse/binr/inventory-results.twig', ['page' => $page, 'resultscount' => $resultscount, 'items' => $items]);
+			$inventory = InvsearchQuery::create();
+			$page->body = $config->twig->render('warehouse/binr/inventory-results.twig', ['page' => $page, 'resultscount' => $resultscount, 'items' => $items, 'inventory' => $inventory]);
 		}
 
 	} else {
 		$page->formurl = $page->parent('template=warehouse-menu')->child('template=redir')->url;
 		$page->body    = $config->twig->render('warehouse/item-form.twig', ['page' => $page]);
 	}
+
+
+
 	include __DIR__ . "/basic-page.php";
