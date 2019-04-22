3c7b9d64d0cf7e84cfd2e7b79aa4f9acf936925e
Paul Gomez <pauldro952@outlook.com>
2019-04-22 09:30:36 -0500

    Main Menu: show warehouse-menu as well

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,9 +3,6 @@
 		$page->headline = $page->title = "Cannot Connect to Dplus Database";
 		$page->body  = "Error has been logged";
 	} else {
-		$page->body = $config->twig->render('dplus-menu/menu-list.twig', ['items' => $pages->get('/')->find('template=dplus-menu')]);
+		$page->body = $config->twig->render('dplus-menu/menu-list.twig', ['items' => $pages->get('/')->find('template=dplus-menu|warehouse-menu')]);
 		include __DIR__ . "/basic-page.php";
 	}
-	
-
-
