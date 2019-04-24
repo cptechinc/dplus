336deccb94fdd19f8d4bff093cd50c96c38af0e4
Paul Gomez <pauldro952@outlook.com>
2019-04-24 10:42:57 -0500

    Added yesorno function to twig util

--- site/templates/twig/util/functions.php
+++ site/templates/twig/util/functions.php
@@ -3,3 +3,8 @@
 		  return date($format, strtotime($date));
     });
     $config->twig->addFunction($convertdate);
+
+	$yesno = new Twig_Function('yesorno', function ($trueorfalse) {
+		return ($trueorfalse === true || strtoupper($trueorfalse) == 'Y') ? 'yes' : 'no';
+    });
+	$config->twig->addFunction($yesno);
