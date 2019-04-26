1ffdc9a70ec265d7a8ed1b1c4afd0bb8a4900266
bbullemer <barbara.bullemer@gmail.com>
2019-04-26 12:21:32 -0500

    Customer Page
    - created customer actions, contacts, sales orders, and shipped orders twig files for customer page
    - included render functions in ci-customer template

--- site/templates/ci-customer.php
+++ site/templates/ci-customer.php
@@ -2,13 +2,30 @@
 	use Map\CustomerTableMap;
 	use Propel\Runtime\ActiveQuery\Criteria;
 
-	$query = CustomerQuery::create();
-
 	if ($input->get->custID) {
 		$custID = $input->get->text('custID');
+
+		$query = CustomerQuery::create();
 		$customer = $query->findOneByCustid($custID);
+
+		$query = ContactQuery::create();
+		$contacts = $query->filterByArcucustid($custID);
+
+		$query = UseractionsQuery::create();
+		$actions = $query->groupByCustomerlink($custID);
+
+		$query = SalesOrderQuery::create();
+		$orders = $query->filterByArcucustid($custID);
+
+		$query = SalesHistoryQuery::create();
+		$shippedorders = $query->filterByArcucustid($custID);
+
 		$page->title = "CI: $customer->name";
 		$page->body  = $config->twig->render('customers/customer-page.twig', ['page' => $page, 'customer' => $customer]);
+		$page->body  .= $config->twig->render('customers/ci-customer/customer-contacts.twig', ['page' => $page, 'customer' => $customer, 'contacts' => $contacts]);
+		$page->body  .= $config->twig->render('customers/ci-customer/customer-actions.twig', ['page' => $page, 'customer' => $customer, 'actions' => $actions]);
+		$page->body  .= $config->twig->render('customers/ci-customer/customer-sales-orders.twig', ['page' => $page, 'customer' => $customer, 'orders' => $orders]);
+		$page->body  .= $config->twig->render('customers/ci-customer/customer-shipped-orders.twig', ['page' => $page, 'customer' => $customer, 'orders' => $shippedorders]);
 	} else {
 		if ($input->get->q) {
 			$q = $input->get->text('q');
