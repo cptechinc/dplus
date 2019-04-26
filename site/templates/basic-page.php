8d4503bd1014d7c931152f78e79b15fb4862e153
Paul Gomez <pauldro952@outlook.com>
2019-04-26 09:30:56 -0500

    Bread crumbs

--- site/templates/basic-page.php
+++ site/templates/basic-page.php
@@ -1,8 +1,23 @@
+<?php
+	$parents = $page->parents();
+?>
 <?php include('./_head.php'); ?>
-	<div class="jumbotron bg-dark page-banner rounded-0">
+	<div class="jumbotron bg-dark page-banner rounded-0 mb-3">
 		<div class="container">
 			<h1 class="display-4 text-light"><?= $page->get('pagetitle|headline|title') ; ?></h1>
 		</div>
+	</div>
+	<div class="container">
+		<nav aria-label="breadcrumb rounded-0">
+			<ol class="breadcrumb">
+				<?php foreach ($parents as $parent) : ?>
+					<li class="breadcrumb-item">
+						<a href="<?= $parent->url; ?>"><?= $parent->title; ?></a>
+					</li>
+				<?php endforeach; ?>
+				<li class="breadcrumb-item active" aria-current="page"><?= $page->title; ?></li>
+			</ol>
+		</nav>
 	</div>
 	<div class='container page pt-3'>
 		<?= $page->body; ?>
