83802713c3c85d506119e52390b6f5033a62c4b9
Paul Gomez <pauldro952@outlook.com>
2019-04-29 14:39:09 -0500

    Added $page->show_title hook

--- site/templates/blank-page.php
+++ site/templates/blank-page.php
@@ -1,6 +1,8 @@
 <?php include('./_head-blank.php'); ?>
 	<div class='container page pt-3'>
-		<h2><?= $page->title; ?></h2>
+		<?php if ($page->show_title) : ?>
+			<h2><?= $page->title; ?></h2>
+		<?php endif; ?>
 		<?= $page->body; ?>
 	</div>
 <?php include('./_foot-blank.php'); ?>
