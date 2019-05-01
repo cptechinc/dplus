736f6d96fb62a54230b60e12a0c1ca611c38034b
Paul Gomez <pauldro952@outlook.com>
2019-05-01 09:06:51 -0500

    Update functions.php
    CRLF -> LF
    Added currency, and attrJS function

--- site/templates/twig/util/functions.php
+++ site/templates/twig/util/functions.php
@@ -1,5 +1,21 @@
 <?php
-    $convertdate = new Twig_Function('convertdate', function ($date, $format = 'm/d/Y') {
-		  return date($format, strtotime($date));
+	$convertdate = new Twig_Function('convertdate', function ($date, $format = 'm/d/Y') {
+		return date($format, strtotime($date));
+	});
+	$config->twig->addFunction($convertdate);
+
+	$yesno = new Twig_Function('yesorno', function ($trueorfalse) {
+		return ($trueorfalse === true || strtoupper($trueorfalse) == 'Y') ? 'yes' : 'no';
     });
-    $config->twig->addFunction($convertdate);
+	$config->twig->addFunction($yesno);
+
+	// an anonymous function
+	$filter = new \Twig\TwigFilter('currency', function ($money) {
+	    return number_format($money, 2, '.', ",");
+	});
+	$config->twig->addFilter($filter);
+
+	$filter = new Twig_Filter('attrJS', function ($string) {
+		return "js-$string";
+	});
+	$config->twig->addFilter($filter);
