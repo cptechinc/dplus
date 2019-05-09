40db025a6731bdb55ee6510c6a6ab0e000647128
Paul Gomez <pauldro952@outlook.com>
2019-05-09 10:47:38 -0500

    Use Dplus FUnction permissions for user

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,3 +1,2 @@
 <?php
-	$page->body = $config->twig->render('dplus-menu/menu-search-page.twig', ['items' => $pages->get('/')->children('template=dplus-menu|warehouse-menu')]);
-	include __DIR__ . "/basic-page.php";
+	include __DIR__ . "/menu.php";
