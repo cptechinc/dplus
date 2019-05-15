985971edc656f073b4f509ad52284b4448149c2d
Paul Gomez <pauldro952@outlook.com>
2019-05-15 10:33:58 -0500

    Tracking and documents

--- site/templates/sales-order-view.php
+++ site/templates/sales-order-view.php
@@ -1,6 +1,7 @@
 <?php
 	if ($input->get->ordn) {
 		$ordn = $input->get->text('ordn');
+		$document_management = $modules->get('DocumentManagement');
 
 		if (SalesOrderQuery::create()->filterByOehdnbr($ordn)->count()) {
 			$order_query = SalesOrderQuery::create();
@@ -12,7 +13,12 @@
 			$page->body =  $config->twig->render('sales-orders/sales-order/sales-order-page.twig', ['page' => $page, 'order' => $order, 'order_items' => $order_items]);
 
 			$shipments = SalesOrderShipmentQuery::create()->findByOrderNumber($ordn);
-			$page->body .= $config->twig->render('sales-orders/sales-order/sales-order-tracking.twig', ['page' => $page, 'order' => $order, 'shipments' => $shipments]);
+			$urlmaker = $modules->get('DplusURLs');
+			$page->body .= $config->twig->render('sales-orders/sales-order/sales-order-tracking.twig', ['page' => $page, 'order' => $order, 'shipments' => $shipments, 'urlmaker' => $urlmaker]);
+
+			$documents = $document_management->get_salesorderdocuments($ordn);
+
+			$page->body .= $config->twig->render('sales-orders/sales-order/documents.twig', ['page' => $page, 'documents' => $documents, 'document_management' => $document_management]);
 
 			$page->body .= $config->twig->render('sales-orders/sales-order/sales-order-actions.twig', ['page' => $page, 'order' => $order, 'shipments' => $shipments]);
 		} elseif (SalesHistoryQuery::create()->filterByOehhnbr($ordn)->count()) {
--- site/templates/twig/util/functions.php
+++ site/templates/twig/util/functions.php
@@ -1,5 +1,22 @@
 <?php
-    $convertdate = new Twig_Function('convertdate', function ($date, $format = 'm/d/Y') {
-		  return date($format, strtotime($date));
-    });
-    $config->twig->addFunction($convertdate);
+	use Twig\TwigFilter;
+
+	$convertdate = new Twig_Function('convertdate', function ($date, $format = 'm/d/Y') {
+		return date($format, strtotime($date));
+	});
+	$config->twig->addFunction($convertdate);
+
+	$yesno = new Twig_Function('yesorno', function ($trueorfalse) {
+		return ($trueorfalse === true || strtoupper($trueorfalse) == 'Y') ? 'yes' : 'no';
+	});
+	$config->twig->addFunction($yesno);
+
+	$filter = new TwigFilter('currency', function ($money) {
+		return number_format($money, 2, '.', ",");
+	});
+	$config->twig->addFilter($filter);
+
+	$filter = new Twig_Filter('attrJS', function ($string) {
+		return "js-$string";
+	});
+	$config->twig->addFilter($filter);
