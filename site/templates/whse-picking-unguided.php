55abd2ee72b5e52022ad1775e488454bdaf91e47
Paul Gomez <pauldro952@outlook.com>
2019-05-20 09:14:19 -0500

    Update whse-picking-unguided.php
    Check if item is lotted for page rendering

--- site/templates/whse-picking-unguided.php
+++ site/templates/whse-picking-unguided.php
@@ -24,7 +24,8 @@
 				$pickitem = PickSalesOrderDetailQuery::create()->findOneBySessionidOrderLinenbr(session_id(), $ordn, $linenbr);
 				$http->get($pages->get('template=redir,redir_file=inventory')->httpUrl."?action=inventory-search&scan=$pickitem->itemid&sessionID=".session_id());
 				$picked_barcodes = WhseitempickQuery::create()->get_order_pickeditems(session_id(), $ordn, $pickitem->itemid);
-
+				$inventory_master = InvsearchQuery::create();
+				$pickingsession->insert_barcode_itemID($pickitem->itemid);
 				$jsconfig = array(
 					'pickitem' => array(
 						'item'          => $pickingsession->get_pickitem_jsconfig(),
@@ -47,40 +48,72 @@
 
 				if ($pickitem->is_item_serialized()) {
 					$page->body .= $config->twig->render('warehouse/picking/unguided/barcode-serialized-form.twig', ['page' => $page, 'whsesession' => $whsesession, 'pickitem' => $pickitem, 'pickingsession' => $pickingsession]);
+					$page->body .= $config->twig->render('warehouse/picking/unguided/picked-barcodes-serialized.twig', ['page' => $page, 'picked_barcodes' => $picked_barcodes, 'pickitem' => $pickitem, 'pickingsession' => $pickingsession]);
+				} elseif ($pickitem->is_item_lotted()) {
+					$page->body .= $config->twig->render('warehouse/picking/unguided/barcode-lotted-form.twig', ['page' => $page, 'whsesession' => $whsesession, 'pickitem' => $pickitem, 'pickingsession' => $pickingsession]);
+					$page->body .= $config->twig->render('warehouse/picking/unguided/picked-barcodes-serialized.twig', ['page' => $page, 'picked_barcodes' => $picked_barcodes, 'pickitem' => $pickitem, 'pickingsession' => $pickingsession]);
 				} else {
 					$page->body .= $config->twig->render('warehouse/picking/unguided/barcode-form.twig', ['page' => $page, 'whsesession' => $whsesession, 'pickitem' => $pickitem, 'config_picking' => $config_picking]);
+					$page->body .= $config->twig->render('warehouse/picking/unguided/picked-barcodes.twig', ['page' => $page, 'picked_barcodes' => $picked_barcodes, 'pickitem' => $pickitem, 'pickingsession' => $pickingsession]);
 				}
 
 				$page->body .= $config->twig->render('warehouse/picking/bins-modal.twig', ['warehouse' => $warehouse]);
-				$page->body .= $config->twig->render('warehouse/picking/unguided/picked-barcodes.twig', ['page' => $page, 'picked_barcodes' => $picked_barcodes, 'pickitem' => $pickitem, 'pickingsession' => $pickingsession]);
+				$inventoryresults = InvsearchQuery::create()->findByItemid(session_id(), $pickitem->itemid);
+
+				$page->body .= $config->twig->render('warehouse/picking/unguided/item-availability-modal.twig', ['inventoryresults' => $inventoryresults, 'pickitem' => $pickitem]);
+				$page->body .= $config->twig->render('warehouse/picking/item-info-modal.twig', ['pickitem' => $pickitem]);
 				$page->body .= $config->twig->render('util/js-variables.twig', ['variables' => $jsconfig]);
 
 			} elseif ($input->get->scan) {
 				$scan = $input->get->text('scan');
 				$page->fullURL->query->remove('binID');
 				$page->fullURL->query->remove('scan');
+				$bincount = 0;
 
 				if (InvsearchQuery::create()->countByLotserial(session_id(), $scan)) {
 					$item = InvsearchQuery::create()->get_lotserial(session_id(), $scan);
-				}
-
-				if (InvsearchQuery::create()->countByItemid(session_id(), $scan)){
+					$bincount = InvsearchQuery::create()->count_itembins_lotserial(session_id(), $item->itemid, $item->lotserial);
+				} elseif (InvsearchQuery::create()->countByItemid(session_id(), $scan)){
 					$item = InvsearchQuery::create()->findOneByItemid(session_id(), $scan);
+					$bincount = InvsearchQuery::create()->count_itembins_itemid(session_id(), $item->itemid);
 				}
 
 				if (PickSalesOrderDetailQuery::create()->countBySessionidOrderItemid(session_id(), $ordn, $item->itemid)) {
 					$linenbr = PickSalesOrderDetailQuery::create()->get_orderlinenbr(session_id(), $ordn, $item->itemid);
 					$page->fullURL->query->set('linenbr', $linenbr);
+
+					// IF THERE'S ONLY ONE BIN AUTO ADD THE SCANNED ITEM
+					// TODO handle with picking session
+					if ($bincount == 1) {
+						$pickitem = PickSalesOrderDetailQuery::create()->findOneBySessionidOrderLinenbr(session_id(), $ordn, $linenbr);
+						$barcode = $item->is_lotted() || $item->is_serialized() ? $item->lotserial : $item->itemid;
+						$pickingsession->add_pickedbarcode($pickitem, $barcode, 1, $item->bin);
+					} else {
+						$session->pickerror = "That item is in multiple bins";
+					}
 					$session->redirect($page->fullURL->getUrl(), $http301 = false);
+				} else {
+					$items_unpicked = PickSalesOrderDetailQuery::create()->get_order_lines_unpicked(session_id(), $ordn);
+					$items_picked   = PickSalesOrderDetailQuery::create()->get_order_lines_picked(session_id(), $ordn);
+					$page->formurl = $pages->get('template=redir,redir_file=inventory')->url;
+
+					$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "Attention!", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "No Items match '$scan'"]);
+					$page->body .= '<div class="form-group"></div>';
+					$page->body .= $config->twig->render('warehouse/item-form.twig', ['page' => $page]);
+					$page->body .= $config->twig->render('warehouse/picking/unguided/select-item-form.twig', ['page' => $page, 'items_unpicked' => $items_unpicked, 'items_picked' => $items_picked, 'pickingsession' => $pickingsession]);
+				}
+			} else {
+				$items_unpicked = PickSalesOrderDetailQuery::create()->get_order_lines_unpicked(session_id(), $ordn);
+				$items_picked   = PickSalesOrderDetailQuery::create()->get_order_lines_picked(session_id(), $ordn);
+				$page->formurl = $pages->get('template=redir,redir_file=inventory')->url;
+
+				if ($whsesession->had_picksucceeded()) {
+					$page->body .= $config->twig->render('util/alert.twig', ['type' => 'success', 'title' => "Success!", 'iconclass' => 'fa fa-floppy-o fa-2x', 'message' => $whsession->status]);
+					$page->body .= '<div class="form-group"></div>';
 				}
 
-
-			} else {
-				$items = PickSalesOrderDetailQuery::create()->findBySessionidOrder(session_id(), $ordn);
-
-				$page->formurl = $pages->get('template=redir,redir_file=inventory')->url;
 				$page->body .= $config->twig->render('warehouse/item-form.twig', ['page' => $page]);
-				$page->body .= $config->twig->render('warehouse/picking/unguided/select-item-form.twig', ['page' => $page, 'items' => $items, 'pickingsession' => $pickingsession]);
+				$page->body .= $config->twig->render('warehouse/picking/unguided/select-item-form.twig', ['page' => $page, 'items_unpicked' => $items_unpicked, 'items_picked' => $items_picked, 'pickingsession' => $pickingsession]);
 			}
 		}
 		// Or check if sales order is finished
