4cef88298dc2777fb44bf19ca83254304a75943b
Paul Gomez <pauldro952@outlook.com>
2019-06-03 15:08:19 -0500

    Update functions.php

--- site/templates/twig/util/functions.php
+++ site/templates/twig/util/functions.php
@@ -1,5 +1,19 @@
 <?php
-    $convertdate = new Twig_Function('convertdate', function ($date, $format = 'm/d/Y') {
-		  return date($format, strtotime($date));
-    });
-    $config->twig->addFunction($convertdate);
+	use \Twig\TwigFilter;
+
+	$convertdate = new Twig_Function('convertdate', function ($date, $format = 'm/d/Y') {
+		$date = date($format, strtotime($date));
+		return $date == '11/30/-0001' ? '' : $date;
+	});
+	$config->twig->addFunction($convertdate);
+
+	$yesno = new Twig_Function('yesorno', function ($trueorfalse) {
+		return ($trueorfalse === true || strtoupper($trueorfalse) == 'Y') ? 'yes' : 'no';
+	});
+	$config->twig->addFunction($yesno);
+
+	// an anonymous function
+	$filter = new TwigFilter('currency', function ($money) {
+		return number_format($money, 2, '.', ",");
+	});
+	$config->twig->addFilter($filter);
