8b88dd6a2867c4eb5f44c4e483e92150d3985fd2
Paul Gomez <pauldro952@outlook.com>
2019-07-03 09:19:52 -0500

    II-pricing

--- site/templates/ii-pricing.php
+++ site/templates/ii-pricing.php
@@ -3,6 +3,7 @@
 
 	use ItemsearchQuery, Itemsearch;
 	use WarehouseQuery, Warehouse;
+	use CustomerQuery, Customer;
 
 	$jsoncode = 'ii-pricing';
 	$module_ii = $modules->get('MiiPages');
@@ -28,20 +29,23 @@
 			$page->body = $config->twig->render('items/ii/pricing/customer/customer-search.twig', ['page' => $page, 'customers' => $customers, 'itemID' => $itemID]);
 			$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $customers->getNbResults()]);
 		} else {
+			$custID = $input->get->text('custID');
+
 			$module_json = $modules->get('JsonDataFiles');
 			$json = $module_json->get_file(session_id(), $page->jsoncode);
 
 			if ($module_json->file_exists(session_id(), $page->jsoncode)) {
 				$session->pricingtry = 0;
-				$page->body =  $config->twig->render('items/ii/requirements/requirements-form.twig', ['page' => $page, 'itemID' => $itemID, 'warehouses' => $warehouses, 'module_ii' => $module_ii, 'view' => $json['reqavl'], 'whse' => $json['whse']]);
-				$page->body .= $config->twig->render('items/ii/requirements/requirements.twig', ['page' => $page, 'view' => $module_ii->get_requirementsoptions()[$json['reqavl']], 'json' => $json, 'module_json' => $module_json]);
+				$customer = CustomerQuery::create()->findOneByCustid($custID);
+				$page->body =  $config->twig->render('items/ii/pricing/customer-item.twig', ['page' => $page, 'customer' => $customer, 'json' => $json]);
+				$page->body .= $config->twig->render('items/ii/pricing/screen.twig', ['page' => $page, 'itemID' => $itemID, 'module_ii' => $module_ii, 'json' => $json]);
 			} else {
 				if ($session->pricingtry > 3) {
 					$page->headline = $page->title = "Pricing File could not be loaded";
 					$page->body = $config->twig->render('util/error-page.twig', ['title' => $page->title, 'msg' => $module_json->get_error()]);
 				} else {
 					$session->pricingtry++;
-					$session->redirect($page->get_itempricingURL($itemID));
+					$session->redirect($page->get_itempricingURL($itemID, $custID));
 				}
 			}
 		}
