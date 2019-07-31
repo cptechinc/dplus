5ec25e7467e5a226333f21198b25dd7172fd543a
bbullemer <barbara.bullemer@gmail.com>
2019-07-31 13:30:34 -0500

    Item Sales History
    - added fields and new hook to MiiPages module
    - added ii-sales-history to ScreenFormatterPages module
    - added ii-sales-history.json file to fields directory
    - added history to JsonDataFiles module
    - added ii-sales-history.json file to default directory
    - added iisaleshist to examples directory
    - created and installed IiSalesHistory module
    - added ii-sales-history template
    - added ii-sales-history case to ii redir
    - created date form for ii-sales-history page
    - created sales history twig file

---
+++ site/templates/ii-sales-history.php
@@ -0,0 +1,41 @@
+<?php
+	include_once('./ii-include.php');
+
+	if ($itemquery->count()) {
+		$page->title = "$itemID Sales History";
+
+		if ($input->get->date) {
+			$date = $input->get->text('date');
+
+			$module_json = $modules->get('JsonDataFiles');
+			$json = $module_json->get_file(session_id(), $page->jsoncode);
+
+			if ($module_json->file_exists(session_id(), $page->jsoncode)) {
+				$session->salesorderstry = 0;
+				$module_formatter = $modules->get('IiSalesHistory');
+				$module_formatter->init_formatter();
+				
+				$page->body .= $config->twig->render('items/ii/ii-links.twig', ['page' => $page, 'itemID' => $itemID]);
+
+				$page->body .= $config->twig->render('items/ii/sales-history/sales-history.twig', ['page' => $page, 'itemID' => $itemID, 'json' => $json, 'module_formatter' => $module_formatter, 'blueprint' => $module_formatter->get_tableblueprint()]);
+
+			} else {
+				if ($session->saleshistorytry > 3) {
+					$page->headline = $page->title = "Sales History File could not be loaded";
+					$page->body = $config->twig->render('util/error-page.twig', ['title' => $page->title, 'msg' => $module_json->get_error()]);
+				} else {
+					$session->saleshistorytry++;
+					$session->redirect($page->get_itemsaleshistoryURL($itemID, $date));
+				}
+			}
+		} else {
+			$page->body = $config->twig->render('items/ii/sales-history/date-form.twig', ['page' => $page, 'itemID' => $itemID]);
+		}
+	}
+
+	if ($page->print) {
+		$page->show_title = true;
+		include __DIR__ . "/blank-page.php";
+	} else {
+		include __DIR__ . "/basic-page.php";
+	}
--- site/templates/redir/ii.php
+++ site/templates/redir/ii.php
@@ -235,6 +235,37 @@
 				$session->loc = $url;
 			}
 			break;
+		case 'ii-sales-history':
+			$data = array("DBNAME=$dplusdb", 'IISALESHIST', "ITEMID=$itemID");
+
+			$date = $input->$requestmethod->text('date');
+
+			if (!empty($date)) {
+				$date_ymd = date('Ymd', strtotime($date));
+				$data[] = "DATE=$date_ymd";
+			}
+			if ($input->$requestmethod->page) {
+				$url = new Purl\Url($input->$requestmethod->text('page'));
+				$url->query->set('date', $date);
+				$session->loc = $url->getUrl();
+			} else {
+				$url = $pages->get('pw_template=ii-sales-history')->httpUrl."?itemID=$itemID&date=$date";
+				$session->loc = $url;
+			}
+
+			// $custID = $input->$requestmethod->text('custID');
+			// $shipID = $input->$requestmethod->text('shipID');
+			// $date = $input->$requestmethod->text('date');
+            // if (!empty($custID)) {$data['CUSTID'] = $custID; } if (!empty($shipID)) {$data['SHIPID'] = $shipID; }
+            // if (!empty($date)) { $data['DATE'] = date('Ymd', strtotime($date)); }
+			//
+			// if ($input->$requestmethod->page) {
+			// 	$session->loc = $input->$requestmethod->text('page');
+			// } else {
+			// 	$url = $pages->get('pw_template=ii-sales-history')->httpUrl."?itemID=$itemID";
+			// 	$session->loc = $url;
+			// }
+			break;
 	}
 
 	if (!empty($data)) {
