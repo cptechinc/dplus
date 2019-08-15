464d5e0c3bda5f1a92256d071396ab09d3d98494
Paul Gomez <pauldro952@outlook.com>
2019-08-15 08:58:30 -0500

    Task view

--- site/templates/user-actions.php
+++ site/templates/user-actions.php
@@ -3,14 +3,19 @@
 
 	$module_useractions = $modules->get('FilterUserActions');
 
-	if ($input->get->id) {
-		$id = $input->get->text('id');
-		$query = UseractionsQuery::create();
-		$action = $query->findOneById($id);
-		$page->body .= $config->twig->render("user-actions/$action->actiontype.twig", ['page' => $page, 'module_useractions' => $module_useractions, $action->actiontype => $action]);
-	} else {
-		$query = $module_useractions->get_actionsquery($input);
-	}
+	if ($input->requestMethod('POST')) {
 
+	} else {
+		if ($input->get->id) {
+			$id = $input->get->text('id');
+			$query = UseractionsQuery::create();
+			$action = $query->findOneById($id);
+			$page->body .= $config->twig->render("user-actions/$action->actiontype.twig", ['page' => $page, 'module_useractions' => $module_useractions, $action->actiontype => $action]);
+		} else {
+			$query = $module_useractions->get_actionsquery($input);
+			$actions = $query->find();
+			$page->body .= $config->twig->render("user-actions/actions-list.twig", ['page' => $page, 'module_useractions' => $module_useractions, 'actions' => $actions]);
+		}
+	}
 
 	include __DIR__ . "/basic-page.php";
