e29bf4a839bcdfcd3b0e7ea3d0365ee5c49ffb31
Paul Gomez <pauldro952@outlook.com>
2019-09-03 14:40:25 -0500

    Sales Order Notes

---
+++ site/templates/sales-order-notes.php
@@ -0,0 +1,34 @@
+<?php
+	$modules->get('DplusoPagesSalesOrder')->init_salesorder_hooks();
+	$module_qnotes_crud = $modules->get('QnotesCrud');
+
+	if ($input->requestMethod('POST')) {
+		$response = $module_qnotes_crud->process_input_salesorder($input);
+		echo var_dump($response);
+	} else {
+		if ($input->get->ordn) {
+			$ordn = $input->get->text('ordn');
+
+			if (SalesOrderQuery::create()->filterByOrdernumber($ordn)->count() || SalesHistoryQuery::create()->filterByOrdernumber($ordn)->count()) {
+				if (SalesOrderQuery::create()->filterByOrdernumber($ordn)->count()) {
+					$order = SalesOrderQuery::create()->findOneByOrdernumber($ordn);
+					$order_items = SalesOrderDetailQuery::create()->findByOrdernumber($ordn);
+				} elseif (SalesHistoryQuery::create()->filterByOrdernumber($ordn)->count()) {
+					$order = SalesHistoryQuery::create()->findOneByOrdernumber($ordn);
+					$order_items = SalesHistoryDetailQuery::create()->filterByOrdernumber($ordn)->find();
+				}
+				$page->title = "Sales Order #$ordn Notes";
+				$page->body = $config->twig->render('sales-orders/sales-order/qnotes-page.twig', ['page' => $page, 'ordn' => $ordn, 'items' => $order_items]);
+				$page->body .= $config->twig->render('sales-orders/sales-order/notes/add-note-modal.twig', ['page' => $page, 'ordn' => $ordn, 'items' => $order_items]);
+				$config->scripts->append(hash_templatefile('scripts/orders/order-notes.js'));
+			} else {
+				$page->headline = "Sales Order #$ordn could not be found";
+				$page->body = $config->twig->render('util/error-page.twig', ['title' => $page->headline, 'msg' => "Check if the Order Number is correct or if it is in Sales History"]);
+			}
+		} else {
+			$page->body = $config->twig->render('sales-orders/sales-order-lookup.twig', ['page' => $page]);
+		}
+	}
+
+
+	include __DIR__ . "/basic-page.php";
