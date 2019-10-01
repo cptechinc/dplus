0915c84cddfc6d34dfeee7b98cecf481b76e60a8
Paul Gomez <pauldro952@outlook.com>
2019-10-01 09:32:37 -0500

    Update bookings.php

--- site/templates/bookings.php
+++ site/templates/bookings.php
@@ -11,19 +11,23 @@
 		}
 
 		$input->get->bookdate = $bookdates;
+
+		if (date('F Y', strtotime($bookdates[0])) == date('F Y', strtotime($bookdates[1]))) {
+			$page->title = date('F Y', strtotime($bookdates[0])) . ' Bookings';
+		} else {
+			$page->title = "Bookings between " . $bookdates[0] . ' and ' . $bookdates[1];
+		}
+	} else {
+		$page->title = date('Y'). " Bookings";
 	}
 
-	if (date('F Y', strtotime($bookdates[0])) == date('F Y', strtotime($bookdates[1]))) {
-		$page->title = date('F Y', strtotime($bookdates[0])) . ' Bookings';
-	} else {
-		$page->title = "Bookings between " . $bookdates[0] . ' and ' . $bookdates[1];
-	}
+
 
 
 	$bookings = $filter_bookings->bookings_user->get_bookings($input)->find();
 	$customers = $filter_bookings->bookings_customer->get_bookings_by_customer($input)->find();
 
-	$page->body .= $config->twig->render("bookings/user/bookings-page.twig", ['bookings' => $bookings, 'interval' => $filter_bookings->bookings_user->interval, 'customers' => $customers]);
+	$page->body .= $config->twig->render("bookings/user/bookings-page.twig", ['page' => $page, 'bookings' => $bookings, 'interval' => $filter_bookings->bookings_user->interval, 'customers' => $customers]);
 	$page->js .= $config->twig->render("bookings/user/js/bookings.js.twig", ['page' => $page, 'bookingsdata' => $filter_bookings->convert_bookings_for_js($bookings), 'interval' => $filter_bookings->bookings_user->interval]);
 
 	$config->styles->append(hash_templatefile('styles/lib/morris.css'));
