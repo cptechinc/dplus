cc5d85f171e2cdfc1f0855664c951478a218ee3f
Paul Gomez <pauldro952@outlook.com>
2019-10-02 14:36:03 -0500

    Update ci-phonebook.php

--- site/templates/ci-phonebook.php
+++ site/templates/ci-phonebook.php
@@ -1,4 +1,29 @@
 <?php
 	$query = CustindexQuery::create();
-	
+
+	if ($input->get->q) {
+		$q = strtoupper($input->get->text('q'));
+
+		$page->title = "Phonebook: Searching for '$q'";
+		// TODO: Add Filtering Function
+	}
+
+	if ($page->has_orderby()) {
+		$orderbycolumn = $page->orderby_column;
+		$sort = $page->orderby_sort;
+		$query->sortBy($orderbycolumn, $sort);
+	}
+
+	if ($user->is_salesrep()) { // TODO ADD filtering functions to the CustIndexQuery to filter to available customer shiptos
+		//$query->filterByCustid($user->get_customers(), Criteria::IN);
+	}
+
+	$contacts = $query->paginate($input->pageNum, 10);
+
+	$page->searchURL = $page->url;
+
+	//TODO Create twig file to show results like the old portal but without last sales date
+	//$page->body .= $config->twig->render('customers/customer-search.twig', ['page' => $page, 'contacts' => $contacts]);
+	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $customers->getNbResults()]);
+
 	include __DIR__ . "/basic-page.php";
