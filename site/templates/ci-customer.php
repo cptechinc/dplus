640eed3c27236c46a39d95d67f692ee51c22a364
bbullemer <barbara.bullemer@gmail.com>
2019-10-02 11:47:50 -0500

    #24 Add Customer Permission Checking
    added if statement to check is user has permission to access customer or shipto

--- site/templates/ci-customer.php
+++ site/templates/ci-customer.php
@@ -1,46 +1,54 @@
 <?php
 	use Map\CustomerTableMap;
 	use Propel\Runtime\ActiveQuery\Criteria;
-	$module_useractions      = $modules->get('FilterUserActions');
+
+	$module_useractions = $modules->get('FilterUserActions');
 	$html = $modules->get('HtmlWriter');
 
 	if ($input->get->custID) {
 		$custID = $input->get->text('custID');
 
-		$modules->get('MciPages')->init_customer_hooks();
-		$load_customer = $modules->get('CiLoadCustomerShipto');
+		if ($user->has_customer($custID)) {
+			$modules->get('MciPages')->init_customer_hooks();
+			$load_customer = $modules->get('CiLoadCustomerShipto');
 
-		$load_customer->set_custID($custID);
+			$load_customer->set_custID($custID);
 
-		// TODO VALIDATION
-		if ($load_customer->customer_exists()) {
-			$customer = $load_customer->get_customer();
-			$actions = $load_customer->get_useractions($input);
-			$contacts = $load_customer->get_contacts();
-			$sales_orders = $load_customer->get_salesorders();
-			$sales_history = $load_customer->get_saleshistory();
-			$page->title = "CI: $customer->name";
-			$toolbar = $config->twig->render('customers/ci/customer/toolbar.twig', ['page' => $page, 'custID' => $customer->id]);
+			// TODO VALIDATION
+			if ($load_customer->customer_exists()) {
+				$customer = $load_customer->get_customer();
+				$actions = $load_customer->get_useractions($input);
+				$contacts = $load_customer->get_contacts();
+				$sales_orders = $load_customer->get_salesorders();
+				$sales_history = $load_customer->get_saleshistory();
+				$page->title = "CI: $customer->name";
+				$toolbar = $config->twig->render('customers/ci/customer/toolbar.twig', ['page' => $page, 'custID' => $customer->id]);
 
-			$header =  $config->twig->render('customers/ci/customer/header.twig', ['page' => $page, 'customer' => $customer]);
+				$header =  $config->twig->render('customers/ci/customer/header.twig', ['page' => $page, 'customer' => $customer]);
 
-			$page->body = "<div class='row'>";
-				$page->body .= $html->div('class=col-sm-2', $toolbar);
-				$page->body .= $html->div('class=col-sm-10', $header);
-			$page->body .= "</div>";
-			$page->body .= $config->twig->render('customers/ci/customer/actions-panel.twig', ['page' => $page, 'module_useractions' => $module_useractions, 'actions' => $actions, 'resultscount'=> $actions->getNbResults()]);
-			$page->body .= $config->twig->render('customers/ci/customer/contacts-panel.twig', ['page' => $page, 'customer' => $customer, 'contacts' => $contacts, 'resultscount'=> $contacts->getNbResults()]);
-			$page->body .= $config->twig->render('customers/ci/customer/sales-orders-panel.twig', ['page' => $page, 'customer' => $customer, 'orders' => $sales_orders, 'resultscount'=> $sales_orders->getNbResults(), 'orderpage' => $pages->get('pw_template=sales-order-view')->url, 'sales_orders_list' => $page->cust_salesordersURL($customer->id)]);
-			$page->body .= $config->twig->render('customers/ci/customer/shipped-orders-panel.twig', ['page' => $page, 'customer' => $customer, 'orders' => $sales_history, 'resultscount'=> $sales_history->getNbResults(), 'orderpage' => $pages->get('pw_template=sales-order-view')->url, 'shipped_orders_list' => $page->cust_saleshistoryURL($customer->id)]);
-			$config->scripts->append(hash_templatefile('scripts/customer/ci-customer.js'));
+				$page->body = "<div class='row'>";
+					$page->body .= $html->div('class=col-sm-2', $toolbar);
+					$page->body .= $html->div('class=col-sm-10', $header);
+				$page->body .= "</div>";
+				$page->body .= $config->twig->render('customers/ci/customer/actions-panel.twig', ['page' => $page, 'module_useractions' => $module_useractions, 'actions' => $actions, 'resultscount'=> $actions->getNbResults()]);
+				$page->body .= $config->twig->render('customers/ci/customer/contacts-panel.twig', ['page' => $page, 'customer' => $customer, 'contacts' => $contacts, 'resultscount'=> $contacts->getNbResults()]);
+				$page->body .= $config->twig->render('customers/ci/customer/sales-orders-panel.twig', ['page' => $page, 'customer' => $customer, 'orders' => $sales_orders, 'resultscount'=> $sales_orders->getNbResults(), 'orderpage' => $pages->get('pw_template=sales-order-view')->url, 'sales_orders_list' => $page->cust_salesordersURL($customer->id)]);
+				$page->body .= $config->twig->render('customers/ci/customer/shipped-orders-panel.twig', ['page' => $page, 'customer' => $customer, 'orders' => $sales_history, 'resultscount'=> $sales_history->getNbResults(), 'orderpage' => $pages->get('pw_template=sales-order-view')->url, 'shipped_orders_list' => $page->cust_saleshistoryURL($customer->id)]);
+				$config->scripts->append(hash_templatefile('scripts/customer/ci-customer.js'));
+			} else {
+				$page->searchURL = $page->url;
+				$page->title = "Customer $custID Not Found";
+				$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => $page->title, 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Check the Customer ID is correct"]);
+				$page->body .= $html->div('class=mb-3');
+				$page->body .= $config->twig->render('customers/search-form.twig', ['page' => $page]);
+			}
 		} else {
 			$page->searchURL = $page->url;
-			$page->title = "Customer $custID Not Found";
-			$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => $page->title, 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Check the Customer ID is correct"]);
+			$page->title = "User $user->name Does Not Have Access to $custID";
+			$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => $page->title, 'iconclass' => 'fa fa-warning fa-2x', 'message' => "User does not have permission to access this customer"]);
 			$page->body .= $html->div('class=mb-3');
 			$page->body .= $config->twig->render('customers/search-form.twig', ['page' => $page]);
 		}
-
 	} else {
 		$query = CustomerQuery::create();
 		$exact_query = CustomerQuery::create();
@@ -64,6 +72,10 @@
 			$sort = $page->orderby_sort;
 			$tablecolumn = Customer::get_aliasproperty($orderbycolumn);
 			$query->sortBy($tablecolumn, $sort);
+		}
+
+		if ($user->is_salesrep()) {
+			$query->filterByCustid($user->get_customers(), Criteria::IN);
 		}
 
 		$customers = $query->paginate($input->pageNum, 10);
--- site/templates/ci-shipto.php
+++ site/templates/ci-shipto.php
@@ -5,30 +5,38 @@
 
 		if ($input->get->shiptoID) {
 			$shiptoID = $input->get->text('shiptoID');
-			$module_shipto = $modules->get('CiLoadCustomerShipto');
-			$module_shipto->set_custID($custID);
-			$module_shipto->set_shiptoID($shiptoID);
-			$query_shipto = CustomerShiptoQuery::create()->filterByCustid($custID)->filterByShiptoid($shiptoID);
 
-			if ($module_shipto->shipto_exists()) {
-				$shipto = $module_shipto->get_shipto();
-				$page->title = "CI: $customer->name Ship-to: $shipto->id";
-				$function_pages = $pages->find('pw_template=ci-contacts');
-				$toolbar = $config->twig->render('customers/ci/shiptos/toolbar.twig', ['shipto' => $shipto, 'pages' => $function_pages]);
-				$header  = $config->twig->render('customers/ci/shiptos/header.twig', ['page' => $page, 'customer' => $customer, 'shipto' => $shipto, 'module_shipto' => $module_shipto]);
+			if ($user->has_customer($custID, $shiptoID)) {
+				$module_shipto = $modules->get('CiLoadCustomerShipto');
+				$module_shipto->set_custID($custID);
+				$module_shipto->set_shiptoID($shiptoID);
+				$query_shipto = CustomerShiptoQuery::create()->filterByCustid($custID)->filterByShiptoid($shiptoID);
 
-				$page->body .= $config->twig->render('customers/ci/ci-links.twig', ['page' => $page]);
-				$page->body .= "<div class='row'>";
-					$page->body .= $html->div('class=col-sm-2', $toolbar);
-					$page->body .= $html->div('class=col-sm-10', $header);
-				$page->body .= "</div>";
+				if ($module_shipto->shipto_exists()) {
+					$shipto = $module_shipto->get_shipto();
+					$page->title = "CI: $customer->name Ship-to: $shipto->id";
+					$function_pages = $pages->find('pw_template=ci-contacts');
+					$toolbar = $config->twig->render('customers/ci/shiptos/toolbar.twig', ['shipto' => $shipto, 'pages' => $function_pages]);
+					$header  = $config->twig->render('customers/ci/shiptos/header.twig', ['page' => $page, 'customer' => $customer, 'shipto' => $shipto, 'module_shipto' => $module_shipto]);
 
-				$orders = $module_shipto->get_salesorders();
-				$orders_history = $module_shipto->get_saleshistory();
-				$page->body .= $config->twig->render('customers/ci/customer/sales-orders-panel.twig', ['page' => $page, 'orders' => $orders, 'resultscount'=> $orders->getNbResults(), 'orderpage' => $pages->get('pw_template=sales-order-view')->url, 'sales_orders_list' => $module_shipto->get_salesordersURL()]);
-				$page->body .= $config->twig->render('customers/ci/customer/shipped-orders-panel.twig', ['page' => $page, 'orders' => $orders_history, 'resultscount'=> $orders_history->getNbResults(), 'orderpage' => $pages->get('pw_template=sales-order-view')->url, 'shipped_orders_list' => $module_shipto->get_saleshistoryURL()]);
+					$page->body .= $config->twig->render('customers/ci/ci-links.twig', ['page' => $page]);
+					$page->body .= "<div class='row'>";
+						$page->body .= $html->div('class=col-sm-2', $toolbar);
+						$page->body .= $html->div('class=col-sm-10', $header);
+					$page->body .= "</div>";
+
+					$orders = $module_shipto->get_salesorders();
+					$orders_history = $module_shipto->get_saleshistory();
+					$page->body .= $config->twig->render('customers/ci/customer/sales-orders-panel.twig', ['page' => $page, 'orders' => $orders, 'resultscount'=> $orders->getNbResults(), 'orderpage' => $pages->get('pw_template=sales-order-view')->url, 'sales_orders_list' => $module_shipto->get_salesordersURL()]);
+					$page->body .= $config->twig->render('customers/ci/customer/shipped-orders-panel.twig', ['page' => $page, 'orders' => $orders_history, 'resultscount'=> $orders_history->getNbResults(), 'orderpage' => $pages->get('pw_template=sales-order-view')->url, 'shipped_orders_list' => $module_shipto->get_saleshistoryURL()]);
+				} else {
+					$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "$custID Ship-to $shiptoID does not exist", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Check if shiptoID is correct"]);
+				}
 			} else {
-				$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "$custID Ship-to $shiptoID does not exist", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Check if shiptoID is correct"]);
+				$page->title = "User $user->name Does Not Have Access to $custID Shipto $shiptoID";
+				$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => $page->title, 'iconclass' => 'fa fa-warning fa-2x', 'message' => "User does not have permission to access this shipto"]);
+				$shiptos = CustomerShiptoQuery::create()->filterByCustid($custID)->find();
+				$page->body .= $config->twig->render('customers/ci/shiptos/shipto-list.twig', ['page' => $page, 'customer' => $customer, 'shiptos' => $shiptos]);
 			}
 		} else {
 			$page->title = "Select a $customer->name Ship-to";
