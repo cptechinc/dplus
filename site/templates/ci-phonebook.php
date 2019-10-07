48267c3bbf9e4d1758f0137d5051e569f471ed08
bbullemer <barbara.bullemer@gmail.com>
2019-10-07 12:16:27 -0500

    Phonebook
    - added phonebook for all customers

--- site/templates/ci-phonebook.php
+++ site/templates/ci-phonebook.php
@@ -1,31 +1,31 @@
-<?php
-	include_once('./ci-include.php');
+<?php // THIS TEMPLATE IS THE PHONEBOOK FOR ALL CUSTOMERS
+	$query = CustindexQuery::create();
 
-	if ($customerquery->count()) {
-		$query = CustindexQuery::create();
-		$query->filterByCustid($custID);
+	if ($input->get->q) {
+		$q = strtoupper($input->get->text('q'));
+		
+		$page->show_breadcrumbs = false;
+		$page->body .= $config->twig->render('customers/ci/bread-crumbs.twig', ['page' => $page, 'customer' => $customer]);
 
-		$page->title = "Phonebook: $customer->name";
-
-		if ($input->get->q) {
-			$q = strtoupper($input->get->text('q'));
-
-			$page->title = "Phonebook: $customer->name Searching for '$q'";
-			$query->filterByMatchExpression($q);
-		}
-
-		if ($page->has_orderby()) {
-			$orderbycolumn = $page->orderby_column;
-			$sort = $page->orderby_sort;
-			$query->sortBy($orderbycolumn, $sort);
-		}
-
-		$contacts = $query->paginate($input->pageNum, 10);
-		$page->searchURL = $page->url;
-		$page->body .= $config->twig->render('customers/ci/ci-links.twig', ['page' => $page, 'custID' => $custID]);
-		$page->body .= $config->twig->render('customers/ci/phonebook/contact-search.twig', ['page' => $page, 'custID' => $custID, 'contacts' => $contacts]);
-		$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $query->count()]);
+		$page->title = "Phonebook: $customer->name Searching for '$q'";
+		$query->filterByMatchExpression($q);
 	}
+
+	if ($user->is_salesrep()) {
+		$query->filterByUserCustperm($user->loginid);
+	}
+
+	if ($page->has_orderby()) {
+		$orderbycolumn = $page->orderby_column;
+		$sort = $page->orderby_sort;
+		$query->sortBy($orderbycolumn, $sort);
+	}
+
+	$customers = $query->paginate($input->pageNum, 10);
+
+	$page->searchURL = $page->url;
+	$page->body .= $config->twig->render('customers/phonebook/phonebook-search.twig', ['page' => $page, 'customers' => $customers]);
+	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $query->count()]);
 
 	if ($page->print) {
 		$page->show_title = true;
