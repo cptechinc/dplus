24bc762950692533af742a3246a229a738b624fa
Paul Gomez <pauldro952@outlook.com>
2019-11-04 15:07:45 -0600

    Purchase Orders Vendor

--- site/templates/purchase-orders-vendor.php
+++ site/templates/purchase-orders-vendor.php
@@ -7,8 +7,24 @@
 	$query->orderByDate_ordered('DESC');
 	$orders = $query->paginate($input->pageNum, 10);
 
+	$vendorID = $input->get->text('vendorID');
+	$load_vendor = $modules->get('ViLoadVendorShipfrom');
+	$load_vendor->set_vendorID($vendorID);
+	$vendor = $load_vendor->get_vendor();
+
+	if ($input->get->shipfromID) {
+		$shipfromID = $input->get->text('shipfromID');
+		$load_vendor->set_shipfromID($shipfromID);
+		$shipfrom = $load_vendor->get_shipfrom();
+	} else {
+		$shipfromID = false;
+	}
+
+	$page->title = $shipfromID ? $shipfrom->name : $vendor->name;
+	$page->title .= " Purchase Orders";
+
 	//$page->body = $config->twig->render('purchase-orders/search-form.twig', ['page' => $page, 'input' => $input]);
 	$page->body .= $html->h3('', $orders->getNbResults() . " Purchase Orders");
-	$page->body .= $config->twig->render('purchase-orders/purchase-orders-list.twig', ['purchaseorders' => $orders, 'orderpage' => '']);
+	$page->body .= $config->twig->render('purchase-orders/vendor/purchase-orders-list.twig', ['purchaseorders' => $orders, 'orderpage' => '']);
 	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'pagenbr' => $input->pageNum, 'resultscount'=> $orders->getNbResults()]);
 	include __DIR__ . "/basic-page.php";
