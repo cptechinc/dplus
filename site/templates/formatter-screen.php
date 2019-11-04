110e29968963f45f15fceafb55294bc4c335056f
Paul Gomez <pauldro952@outlook.com>
2019-11-04 14:24:18 -0600

    Fixed Preview, Save for Screen Formatters

--- site/templates/formatter-screen.php
+++ site/templates/formatter-screen.php
@@ -1,24 +1,33 @@
 <?php
-	$page->pagetitle = $page->title;
+	$requestmethod = $input->requestMethod('POST') ? 'post' : 'get';
 	$module_formatter = $modules->get($page->formatter);
+	$module_formatter->set_userID($user->loginid);
 	$module_formatter->init_formatter();
-	$http = new WireHttp();
 	$html = $modules->get('HtmlWriter');
 
 	if ($input->post) {
+		$action = $input->$requestmethod->text('action');
 
-	}
+		if ($action == 'preview') {
+			$module_json = $modules->get('JsonExampleDataFiles');
+			$json = $module_json->get_file(session_id(), $page->jsoncode);
+			$module_formatter->generate_formatterfrominput($input);
+			$preview = $config->twig->render($page->twig_include, ['json' => $json, 'module_formatter' => $module_formatter, 'blueprint' => $module_formatter->get_tableblueprint()]);
+			$button = $html->button('class=btn btn-outline-secondary|type=button|data-toggle=collapse|data-target=#preview-screen|aria-expanded=true|aria-controls=preview-screen', 'Show / Hide');
+			$header = $html->div('class=row mb-4', $html->div('class=col-sm-6', $html->h3('class=text-secondary', 'Preview:')) . $html->div('class=col-sm-6', $button));
 
-	if ($input->get->text('action') == 'preview') {
-		$module_json = $modules->get('JsonExampleDataFiles');
-		$json = $module_json->get_file(session_id(), $page->jsoncode);
-		//$module_formatter->generate_formatterfrominput($input);
-
-		$preview = $config->twig->render('items/ii/sales-orders/sales-orders.twig', ['json' => $json, 'module_formatter' => $module_formatter, 'blueprint' => $module_formatter->get_tableblueprint()]);
-		$button = $html->button('class=btn btn-outline-secondary|type=button|data-toggle=collapse|data-target=#preview-screen|aria-expanded=true|aria-controls=preview-screen', 'Show / Hide');
-		$header = $html->div('class=row mb-4', $html->div('class=col-sm-6', $html->h3('class=text-secondary', 'Preview:')) . $html->div('class=col-sm-6', $button));
-
-		$page->body .= $html->div('class=border border-secondary p-2 mb-3',  $header . $html->div('class=collapse show|id=preview-screen', $preview));
+			$page->body .= $html->div('class=border border-secondary p-2 mb-3',  $header . $html->div('class=collapse show|id=preview-screen', $preview));
+		} elseif ($action == 'save') {
+			$module_formatter->generate_formatterfrominput($input);
+			$result = $module_formatter->save();
+			echo $con->getLastExecutedQuery();
+			if ($result) {
+				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'success', 'title' => 'Success!', 'iconclass' => 'fa fa-floppy-o fa-2x', 'message' => "$page->title formatter was able to be saved"]);
+			} else {
+				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "$page->title formatter was not able to be saved"]);
+			}
+			$page->body .= $html->div('class=mb-3');
+		}
 	}
 
 	$page->body .= $config->twig->render('screen-formatters/formatter-form.twig', ['page' => $page, 'module_formatter' => $module_formatter]);
