cdee02ecad58cb317031ce3b26e2363f99007499
Paul Gomez <pauldro952@outlook.com>
2019-11-05 13:41:55 -0600

    Create Vi documents

---
+++ site/templates/vi-documents.php
@@ -0,0 +1,54 @@
+<?php
+	include_once('./vi-include.php');
+
+	if ($vendorquery->count()) {
+		$page->show_breadcrumbs = false;
+		$page->body .= $config->twig->render('vendors/vi/bread-crumbs.twig', ['page' => $page, 'vendor' => $vendor]);
+
+		$page->title = "$vendor->name Documents";
+		$document_management = $modules->get('DocumentManagement');
+		$html = $modules->get('HtmlWriter');
+
+		if ($input->get->document && $input->get->folder) {
+			$folder = $input->get->text('folder');
+			$filename = $input->get->text('document');
+			$document_management->move_document($folder, $filename);
+
+			if ($document_management->is_filewebaccessible($filename)) {
+				$session->redirect($config->url_webdocs.$filename);
+			}
+		}
+
+		$page->body .= $config->twig->render('vendors/vi/vi-links.twig', ['page' => $page, 'vendorID' => $vendorID]);
+
+		if ($input->get->folder) {
+			$folder = $input->get->text('folder');
+
+			switch ($folder) {
+				case 'PO':
+					$ponbr = PurchaseOrder::get_paddedponumber($input->get->text('ponbr'));
+					$page->title = "PO #$ponbr Documents";
+
+					if (PurchaseOrderQuery::create()->filterByPonbr($ponbr)->count()) {
+						$documents = $document_management->get_purchaseorderdocuments($ponbr);
+					} else {
+						$documents = array();
+					}
+
+					$href = $pages->get('pw_template=vi-purchase-orders')->url."?vendorID=$vendorID";
+					$page->body .= $html->div('class=mb-3', $html->a("href=$href|class=btn btn-secondary", $html->icon('fa fa-arrow-left') . " Back to Vendor Purchase Orders"));
+					$page->body .= $config->twig->render('vendors/vi/documents/documents-dm.twig', ['page' => $page, 'documents' => $documents, 'document_management' => $document_management, 'vendorID' => $vendorID]);
+					break;
+			}
+		} else {
+			$documents = [];
+			$page->body .= $config->twig->render('vendors/vi/documents/documents-dm.twig', ['page' => $page, 'documents' => $documents, 'document_management' => $document_management, 'vendorID' => $vendorID]);
+		}
+	}
+
+	if ($page->print) {
+		$page->show_title = true;
+		include __DIR__ . "/blank-page.php";
+	} else {
+		include __DIR__ . "/basic-page.php";
+	}
