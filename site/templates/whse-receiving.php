1332edcf712fa5e2a5b338d786b8e090b18ccbc8
Paul Gomez <pauldro952@outlook.com>
2019-11-08 08:37:24 -0600

    provally updates

--- site/templates/whse-receiving.php
+++ site/templates/whse-receiving.php
@@ -24,9 +24,20 @@
 				$page->title = "PO # $ponbr Line #$linenbr breakdown";
 				$page->body .= $config->twig->render('warehouse/inventory/receiving/line/links-header.twig', ['page' => $page]);
 				$page->body .= $config->twig->render('warehouse/inventory/receiving/po-header.twig', ['page' => $page, 'purchaseorder' => $purchaseorder]);
-				$page->body .= $config->twig->render('warehouse/inventory/receiving/line/item-details.twig', ['page' => $page, 'item' => $lineitem]);
+
+				if (file_exists($config->paths->templates."twig/warehouse/inventory/receiving/line/$config->company/item-details")) {
+					$page->body .= $config->twig->render("warehouse/inventory/receiving/line/$config->company/item-details.twig", ['page' => $page, 'item' => $lineitem]);
+				} else {
+					$page->body .= $config->twig->render('warehouse/inventory/receiving/line/item-details.twig', ['page' => $page, 'item' => $lineitem]);
+				}
+
 				$page->body .= $html->div('class=mb-3');
-				$page->body .= $config->twig->render('warehouse/inventory/receiving/line/received-lots.twig', ['page' => $page, 'item' => $lineitem]);
+
+				if (file_exists($config->paths->templates."twig/warehouse/inventory/receiving/line/$config->company/received-lots.twig")) {
+					$page->body .= $config->twig->render("warehouse/inventory/receiving/line/$config->company/received-lots.twig", ['page' => $page, 'item' => $lineitem]);
+				} else {
+					$page->body .= $config->twig->render('warehouse/inventory/receiving/line/received-lots.twig', ['page' => $page, 'item' => $lineitem]);
+				}
 			} else {
 				$page->body .= $config->twig->render('warehouse/inventory/receiving/po-header.twig', ['page' => $page, 'purchaseorder' => $purchaseorder]);
 				$page->body .= $config->twig->render('warehouse/inventory/receiving/po-items.twig', ['page' => $page, 'items' => $purchaseorder->get_receivingitems()]);
@@ -41,6 +52,17 @@
 					if ($query_phys->count() == 1) {
 						$physicalitem = $query_phys->findOne();
 						$page->body .= $html->div('class=mb-3');
+
+						if ($physicalitem->has_error()) {
+							
+							if (!$physicalitem->is_on_po()) {
+								$physicalitem->setItemid('');
+								$physicalitem->setLotserial('');
+								$physicalitem->setLotserialref('');
+							}
+							$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => $physicalitem->get_error()]);
+							$page->body .= $html->div('class=mb-3');
+						}
 						$page->body .= $config->twig->render('warehouse/inventory/receiving/po-line-item-scanned-form.twig', ['page' => $page, 'item' => $physicalitem]);
 					} elseif ($query_phys->count() > 1) {
 						if ($input->get->recno) {
@@ -74,29 +96,31 @@
 								$page->title = "No results found for ''$q'";
 								$page->formurl = $page->url;
 								$page->body .= $html->div('class=mb-3');
-								$page->body .= $config->twig->render('warehouse/inventory/receiving/po-line-item-form.twig', ['page' => $page]);
+								$page->body .= $config->twig->render('warehouse/inventory/receiving/po-line-item-form.twig', ['page' => $page, 'ponbr' => $ponbr]);
 							}
 						} else {
 							$page->title = "No results found for ''$scan'";
 							$page->formurl = $page->url;
 							$page->body .= $html->div('class=mb-3');
-							$page->body .= $config->twig->render('warehouse/inventory/receiving/po-line-item-form.twig', ['page' => $page]);
+							$page->body .= $config->twig->render('warehouse/inventory/receiving/po-line-item-form.twig', ['page' => $page, 'ponbr' => $ponbr]);
 						}
 					}
 				} else {
 					$page->formurl = $pages->get('template=redir, redir_file=inventory')->url;
 					$page->body .= $html->div('class=mb-3');
 					$page->body .= $html->h3('', 'Scan item to add');
-					$page->body .= $config->twig->render('warehouse/inventory/receiving/po-line-item-form.twig', ['page' => $page]);
+					$page->body .= $config->twig->render('warehouse/inventory/receiving/po-line-item-form.twig', ['page' => $page, 'ponbr' => $ponbr]);
 				}
 			}
 		} else {
 			$page->title = "PO # $ponbr Does Not Exist";
 			$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => $page->title, 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Check the Purchase Order Number and try again"]);
 			$page->body .= $html->div('class=mb-3');
+			$page->formurl = $pages->get('template=redir, redir_file=inventory')->url;
 			$page->body .= $config->twig->render('warehouse/inventory/receiving/po-form.twig', ['page' => $page]);
 		}
 	} else {
+		$page->formurl = $pages->get('template=redir, redir_file=inventory')->url;
 		$page->body .= $config->twig->render('warehouse/inventory/receiving/po-form.twig', ['page' => $page]);
 	}
 
