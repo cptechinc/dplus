22191ac2a8c007c0514168cb18890b3adcf45225
Paul Gomez <pauldro952@outlook.com>
2019-11-13 14:21:50 -0600

    Purchase Order Documents / Notes

--- site/templates/purchase-order-documents.php
+++ site/templates/purchase-order-documents.php
@@ -1,22 +1,27 @@
 <?php
 	// TODO : INVOICED
-	$modules->get('DpagesMpo')->init_purchaseorder_hooks();
 
 	if ($input->get->ponbr) {
 		$ponbr = PurchaseOrder::get_paddedponumber($input->get->text('ponbr'));
 		$query = PurchaseOrderQuery::create()->filterByPonbr($ponbr);
+		$document_management = $modules->get('DocumentManagement');
 
 		if ($query->count()) {
-			$page->title = "Purchase Order #$ponbr";
-			$purchaseorder = $query->findOne();
+			$page->title = "Purchase Order #$ponbr Documents";
 			$document_management = $modules->get('DocumentManagement');
 			$documents = $document_management->get_purchaseorderdocuments($ponbr);
 			$page->listpage = $pages->get('pw_template=purchase-orders');
 
-			$page->body .= $config->twig->render('purchase-orders/purchase-order/links-header.twig', ['page' => $page]);
-			$page->body .= $config->twig->render('purchase-orders/purchase-order/purchase-order.twig', ['page' => $page, 'purchaseorder' => $purchaseorder]);
+			if ($input->get->document && $input->get->folder) {
+				$folder = $input->get->text('folder');
+				$filename = $input->get->text('document');
+				$document_management->move_document($folder, $filename);
+
+				if ($document_management->is_filewebaccessible($filename)) {
+					$session->redirect($config->url_webdocs.$filename);
+				}
+			}
 			$page->body .= $config->twig->render('purchase-orders/purchase-order/documents.twig', ['page' => $page, 'ponbr' => $ponbr, 'documents' => $documents]);
-			$page->body .= $config->twig->render('purchase-orders/purchase-order/qnotes.twig', ['page' => $page, 'ponbr' => $ponbr, 'notes' => $purchaseorder->get_notes()]);
 		} else {
 			$page->headline = $page->title = "Purchase Order #$ponbr could not be found";
 			$page->body = $config->twig->render('util/error-page.twig', ['msg' => "Check if the Purchase Order Number is correct"]);
