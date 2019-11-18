d10e0c0530ad19b273d1636ff8d792492e4b5473
Paul Gomez <pauldro952@outlook.com>
2019-11-18 09:34:33 -0600

    Update purchase-history-orders.php

--- site/templates/purchase-history-orders.php
+++ site/templates/purchase-history-orders.php
@@ -7,8 +7,10 @@
 	$query->orderByDate_invoiced('DESC');
 	$invoices = $query->paginate($input->pageNum, 10);
 
+	$invpage = $pages->get('pw_template=purchase-order-view')->url;
+
 	//$page->body = $config->twig->render('purchase-historys/search-form.twig', ['page' => $page, 'input' => $input]);
 	$page->body .= $html->h3('', $invoices->getNbResults() . " Invoices");
-	$page->body .= $config->twig->render('purchase-orders/invoices/invoices-list.twig', ['invoices' => $invoices, 'orderpage' => '']);
+	$page->body .= $config->twig->render('purchase-orders/invoices/invoices-list.twig', ['invoices' => $invoices, 'invpage' => $invpage]);
 	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'pagenbr' => $input->pageNum, 'resultscount'=> $invoices->getNbResults()]);
 	include __DIR__ . "/basic-page.php";
