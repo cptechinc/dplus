7281c8e0cf2a0fa59a14b8efe06f5b60ae3920e8
bbullemer <barbara.bullemer@gmail.com>
2019-11-19 14:02:38 -0600

    Update vi-payments.php
    - corrected name and installed the right module

--- site/templates/vi-payments.php
+++ site/templates/vi-payments.php
@@ -16,7 +16,7 @@
 			}
 			$session->paymentstry = 0;
 
-			$module_formatter = $modules->get('ViPurchaseHistory');
+			$module_formatter = $modules->get('ViPaymentHistory');
 			$module_formatter->init_formatter();
 			$document_management = $modules->get('DocumentManagement');
 			$refreshurl = $page->get_vipaymentsURL($vendorID);
