d92d486de95ecf802d8ac9d8edb2e15fd844e9a3
bbullemer <barbara.bullemer@gmail.com>
2019-12-04 14:01:06 -0600

    #44
    - added if statement to templates to check for orderby column
    - added links to table columns in list twig files

--- site/templates/quotes-customer.php
+++ site/templates/quotes-customer.php
@@ -17,11 +17,19 @@
 
 		$query = $filter_quotes->get_query();
 		$query->orderByDate_quoted('DESC');
+
+		if ($page->has_orderby()) {
+			$orderbycolumn = $page->orderby_column;
+			$sort = $page->orderby_sort;
+			$tablecolumn = Quote::get_aliasproperty($orderbycolumn);
+			$query->sortBy($tablecolumn, $sort);
+		}
+
 		$quotes = $query->paginate($input->pageNum, 10);
 
 		// TODO: Filter Form
 		//$page->body = $config->twig->render('sales-orders/search-form.twig', ['page' => $page, 'input' => $input]);
-		$page->body .= $config->twig->render('quotes/customer/quotes-list.twig', ['quotes' => $quotes, 'quotepage' => $pages->get('pw_template=quote-view')->url]);
+		$page->body .= $config->twig->render('quotes/customer/quotes-list.twig', ['page' => $page, 'quotes' => $quotes, 'quotepage' => $pages->get('pw_template=quote-view')->url]);
 		$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'pagenbr' => $input->pageNum, 'resultscount'=> $quotes->getNbResults()]);
 
 	} else {
