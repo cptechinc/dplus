63c3b16e6129119dd05dd87f1dd311995e331963
Paul Gomez <pauldro952@outlook.com>
2019-12-05 09:49:02 -0600

    Sorting on quotes template

--- site/templates/quotes.php
+++ site/templates/quotes.php
@@ -2,17 +2,8 @@
 	$filter_quotes = $modules->get('FilterQuotes');
 	$filter_quotes->init_query($user);
 	$filter_quotes->filter_query($input);
-
+	$filter_quotes->apply_sortby($page);
 	$query = $filter_quotes->get_query();
-	
-	if ($page->has_orderby()) {
-		$orderbycolumn = $page->orderby_column;
-		$sort = $page->orderby_sort;
-		$tablecolumn = Quote::get_aliasproperty($orderbycolumn);
-		$query->sortBy($tablecolumn, $sort);
-	} else {
-		$query->orderByDate_quoted('DESC');
-	}
 
 	$quotes = $query->paginate($input->pageNum, 10);
 
