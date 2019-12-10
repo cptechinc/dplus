9af2122b90c2b46356708c3153974f93a785fd3d
Paul Gomez <pauldro952@outlook.com>
2019-12-10 09:58:43 -0600

    Update ar-code-table.php
    Use page->codetable

--- site/templates/ar-code-table.php
+++ site/templates/ar-code-table.php
@@ -1,42 +1,39 @@
 <?php
+	// NOTE: $page->codetable is a hook Property that points to $page->name
+
 	$ar_codetables = $modules->get('CodeTablesAr');
 
 	if ($input->requestMethod('POST') || $input->get->action) {
 		$rm = strtolower($input->requestMethod());
-		$table = $input->$rm->text('table');
+		$code  = $input->$rm->text('code');
 
-		if ($ar_codetables->validate_codetable($table)) {
-			$module_codetable = $ar_codetables->get_codetable_module($table);
+		if ($ar_codetables->validate_codetable($page->codetable)) {
+			$module_codetable = $ar_codetables->get_codetable_module($page->codetable);
 			$module_codetable->process_input($input);
-			$session->redirect($page->get_codetable_viewURL($table), $http301 = false);
+			$session->redirect($page->get_codetable_viewURL($page->codetable, $code), $http301 = false);
 		}
 	}
 
-	if ($input->get->table) {
-		$table = $input->get->text('table');
+	if ($ar_codetables->validate_codetable($page->codetable)) {
+		$module_codetable = $ar_codetables->get_codetable_module($page->codetable);
+		$page->headline = "$module_codetable->description Table";
+		$page->body .= $config->twig->render('code-tables/links-header.twig', ['page' => $page, 'input' => $input]);
 
-		if ($ar_codetables->validate_codetable($table)) {
-			$module_codetable = $ar_codetables->get_codetable_module($table);
-			$page->headline = "$module_codetable->description Table";
-			$page->body .= $config->twig->render('code-tables/links-header.twig', ['page' => $page, 'input' => $input]);
+		if ($session->response_codetable) {
+			$page->body .= $config->twig->render('code-tables/code-table-response.twig', ['response' => $session->response_codetable]);
+		}
 
-			if ($session->response_codetable) {
-				$page->body .= $config->twig->render('code-tables/code-table-response.twig', ['response' => $session->response_codetable]);
-			}
-
-			if (file_exists(__DIR__."/ar-code-table-$table.php")) {
-				include(__DIR__."/ar-code-table-$table.php");
-			} else {
-				$page->body .= $config->twig->render("code-tables/mar/$table-list.twig", ['page' => $page, 'table' => $table, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
-				$page->body .= $config->twig->render('code-tables/edit-code-modal.twig', ['page' => $page, 'file' => "mar/$table-form.twig"]);
-				$page->js .= $config->twig->render("code-tables/mar/$table.js.twig", ['page' => $page]);
-			}
+		if (file_exists(__DIR__."/ar-code-table-$page->codetable.php")) {
+			include(__DIR__."/ar-code-table-$page->codetable.php");
 		} else {
-			$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Code Table Error", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "AR Code Table '$code' does not exist"]);
+			$page->body .= $config->twig->render("code-tables/mar/$page->codetable/list.twig", ['page' => $page, 'table' => $table, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
+			$page->body .= $config->twig->render('code-tables/edit-code-modal.twig', ['page' => $page, 'file' => "mar/$page->codetable/form.twig"]);
+			$page->js .= $config->twig->render("code-tables/mar/$page->codetable/js.twig", ['page' => $page]);
 		}
 	} else {
-		$session->redirect($page->parent->httpUrl);
+		$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Code Table Error", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "AR Code Table '$page->codetable' does not exist"]);
 	}
+
 	$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
 
 	if ($session->response_codetable) {
