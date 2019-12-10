f03e10e3054a6adfce3b108c85ae58c1cdcf07d6
Paul Gomez <pauldro952@outlook.com>
2019-12-10 09:57:26 -0600

    Delete ar-code-table-menu.php
    Ar Main will now be a dplus-menu template

--- site/templates/ar-code-table-menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$module_codetables = $modules->get('CodeTablesAr');
-	$codetables = $module_codetables->get_codetables();
-
-	$page->body .= $config->twig->render('code-tables/tables-list.twig', ['page' => $page, 'tables' => $codetables]);
-	include __DIR__ . "/basic-page.php";
