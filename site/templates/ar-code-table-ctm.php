fd52934b58bff3926c483cf542e872e8e4a4f88b
Paul Gomez <pauldro952@outlook.com>
2019-12-10 09:58:56 -0600

    Update ar-code-table-ctm.php
    use $page->codetable

--- site/templates/ar-code-table-ctm.php
+++ site/templates/ar-code-table-ctm.php
@@ -1,16 +1,18 @@
 <?php
 	if ($input->get->code) {
-		$configAR = ConfigArQuery::create()->findOne();
 		$code = $input->get->text('code');
+		$page->headline = "Editing $page->title $code";
+		$configAR = ConfigArQuery::create()->findOne();
 
 		if ($configAR->gl_report_type() == 'inventory') {
-			$page->body .= $config->twig->render("code-tables/mar/$table/edit-code-form.twig", ['page' => $page, 'table' => $table, 'code' => $module_codetable->get_code($code)]);
+			$page->body .= $config->twig->render("code-tables/mar/$page->codetable/edit-code-form-inventory.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $module_codetable->get_code($code)]);
 		} else {
-
+			$gl_codes = GlCodeQuery::create()->find();
+			$page->body .= $config->twig->render("code-tables/mar/$page->codetable/edit-code-form-customer.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $module_codetable->get_code($code), 'gl_codes' => $gl_codes]);
 		}
 	} else {
-		$page->body .= $config->twig->render("code-tables/mar/$table/list.twig", ['page' => $page, 'table' => $table, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
+		$page->body .= $config->twig->render("code-tables/mar/$page->codetable/list.twig", ['page' => $page, 'table' => $page->codetable, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
 	}
 
-//$page->body .= $config->twig->render('code-tables/edit-code-modal.twig', ['page' => $page, 'file' => "mar/$table-form.twig"]);
-//$page->js .= $config->twig->render("code-tables/mar/$table.js.twig", ['page' => $page]);
+//$page->body .= $config->twig->render('code-tables/edit-code-modal.twig', ['page' => $page, 'file' => "mar/$page->codetable-form.twig"]);
+//$page->js .= $config->twig->render("code-tables/mar/$page->codetable.js.twig", ['page' => $page]);
