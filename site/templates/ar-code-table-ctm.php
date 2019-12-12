90a0d8b34c74dd982a23c595a3eba43b0699ee87
Paul Gomez <pauldro952@outlook.com>
2019-12-12 15:10:11 -0600

    CTM add ability to add new code

--- site/templates/ar-code-table-ctm.php
+++ site/templates/ar-code-table-ctm.php
@@ -1,14 +1,29 @@
 <?php
+	$html = $modules->get('HtmlWriter');
+
 	if ($input->get->code) {
 		$code = $input->get->text('code');
-		$page->headline = "Editing $page->title $code";
 		$configAR = ConfigArQuery::create()->findOne();
 
+		if ($module_codetable->code_exists($code)) {
+			$page->title = $page->headline = "Editing $page->title $code";
+			$typecode = $module_codetable->get_code($code);
+		} else {
+			$page->title = $page->headline = "Create $page->title";
+			$typecode = new CustomerTypeCode();
+
+			if ($code != 'new') {
+				$typecode->setCode($code);
+				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Type Code '$code' could not be found, use the form to create it"]);
+				$page->body .= $html->div('class=mb-3');
+			}
+		}
+
 		if ($configAR->gl_report_type() == 'inventory') {
-			$page->body .= $config->twig->render("code-tables/mar/$page->codetable/edit-code-form-inventory.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $module_codetable->get_code($code)]);
+			$page->body .= $config->twig->render("code-tables/mar/$page->codetable/edit-code-form-inventory.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $typecode]);
 		} else {
 			$gl_codes = GlCodeQuery::create()->find();
-			$page->body .= $config->twig->render("code-tables/mar/$page->codetable/edit-code-form-customer.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $module_codetable->get_code($code), 'gl_codes' => $gl_codes]);
+			$page->body .= $config->twig->render("code-tables/mar/$page->codetable/edit-code-form-customer.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $typecode, 'gl_codes' => $gl_codes]);
 		}
 	} else {
 		$page->body .= $config->twig->render("code-tables/mar/$page->codetable/list.twig", ['page' => $page, 'table' => $page->codetable, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
