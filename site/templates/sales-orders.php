8b6cda6b2c6b0a3bc44f1af91d94c72df4dfafc5
Paul Gomez <pauldro952@outlook.com>
2019-12-17 10:16:39 -0600

    spreadsheet orders

--- site/templates/sales-orders.php
+++ site/templates/sales-orders.php
@@ -6,7 +6,12 @@
 	$query = $filter_salesorders->get_query();
 	$orders = $query->paginate($input->pageNum, 10);
 
+	if ($input->get->text('download') == 'xlsx') {
+		include('./sales-orders-spreadsheet.php');
+	}
+
 	$page->body = $config->twig->render('sales-orders/search-form.twig', ['page' => $page, 'input' => $input]);
 	$page->body .= $config->twig->render('sales-orders/sales-orders-list-links.twig', ['page' => $page, 'orders' => $orders, 'orderpage' => $pages->get('pw_template=sales-order-view')->url]);
 	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'pagenbr' => $input->pageNum, 'resultscount'=> $orders->getNbResults()]);
+
 	include __DIR__ . "/basic-page.php";
