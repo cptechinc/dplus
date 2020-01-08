1ada6c36477040aeaefff3b261f34322acc14025
bbullemer <barbara.bullemer@gmail.com>
2020-01-08 12:19:52 -0600

    IGM Code Table JS
    - added js to min-code-table-igm template file
    - added js that will check if surcharge is dollar or percent and turn opposite input into readonly with value of zero

--- site/templates/min-code-table-igm.php
+++ site/templates/min-code-table-igm.php
@@ -5,6 +5,7 @@
 		$itemgroup = $module_codetable->get_code($code);
 
 		$page->body .= $config->twig->render("code-tables/min/$page->codetable/form.twig", ['page' => $page, 'table' => $page->codetable, 'itemgroup' => $itemgroup]);
+        $page->js .= $config->twig->render("code-tables/min/$page->codetable/js.twig", ['page' => $page]);
 	} else {
 		$page->body .= $config->twig->render("code-tables/min/$page->codetable/list.twig", ['page' => $page, 'table' => $page->codetable, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
 	}
