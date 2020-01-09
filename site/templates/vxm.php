41a784ec8b8c6df0fca9ca53f25041c44be30b6a
Paul Gomez <pauldro952@outlook.com>
2020-01-09 08:50:07 -0600

    Vendor VXM Items List

--- site/templates/vxm.php
+++ site/templates/vxm.php
@@ -2,7 +2,24 @@
 	$vxm = $modules->get('XrefVxm');
 
 	if ($input->get->vendorID) {
+		$vendorID = $input->get->text('vendorID');
+		$load_vendor = $modules->get('ViLoadVendorShipfrom');
+		$load_vendor->set_vendorID($vendorID);
+		$vendor = $load_vendor->get_vendor();
+
+		if (!$load_vendor->vendor_exists()) {
+			$session->redirect($page->url."?q=$vendorID");
+		}
+
+		if ($input->get->vendoritemID) {
+		} else {
+			$page->headline = "VXM: Vendor $vendor->name";
+			$items = $vxm->get_vendoritems($vendorID);
+			$page->body .= $config->twig->render('items/vxm/vendor/item-list.twig', ['page' => $page, 'items' => $items, 'vendorID' => $vendorID]);
+			$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $items->count()]);
+		}
 	} elseif ($input->get->itemID) {
+
 	} elseif ($input->get->search) {
 		$q = $input->get->text('q');
 		$searchtype = $input->get->text('search');
