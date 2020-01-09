10a50ba98389c0dc6ac4a85e0ffcf2f54bb48e0a
Paul Gomez <pauldro952@outlook.com>
2020-01-09 08:46:51 -0600

    VXM Search Form

--- site/templates/vxm.php
+++ site/templates/vxm.php
@@ -5,6 +5,7 @@
 	} elseif ($input->get->itemID) {
 	} elseif ($input->get->search) {
 	} else {
+		$page->body .= $config->twig->render('items/vxm/vxm-search.twig', ['page' => $page]);
 	}
 
 	include __DIR__ . "/basic-page.php";
