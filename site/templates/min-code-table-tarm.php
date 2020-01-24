749585cf06088b1a8c3bc6c2b5b9b954070650e2
bbullemer <barbara.bullemer@gmail.com>
2020-01-24 10:18:56 -0600

    Update min-code-table-tarm.php
    - cleaned up code by using functions from module
    - passed tariffcode to js file for mac chracter validation

--- site/templates/min-code-table-tarm.php
+++ site/templates/min-code-table-tarm.php
@@ -2,16 +2,16 @@
 	if ($input->get->code) {
 		$code = $input->get->text('code');
 		$page->headline = "Editing $page->title $code";
+		$countries = CountryCodesQuery::create()->find();
 
-		$tariff_country_count = count(TariffCodeCountry::get_countries($code));
-
-		$existing_countries = TariffCodeCountryQuery::create()->get_existing_countries($code);
-
-        $countries = CountryCodesQuery::create()->find();
-		$tariff = $module_codetable->get_code($code);
+		if ($module_codetable->code_exists($code)) {
+			$tariff = $module_codetable->get_code($code);
+		} else {
+			$tariff = new TariffCode();
+		}
 
 		$page->body .= $config->twig->render("code-tables/min/$page->codetable/form.twig", ['page' => $page, 'table' => $page->codetable, 'tariff_country_count' => $tariff_country_count, 'existing_countries' => $existing_countries, 'countries' => $countries, 'tariff' => $tariff]);
-        $page->js .= $config->twig->render("code-tables/min/$page->codetable/js.twig", ['page' => $page]);
+		$page->js .= $config->twig->render("code-tables/min/$page->codetable/js.twig", ['page' => $page, 'tariffcode' => $tariff]);
 	} else {
 		$page->body .= $config->twig->render("code-tables/min/$page->codetable/list.twig", ['page' => $page, 'table' => $page->codetable, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
 	}
