dcbf1c46ab66ff8f7e52441b68e346c165d2f461
bbullemer <barbara.bullemer@gmail.com>
2020-01-27 11:26:19 -0600

    Update so-code-table.php
    - added variable to js file that holds value of max characters for code field

--- site/templates/so-code-table.php
+++ site/templates/so-code-table.php
@@ -30,7 +30,7 @@
 		} else {
 			$page->body .= $config->twig->render("code-tables/mso/$page->codetable/list.twig", ['page' => $page, 'table' => $table, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
 			$page->body .= $config->twig->render('code-tables/edit-code-modal.twig', ['page' => $page, 'file' => "mso/$page->codetable/form.twig"]);
-			$page->js .= $config->twig->render("code-tables/mso/$page->codetable/js.twig", ['page' => $page]);
+			$page->js .= $config->twig->render("code-tables/mso/$page->codetable/js.twig", ['page' => $page, 'max_length_code' => $module_codetable->get_max_length_code()]);
 		}
 	} else {
 		$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Code Table Error", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "AR Code Table '$page->codetable' does not exist"]);
