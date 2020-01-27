8a611efb2374533286c86e0f9a0d1d032f02d33a
Paul Gomez <pauldro952@outlook.com>
2020-01-27 10:07:17 -0600

    ci-quotes display JSON error

--- site/templates/ci-quotes.php
+++ site/templates/ci-quotes.php
@@ -21,7 +21,12 @@
 			$refreshurl = $page->get_customerquotesURL($custID);
 
 			$page->body .= $config->twig->render('customers/ci/ci-links.twig', ['page' => $page, 'custID' => $custID, 'lastmodified' => $module_json->file_modified(session_id(), $page->jsoncode), 'refreshurl' => $refreshurl]);
-			$page->body .= $config->twig->render('customers/ci/quotes/quotes.twig', ['page' => $page, 'custID' => $custID, 'json' => $json, 'module_formatter' => $module_formatter, 'blueprint' => $module_formatter->get_tableblueprint(), 'document_management' => $document_management]);
+
+			if ($json['error']) {
+				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => $json['errormsg']]);
+			} else {
+				$page->body .= $config->twig->render('customers/ci/quotes/quotes.twig', ['page' => $page, 'custID' => $custID, 'json' => $json, 'module_formatter' => $module_formatter, 'blueprint' => $module_formatter->get_tableblueprint(), 'document_management' => $document_management]);
+			}
 		} else {
 			if ($session->quotestry > 3) {
 				$page->headline = $page->title = "Quotes File could not be loaded";
