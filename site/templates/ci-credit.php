09b4491765a18ce737f648915f8bc8e07322f50b
Paul Gomez <pauldro952@outlook.com>
2020-01-27 10:05:57 -0600

    ci-credit display JSON error

--- site/templates/ci-credit.php
+++ site/templates/ci-credit.php
@@ -18,7 +18,12 @@
 
 			$refreshurl = $page->get_customercreditURL($custID, $itemID);
 			$page->body .= $config->twig->render('customers/ci/ci-links.twig', ['page' => $page, 'custID' => $custID, 'lastmodified' => $module_json->file_modified(session_id(), $page->jsoncode), 'refreshurl' => $refreshurl]);
-			$page->body .= $config->twig->render('customers/ci/credit/credit.twig', ['page' => $page, 'customer' => $customer, 'json' => $json]);
+
+			if ($json['error']) {
+				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => $json['errormsg']]);
+			} else {
+				$page->body .= $config->twig->render('customers/ci/credit/credit.twig', ['page' => $page, 'customer' => $customer, 'json' => $json]);
+			}
 		} else {
 			if ($session->credittry > 3) {
 				$page->headline = $page->title = "Credit File could not be loaded";
