fe1ab1f3fb2e4749400a559c35b8df07510dff8e
Paul Gomez <pauldro952@outlook.com>
2020-01-27 10:43:14 -0600

    ii-activity display JSON error

--- site/templates/ii-activity.php
+++ site/templates/ii-activity.php
@@ -24,8 +24,12 @@
 				$refreshurl = $page->get_itemactivityURL($itemID);
 
 				$page->body .= $config->twig->render('items/ii/ii-links.twig', ['page' => $page, 'itemID' => $itemID, 'lastmodified' => $module_json->file_modified(session_id(), $page->jsoncode), 'refreshurl' => $refreshurl]);
-				$page->body .= $config->twig->render('items/ii/activity/activity-screen.twig', ['page' => $page, 'json' => $json, 'module_json' => $module_json, 'date' => $date, 'itemID' => $itemID, 'document_management' => $document_management, 'con' => $con]);
 
+				if ($json['error']) {
+					$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => $json['errormsg']]);
+				} else {
+					$page->body .= $config->twig->render('items/ii/activity/activity-screen.twig', ['page' => $page, 'json' => $json, 'module_json' => $module_json, 'date' => $date, 'itemID' => $itemID, 'document_management' => $document_management]);
+				}
 			} else {
 				if ($session->activitytry > 3) {
 					$page->headline = $page->title = "Activity File could not be loaded";
