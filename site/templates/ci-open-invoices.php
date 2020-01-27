1753f7573a0d0acc7e357aa8e60fee51c613d9db
Paul Gomez <pauldro952@outlook.com>
2020-01-27 10:06:47 -0600

    ci-open-invoices display JSON error

--- site/templates/ci-open-invoices.php
+++ site/templates/ci-open-invoices.php
@@ -21,7 +21,12 @@
 			$refreshurl = $page->get_customeropeninvoicesURL($custID);
 
 			$page->body .= $config->twig->render('customers/ci/ci-links.twig', ['page' => $page, 'custID' => $custID, 'lastmodified' => $module_json->file_modified(session_id(), $page->jsoncode), 'refreshurl' => $refreshurl]);
-			$page->body .= $config->twig->render('customers/ci/open-invoices/open-invoices.twig', ['page' => $page, 'custID' => $custID, 'json' => $json, 'module_formatter' => $module_formatter, 'blueprint' => $module_formatter->get_tableblueprint(), 'document_management' => $document_management]);
+
+			if ($json['error']) {
+				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => $json['errormsg']]);
+			} else {
+				$page->body .= $config->twig->render('customers/ci/open-invoices/open-invoices.twig', ['page' => $page, 'custID' => $custID, 'json' => $json, 'module_formatter' => $module_formatter, 'blueprint' => $module_formatter->get_tableblueprint(), 'document_management' => $document_management]);
+			}
 		} else {
 			if ($session->openinvoicestry > 3) {
 				$page->headline = $page->title = "Open Invoices File could not be loaded";
