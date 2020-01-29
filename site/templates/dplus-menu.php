6351415be7934b74fcd1cf830f34ee225ea5b719
Paul Gomez <pauldro952@outlook.com>
2020-01-29 13:23:39 -0600

    Update dplus-menu.php
    (cherry picked from commit 8e96d4b95e0c00ef0e5a8af6db1f1c018d5d81f9)

--- site/templates/dplus-menu.php
+++ site/templates/dplus-menu.php
@@ -2,7 +2,7 @@
 	if ($user->has_function($page->dplus_function) || empty($page->dplus_function)) {
 		$permission_list = implode("|", $user->get_functions());
 		$page->pagetitle = "Menu: $page->title";
-		$items = $page->children("template!=redir, dplus_function=$permission_list");
+		$items = $page->children("template!=redir|dplus-json, dplus_function=$permission_list");
 		$page->body = $config->twig->render('dplus-menu/menu-list.twig', ['page' => $page, 'items' => $items]);
 	} else {
 		$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "You don't have access to this function", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Function: $page->dplus_function"]);
