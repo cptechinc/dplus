284e31b0e449a5d4b072ac1d0d749533762f7a94
bbullemer <barbara.bullemer@gmail.com>
2020-01-29 09:38:58 -0600

    IWHM Code Table Max Character
    - added code to validate max character length allowance for code field

--- site/templates/min-code-table-iwhm.php
+++ site/templates/min-code-table-iwhm.php
@@ -23,6 +23,7 @@
 		}
 
 		$page->body .= $config->twig->render("code-tables/min/$page->codetable/form.twig", ['page' => $page, 'table' => $page->codetable, 'warehouse' => $warehouse, 'config_in' => $config_in, 'states' => $states, 'countries' => $countries, 'warehouses' => $warehouses]);
+		$page->js   .= $config->twig->render("code-tables/min/$page->codetable/js.twig", ['page' => $page, 'warehouse' => $warehouse]);
 
 		// SHOW NOTES IF TABLE ALREADY EXISTS
 		if ($module_codetable->code_exists($code)) {
