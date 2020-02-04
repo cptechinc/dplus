95e1e22203ebf82ab022e295aa87096af08ff711
Paul Gomez <pauldro952@outlook.com>
2020-02-04 09:45:14 -0600

    Update itm.php
    Allow for searching and if there's an exact match, redirect to the itm page, if not show results.

--- site/templates/itm.php
+++ site/templates/itm.php
@@ -1,8 +1,9 @@
 <?php
 	$page->title = "ITM";
 
-	if ($input->get->itemID) {
+	$itm = $modules->get('Itm');
 
+	if ($input->get->itemID) {
 		$itemID = $input->get->text('itemID');
 
 		$q = ItemMasterItemQuery::create();
@@ -11,12 +12,32 @@
 		if ($q->count()) {
 			$page->title .= " $itemID";
 			$item = $q->findOne();
-			$itemgroups = ItemGroupCodeQuery::create()->find();
+			$itemgroups = InvGroupCodeQuery::create()->find();
 			$pricecodes = ItemPricingQuery::create()->find();
 			$page->body .= $config->twig->render('items/itm/itm-form.twig', ['page' => $page, 'item' => $item, 'itemgroups' => $itemgroups, 'pricecodes' => $pricecodes]);
 		} else {
 			$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Error!", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Item ID $itemID not found in the Item Master"]);
 		}
-	}
+	} elseif ($input->get->q) {
+		$q = $input->get->text('q');
+		$exact_query = ItemMasterItemQuery::create();
 
+		if ($exact_query->filterByItemid($q)->count() == 1) {
+			$session->redirect($page->itmURL($q));
+		} else {
+			$page->headline = "VXM: Searching Items for '$q'";
+			$search_items = $modules->get('FilterItemMaster');
+			$search_items->init_query($user);
+			$search_items->filter_search($q);
+			$search_items->apply_sortby($page);
+			$query = $search_items->get_query();
+			$items = $query->paginate($input->pageNum, 10);
+			$page->searchURL = $page->url;
+			$page->body .= $config->twig->render('items/vxm/search/item/item-search.twig', ['page' => $page, 'items' => $items]);
+			$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $items->getNbResults()]);
+		}
+	} else {
+		$page->searchURL = $page->url;
+		$page->body .= $config->twig->render('items/item-search.twig', ['page' => $page, 'items' => array()]);
+	}
 	include __DIR__ . "/basic-page.php";
