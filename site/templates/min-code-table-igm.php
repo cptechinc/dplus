b4f3f52bc3639bba4195929d52e075db79b1d80a
Barbara <barbara@cptechinc.com>
2020-02-17 11:09:30 -0600

    IGM Code Tables
    - added product line codes and gl accounts options to certain fields

--- site/templates/min-code-table-igm.php
+++ site/templates/min-code-table-igm.php
@@ -10,7 +10,10 @@
 			$itemgroup = new InvGroupCode();
 		}
 
-		$page->body .= $config->twig->render("code-tables/min/$page->codetable/form.twig", ['page' => $page, 'table' => $page->codetable, 'itemgroup' => $itemgroup]);
+		$product_line_codes = InvProductLineCodeQuery::create()->find();
+		$gl_accounts = GlCodeQuery::create()->find();
+
+		$page->body .= $config->twig->render("code-tables/min/$page->codetable/form.twig", ['page' => $page, 'table' => $page->codetable, 'itemgroup' => $itemgroup, 'product_line_codes' => $product_line_codes, 'gl_accounts' => $gl_accounts]);
         $page->js .= $config->twig->render("code-tables/min/$page->codetable/js.twig", ['page' => $page, 'itemgroup' => $itemgroup]);
 	} else {
 		$page->body .= $config->twig->render("code-tables/min/$page->codetable/list.twig", ['page' => $page, 'table' => $page->codetable, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
