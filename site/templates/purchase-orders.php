03244a9393323f6b92469acd5b5f87955bd0e7cd
Paul Gomez <pauldro952@hotmail.com>
2020-02-19 09:41:22 -0600

    Fixed URL to Purchase Order Page

--- site/templates/purchase-orders.php
+++ site/templates/purchase-orders.php
@@ -9,6 +9,6 @@
 
 	$page->body = $config->twig->render('purchase-orders/search-form.twig', ['page' => $page, 'input' => $input]);
 	$page->body .= $html->h3('', $orders->getNbResults() . " Purchase Orders");
-	$page->body .= $config->twig->render('purchase-orders/purchase-orders-list-links.twig', ['page' => $page, 'purchaseorders' => $orders, 'orderpage' => '']);
+	$page->body .= $config->twig->render('purchase-orders/purchase-orders-list-links.twig', ['page' => $page, 'purchaseorders' => $orders, 'orderpage' => $pages->get('pw_template=purchase-order-view')->url]);
 	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'pagenbr' => $input->pageNum, 'resultscount'=> $orders->getNbResults()]);
 	include __DIR__ . "/basic-page.php";
