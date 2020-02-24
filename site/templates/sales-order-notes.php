64aa6e430b0026e2c045f822de675d0925f254ed
Paul Gomez <pauldro952@hotmail.com>
2020-02-24 09:15:13 -0600

    Update sales-order-notes.php
    Use Qnotes Sales Order Module for retreiving Qnotes

--- site/templates/sales-order-notes.php
+++ site/templates/sales-order-notes.php
@@ -1,12 +1,11 @@
 <?php
 	$modules->get('DpagesMso')->init_salesorder_hooks();
-	$module_qnotes_crud = $modules->get('QnotesCrud');
+	$module_qnotes = $modules->get('QnotesSalesOrder');
 
 	if ($input->requestMethod('POST')) {
-		$response = $module_qnotes_crud->process_input_salesorder($input);
+		$response = $module_qnotes->process_input($input);
 		$page->fullURL->query->remove('linenbr');
 		$session->redirect($page->fullURL->getURL());
-
 	} else {
 		if ($input->get->ordn) {
 			$ordn = $input->get->text('ordn');
@@ -20,8 +19,8 @@
 					$order_items = SalesHistoryDetailQuery::create()->filterByOrdernumber($ordn)->find();
 				}
 				$page->title = "Sales Order #$ordn Notes";
-				$page->body = $config->twig->render('sales-orders/sales-order/qnotes-page.twig', ['page' => $page, 'user' => $user, 'ordn' => $ordn, 'order' => $order, 'items' => $order_items]);
-				$page->body .= $config->twig->render('sales-orders/sales-order/notes/add-note-modal.twig', ['page' => $page, 'ordn' => $ordn]);
+				$page->body = $config->twig->render('sales-orders/sales-order/qnotes-page.twig', ['page' => $page, 'user' => $user, 'ordn' => $ordn, 'order' => $order, 'items' => $order_items, 'qnotes_so' => $module_qnotes]);
+				$page->body .= $config->twig->render('sales-orders/sales-order/notes/add-note-modal.twig', ['page' => $page, 'ordn' => $ordn, 'qnotes_so' => $module_qnotes]);
 				$config->scripts->append(hash_templatefile('scripts/orders/order-notes.js'));
 			} else {
 				$page->headline = "Sales Order #$ordn could not be found";
