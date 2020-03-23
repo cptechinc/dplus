66f9cc1cf1ae36195186ba4d3dbb777e8299fc4e
Barbara <barbara@cptechinc.com>
2020-03-23 10:51:40 -0500

    CodeTablesRgasc
    - added warehouse dropdown to form
    - moved save and edit to right side of modal
    - added '...' to description on list to prevent overlap

--- site/templates/so-code-table.php
+++ site/templates/so-code-table.php
@@ -27,11 +27,15 @@
 			$page->body .= $config->twig->render('code-tables/code-table-response.twig', ['response' => $session->response_codetable]);
 		}
 
+		if ($module_codetable == 'CodeTablesRgasc') {
+			$warehouses = WarehouseQuery::create()->find();
+		}
+
 		if (file_exists(__DIR__."/so-code-table-$page->codetable.php")) {
 			include(__DIR__."/so-code-table-$page->codetable.php");
 		} else {
 			$page->body .= $config->twig->render("code-tables/mso/$page->codetable/list.twig", ['page' => $page, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
-			$page->body .= $config->twig->render('code-tables/edit-code-modal.twig', ['page' => $page, 'file' => "mso/$page->codetable/form.twig", 'max_length_code' => $module_codetable->get_max_length_code()]);
+			$page->body .= $config->twig->render('code-tables/edit-code-modal.twig', ['page' => $page, 'file' => "mso/$page->codetable/form.twig", 'warehouses' => $warehouses, 'max_length_code' => $module_codetable->get_max_length_code()]);
 			$page->js .= $config->twig->render("code-tables/mso/$page->codetable/js.twig", ['page' => $page, 'max_length_code' => $module_codetable->get_max_length_code()]);
 		}
 	} else {
