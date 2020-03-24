cc058b07b3d31d8ba3e79c4c4b6df5e833bd4ad3
Barbara <barbara@cptechinc.com>
2020-03-24 15:24:27 -0500

    CodeTablesCtm Record Locker Changes

--- site/templates/ar-code-table-ctm.php
+++ site/templates/ar-code-table-ctm.php
@@ -1,5 +1,6 @@
 <?php
 	$html = $modules->get('HtmlWriter');
+	$recordlocker = $modules->get('RecordLockerUser');
 
 	if ($input->get->code) {
 		$code = $input->get->text('code');
@@ -19,8 +20,25 @@
 			}
 		}
 
+		if (!$typecode->isNew()) {
+			/**
+			 * Show alert that typecode is locked if
+			 *  1. Typecode Isn't new
+			 *  2. The typecode has a record lock
+			 *  3. Userid does not match the lock
+			 * Otherwise if not locked, create lock
+			 */
+			if ($recordlocker->function_locked($page->codetable, $typecode->code) && !$recordlocker->function_locked_by_user($page->codetable, $typecode->code)) {
+				$msg = "$typecode->code is being locked by " . $recordlocker->get_locked_user($page->codetable, $typecode->code);
+				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "Typecode $typecode->code is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
+				$page->body .= $html->div('class=mb-3');
+			} elseif (!$recordlocker->function_locked($page->codetable, $typecode->code)) {
+				$recordlocker->create_lock($page->codetable, $typecode->code);
+			}
+		}
+
 		if ($configAR->gl_report_type() == 'inventory') {
-			$page->body .= $config->twig->render("code-tables/mar/$page->codetable/edit-code-form.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $typecode, 'module_custnotes' => $modules->get('CodeTablesCtmNotes')]);
+			$page->body .= $config->twig->render("code-tables/mar/$page->codetable/edit-code-form.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $typecode, 'module_custnotes' => $modules->get('CodeTablesCtmNotes'), 'recordlocker' => $recordlocker]);
 		} else {
 			$gl_codes = GlCodeQuery::create()->find();
 			$page->body .= $config->twig->render("code-tables/mar/$page->codetable/edit-code-form-customer.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $typecode, 'gl_codes' => $gl_codes, 'module_custnotes' => $modules->get('CodeTablesCtmNotes')]);
@@ -30,6 +48,7 @@
 		$page->js   .= $config->twig->render("code-tables/mar/$page->codetable/js.twig", ['page' => $page, 'typecode' => $typecode]);
 	} else {
 		$page->title = $page->headline = "CTM";
+		$recordlocker->remove_lock($page->codetable);
 		$page->body .= $config->twig->render("code-tables/mar/$page->codetable/list.twig", ['page' => $page, 'table' => $page->codetable, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable]);
 	}
 
