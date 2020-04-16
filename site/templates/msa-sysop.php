8d8f46537bef6d804af67a4ed1a0ec502721a864
Barbara <barbara@cptechinc.com>
2020-04-16 12:50:42 -0500

    SYSOP Highlight Issue
    - codes with spaces would not highlight

--- site/templates/msa-sysop.php
+++ site/templates/msa-sysop.php
@@ -20,6 +20,7 @@
 
 	if ($msa_codetables->validate_codetable($page->codetable)) {
 		$page->focus = $input->get->focus ? $input->get->text('focus') : '';
+		$page->focus = str_replace(' ', '', $page->focus);
 
 		$module_codetable = $msa_codetables->get_codetable_module($page->codetable);
 
