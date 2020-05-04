f14eea0278f65cde578f8534fa6c15c06b52a8f9
Paul Gomez <pauldro952@hotmail.com>
2020-05-04 11:08:53 -0500

    ITM order notes

--- site/templates/itm.php
+++ site/templates/itm.php
@@ -47,7 +47,8 @@
 				$page->body .= $config->twig->render('items/itm/notes/internal/modal.twig', ['page' => $page, 'item' => $item, 'm_notes' => $module_notes, 'user' => $user]);
 				$page->body .= $config->twig->render('items/itm/notes/revision/modal.twig', ['page' => $page, 'item' => $item, 'm_notes' => $module_notes, 'user' => $user]);
 				$page->body .= $config->twig->render('items/itm/notes/inspection/modal.twig', ['page' => $page, 'item' => $item, 'm_notes' => $module_notes, 'user' => $user]);
-				$page->js   .= $config->twig->render("items/itm/notes/js.twig", ['page' => $page, 'validateurl' => $pages->get('pw_template=itm-json')->url, 'item' => $item]);
+				$page->body .= $config->twig->render('items/itm/notes/order/modal.twig', ['page' => $page, 'item' => $item, 'm_notes' => $module_notes]);
+				$page->js   .= $config->twig->render("items/itm/notes/js.twig", ['page' => $page, 'validateurl' => $pages->get('pw_template=itm-json')->url, 'item' => $item, 'm_notes' => $module_notes]);
 			}
 			$page->body .= $config->twig->render("util/ajax-modal.twig");
 			$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
