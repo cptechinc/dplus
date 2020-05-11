af1c43bd483c1d102548d27ec38f2bff8c17a191
Paul Gomez <pauldro952@hotmail.com>
2020-05-11 10:26:34 -0500

    Update itm-whse.php

--- site/templates/itm-whse.php
+++ site/templates/itm-whse.php
@@ -1,30 +1,82 @@
 <?php
 	$itm = $modules->get('Itm');
-	$itm_warehouse = $modules->get('ItmWarehouse');
+	$itm_warehouse = $modules->get('ItmWarehouse')->init();
+	$html = $modules->get('HtmlWriter');
+	$recordlocker = $modules->get('RecordLockerUser');
+
+	if ($input->requestMethod('POST') || $input->get->action) {
+		$rm = strtolower($input->requestMethod());
+		$itm_warehouse->process_input($input);
+		$itemID = $input->$rm->text('itemID');
+		$whseID = $input->$rm->text('whseID');
+		$session->redirect($page->itm_warehouseURL($itemID, $whseID));
+	}
+
+	$page->show_breadcrumbs = false;
+	$page->body .= $config->twig->render('items/itm/bread-crumbs.twig', ['page' => $page, 'page_itm' => $page->parent, 'input' => $input]);
+
+	if ($session->response_itm) {
+		$page->body .= $config->twig->render('items/itm/response-alert.twig', ['response' => $session->response_itm]);
+		$session->remove('response_itm');
+	}
+
 
 	if ($input->get->itemID) {
 		$itemID = $input->get->text('itemID');
 
 		if ($itm->item_exists($itemID)) {
-			if ($input->requestMethod('POST') || $input->get->action) {
-				$rm = strtolower($input->requestMethod());
-				$itm_warehouse->process_input($input);
-				$itemID = $input->$rm->text('itemID');
-				//$session->redirect($page->itm_warehouseURL($itemID));
-			}
-
-			if ($session->response_itm) {
-				$page->body .= $config->twig->render('items/itm/response-alert.twig', ['response' => $session->response_itm]);
-				$session->remove('response_itm');
-			}
-
-			$page->headline = "Warehouses for $itemID";
 			$item = $itm->get_item($itemID);
-
 			$page->body .= $config->twig->render('items/itm/itm-links.twig', ['page' => $page, 'page_itm' => $page->parent]);
-			$page->body .= $config->twig->render('items/itm/warehouse/description.twig', ['page' => $page, 'item' => $item]);
-			$page->body .= $config->twig->render('items/itm/warehouse/list.twig', ['page' => $page, 'warehouses' => $itm_warehouse->get_warehouses($itemID)]);
 
+			if ($input->get->whseID) {
+				$whseID = $input->get->text('whseID');
+
+				if ($itm_warehouse->itemwarehouse_exists($whseID, $itemID) || $whseID == 'new') {
+					$warehouse_lookup = $modules->get('LookupWarehouse');
+
+
+					if ($whseID == 'new') {
+						$item_warehouse = $itm_warehouse->get_new_itemwarehouse();
+						$item_warehouse->setItemid($itemID);
+						$page->headline = "Adding Warehouse for Item $itemID";
+					} else {
+						$item_warehouse = $itm_warehouse->get_itemwarehouse($whseID, $itemID);
+						$page->headline = "Warehouse $whseID Item $itemID";
+					}
+
+					if (!$item_warehouse->isNew()) {
+						/**
+						 * Show alert that warehouse is locked if
+						 *  1. Warehouse isn't new
+						 *  2. The warehouse has a record lock
+						 *  3. Userid does not match the lock
+						 * Otherwise if not locked, create lock
+						 */
+						if ($recordlocker->function_locked($page->pw_template, "$item_warehouse->itemid-$item_warehouse->warehouseid") && !$recordlocker->function_locked_by_user($page->pw_template, "$item_warehouse->itemid-$item_warehouse->warehouseid")) {
+							$msg = "Warehouse $item_warehouse->warehouseid for $item_warehouse->itemid is being locked by " . $recordlocker->get_locked_user($page->pw_template, "$item_warehouse->itemid-$item_warehouse->warehouseid");
+							$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "Warehouse $item_warehouse->warehouseid is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
+							$page->body .= $html->div('class=mb-3');
+						} elseif (!$recordlocker->function_locked($page->pw_template, "$item_warehouse->itemid-$item_warehouse->warehouseid")) {
+							$recordlocker->create_lock($page->pw_template, "$item_warehouse->itemid-$item_warehouse->warehouseid");
+						}
+					}
+
+					$page->body .= $config->twig->render('items/itm/warehouse/form.twig', ['page' => $page, 'warehouse' => $item_warehouse, 'm_whse' => $itm_warehouse]);
+					$page->body .= $config->twig->render('items/itm/warehouse/bins-modal.twig', ['page' => $page, 'itemID' => $itemID, 'm_whse' => $itm_warehouse]);
+					$jsconfig = array('warehouses' => $itm_warehouse->get_itemwarehouses_js($itemID), 'in_defaultbin' => $itm_warehouse->configs->inventory->default_bin);
+					$page->body .= $config->twig->render('util/js-variables.twig', ['variables' => $jsconfig]);
+					$page->js   .= $config->twig->render('items/itm/warehouse/js.twig', ['page' => $page, 'warehouse' => $item_warehouse, 'm_whse' => $itm_warehouse]);
+					$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
+				} else {
+					$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Error!", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Item $itemID does not exist in Warehouse $whseID"]);
+					$page->body .= $html->div('class=mb-3');
+					$page->body .= $html->a('class=btn btn-primary|href='.$page->itm_warehouseURL($itemID), $html->icon('fa fa-undo')." Back to $itemID Warehouses");
+				}
+			} else {
+				$page->headline = "Warehouses for $itemID";
+				$page->body .= $config->twig->render('items/itm/warehouse/description.twig', ['page' => $page, 'item' => $item]);
+				$page->body .= $config->twig->render('items/itm/warehouse/list.twig', ['page' => $page, 'itemID' => $itemID, 'warehouses' => $itm_warehouse->get_itemwarehouses($itemID)]);
+			}
 		}
 	} else {
 		$session->redirect($page->itmURL());
