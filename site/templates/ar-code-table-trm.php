4de4d76c98808028eee288982e1601f626c9fa13
Barbara <barbara@cptechinc.com>
2020-05-26 12:02:41 -0500

    Update ar-code-table-trm.php
    - added creditcards

--- site/templates/ar-code-table-trm.php
+++ site/templates/ar-code-table-trm.php
@@ -5,7 +5,7 @@
 	if ($input->get->code) {
 		$code = $input->get->text('code');
 		$countries = CountryQuery::create()->find();
-	 	// $creditcards = CreditCardDigitGetQuery::create()->find();
+	 	$creditcards = CreditCardDigitGetQuery::create()->find();
 
 		if ($module_codetable->code_exists($code)) {
 			$page->title = $page->headline = "TRM: $code";
@@ -38,7 +38,7 @@
 			}
 		}
 
-		$page->body .= $config->twig->render("code-tables/mar/$page->codetable/form.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $termscode, 'countries' => $countries, 'recordlocker' => $recordlocker, 'm_trm' => $module_codetable]);
+		$page->body .= $config->twig->render("code-tables/mar/$page->codetable/form.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $termscode, 'countries' => $countries, 'creditcards' => $creditcards, 'recordlocker' => $recordlocker, 'm_trm' => $module_codetable]);
 		$page->js   .= $config->twig->render("code-tables/mar/$page->codetable/js.twig", ['page' => $page, 'termscode' => $termscode]);
 	} else {
 		$page->title = $page->headline = "Customer Terms Code";
