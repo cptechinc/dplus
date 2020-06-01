69e0f136d6beae5c5e48eb5527ab4784261d409e
Paul Gomez <pauldro952@hotmail.com>
2020-06-01 14:33:55 -0500

    ITM VXM

--- site/templates/itm-xrefs-vxm.php
+++ site/templates/itm-xrefs-vxm.php
@@ -12,7 +12,7 @@
 		$values = $input->$rm;
 		$vendoritemID = $values->text('action') == 'remove-vxm-item' ? '' : $input->$rm->text('vendoritemID');
 
-		// TODO: Redirect
+		$session->redirect($page->vxm_itemURL($values->text('vendorID'), $vendoritemID));
 	}
 
 	$page->show_breadcrumbs = false;
@@ -27,51 +27,53 @@
 		$itemID = strtoupper($input->get->text('itemID'));
 
 		if ($itm->item_exists($itemID)) {
-			$item = $itm->get_item($itemID);
-
-			if ($input->get->vendorID) {
+			if ($input->get->vendoritemID) {
 				$vendorID = $input->get->text('vendorID');
-
-				if ($input->get->vendoritemID) {
-					$vendoritemID = $input->get->text('vendoritemID');
-
-					if ($vxm->vendors_item_exists($vendorID, $vendoritemID)) {
-						$vendor = $modules->get('ViLoadVendorShipfrom')->set_vendorID($vendorID)->get_vendor();
-						$item = $vxm->get_vxm_item($vendorID, $vendoritemID);
-					} else {
-						// TODO
-					}
-
-					if (!$item->isNew()) {
-						/**
-						 * Show alert that VXM is locked if
-						 *  1. VXM isn't new
-						 *  2. The VXM has a record lock
-						 *  3. Userid does not match the lock
-						 * Otherwise if not locked, create lock
-						 */
-						if ($recordlocker->function_locked($page->name, $vxm->get_recordlocker_key($item)) && !$recordlocker->function_locked_by_user($page->name, $vxm->get_recordlocker_key($item))) {
-							$msg = "VXM ". $vxm->get_recordlocker_key($item) ." is being locked by " . $recordlocker->get_locked_user($page->name, $vxm->get_recordlocker_key($item));
-							$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "VXM $vxm->get_recordlocker_key($item) is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
-							$page->body .= $html->div('class=mb-3');
-						} elseif (!$recordlocker->function_locked($page->name, $vxm->get_recordlocker_key($item))) {
-							$recordlocker->create_lock($page->name, $vxm->get_recordlocker_key($item));
-						}
-					}
-
-				$unitsofm = UnitofMeasurePurchaseQuery::create()->find();
+				$vendoritemID = $input->get->text('vendoritemID');
 				$page->headline = "ITM: $itemID VXM Item $vendoritemID for $vendorID";
-				$page->body .= $config->twig->render('items/vxm/vendor/item.twig', ['page' => $page, 'item' => $item, 'vendor' => $vendor, 'unitsofm' => $unitsofm, 'vxm' => $vxm, 'recordlocker' => $recordlocker]);
-				$page->js .= $config->twig->render('items/vxm/vendor/item/js.twig', ['item' => $item, 'url_validate' => $pages->get('pw_template=vxm-validate')->httpUrl]);
+
+				if ($vxm->vxm_item_exists($vendorID, $vendoritemID)) {
+					$item = $vxm->get_vxm_item($vendorID, $vendoritemID);
+
+					/**
+					 * Show alert that VXM is locked if
+					 *  1. VXM isn't new
+					 *  2. The VXM has a record lock
+					 *  3. Userid does not match the lock
+					 * Otherwise if not locked, create lock
+					 */
+					if ($recordlocker->function_locked($page->name, $vxm->get_recordlocker_key($item)) && !$recordlocker->function_locked_by_user($page->name, $vxm->get_recordlocker_key($item))) {
+						$msg = "VXM ". $vxm->get_recordlocker_key($item) ." is being locked by " . $recordlocker->get_locked_user($page->name, $vxm->get_recordlocker_key($item));
+						$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "VXM ".$vxm->get_recordlocker_key($item)." is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
+						$page->body .= $html->div('class=mb-3');
+					} elseif (!$recordlocker->function_locked($page->name, $vxm->get_recordlocker_key($item))) {
+						$recordlocker->create_lock($page->name, $vxm->get_recordlocker_key($item));
+					}
+				} else {
+					$item = $vxm->get_vxm_item_new();
+					$item->setVendorid($vendorID);
+					$item->setOuritemid($itemID);
+					$page->headline = "ITEM: VXM Creating Item";
+
+					if ($vendoritemID != 'new') {
+						$item->setVendoritemid($vendoritemID);
+						$msg = "VXM for Vendor $vendorID Vendor Item ID $vendoritemID does not exist";
+						$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => 'Error with VXM Record', 'iconclass' => 'fa fa-warning fa-2x', 'message' => $msg]);
+						$page->body .= $html->div('class=mb-3');
+					}
 				}
+				$page->searchvendorsURL = $pages->get('pw_template=vi-search')->url;
+				$page->body .= $config->twig->render('items/itm/description.twig', ['page' => $page, 'item' => $itm->get_item($itemID)]);
+				$page->body .= $config->twig->render('items/vxm/item/form.twig', ['page' => $page, 'item' => $item, 'vxm' => $vxm, 'recordlocker' => $recordlocker]);
+				$page->js .= $config->twig->render('items/vxm/item/form/js.twig', ['page' => $page, 'item' => $item, 'url_validate' => $pages->get('pw_template=vxm-validate')->httpUrl]);
 			} else {
 				$filter_vxm->filter_query($input);
 				$filter_vxm->apply_sortby($page);
 				$page->headline = "ITEM: VXM Item $itemID";
 				$items = $filter_vxm->query->paginate($input->pageNum, 10);
-
-				$page->body .= $html->h3('', $items->getNbResults() ." VXM Items for $itemID");
-				$page->body .= $config->twig->render('items/vxm/item/item-list.twig', ['page' => $page, 'items' => $items, 'vxm' => $vxm, 'recordlocker' => $recordlocker]);
+				
+				$page->body .= $config->twig->render('items/vxm/item-list-header.twig', ['page' => $page, 'heading' => $items->getNbResults() ." VXM Items for $itemID"]);
+				$page->body .= $config->twig->render('items/vxm/item-list.twig', ['page' => $page, 'items' => $items, 'vxm' => $vxm, 'recordlocker' => $recordlocker]);
 				$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $items->getNbResults()]);
 			}
 
