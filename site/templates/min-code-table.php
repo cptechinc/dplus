0e636a33b989bc3b08056335779dd8e9bd5d0590
Barbara <barbara@cptechinc.com>
2020-06-22 14:05:22 -0500

    CodeTablesIplm Existing Code

--- site/templates/min-code-table.php
+++ site/templates/min-code-table.php
@@ -39,7 +39,7 @@
 		} else {
 			$page->body .= $config->twig->render("code-tables/min/$page->codetable/list.twig", ['page' => $page, 'codes' => $module_codetable->get_codes(), 'response' => $session->response_codetable, 'config_so' => $config_so, 'config_ar' => $config_ar]);
 			$page->body .= $config->twig->render('code-tables/edit-code-modal.twig', ['page' => $page, 'file' => "min/$page->codetable/form.twig", 'max_length_code' => $module_codetable->get_max_length_code(), 'config_so' => $config_so, 'config_ar' => $config_ar, 'dpluscustomer' => $dpluscustomer]);
-			$page->js   .= $config->twig->render("code-tables/min/$page->codetable/js.twig", ['page' => $page, 'max_length_code' => $module_codetable->get_max_length_code()]);
+			$page->js   .= $config->twig->render("code-tables/min/$page->codetable/js.twig", ['page' => $page, 'max_length_code' => $module_codetable->get_max_length_code(), 'm_in' => $module_codetable]);
 		}
 	} else {
 		$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Code Table Error", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "IN Code Table '$page->codetable' does not exist"]);
