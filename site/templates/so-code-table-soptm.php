5e5009b3c5d98f8c0406d8a50b35b70ee6c83ad8
Barbara <barbara@cptechinc.com>
2020-06-24 11:16:09 -0500

    CodeTablesSoptm Existing Code

--- site/templates/so-code-table-soptm.php
+++ site/templates/so-code-table-soptm.php
@@ -7,8 +7,8 @@
 		$optcodes = $module_codetable->get_codes($sysopcode);
 
 		$page->body .= $config->twig->render("code-tables/mso/$page->codetable/list-codes.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $sysop, 'sysop' => $sysop, 'optcodes' => $optcodes, 'response' => $session->response_codetable]);
-		$page->body .= $config->twig->render('code-tables/edit-code-modal.twig', ['page' => $page, 'file' => "mso/$page->codetable/form.twig", 'max_length_code' => SysopOptionalCode::MAX_LENGTH_CODE]);
-		$page->js   .= $config->twig->render("code-tables/mso/$page->codetable/js.twig", ['page' => $page, 'sysop' => $sysopcode, 'max_length_code' => SysopOptionalCode::MAX_LENGTH_CODE]);
+		$page->body .= $config->twig->render('code-tables/edit-code-modal.twig', ['page' => $page, 'file' => "mso/$page->codetable/form.twig", 'sysop' => $sysopcode, 'max_length_code' => SysopOptionalCode::MAX_LENGTH_CODE]);
+		$page->js   .= $config->twig->render("code-tables/mso/$page->codetable/js.twig", ['page' => $page, 'sysop' => $sysopcode, 'max_length_code' => SysopOptionalCode::MAX_LENGTH_CODE, 'm_soptm' => $module_codetable]);
 	} else {
 		$page->body .= $config->twig->render("code-tables/mso/$page->codetable/list-sysop.twig", ['page' => $page, 'table' => $page->codetable, 'codes' => $module_codetable->get_sysops()]);
 	}
