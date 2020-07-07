abc1c606adf8817e0cb054b20cbdcdd3a1a41a11
Barbara <barbara@cptechinc.com>
2020-07-07 12:02:07 -0500

    CodeTablesAoptm

--- site/templates/ap-code-table.php
+++ site/templates/ap-code-table.php
@@ -8,9 +8,12 @@
 		$action = $input->$rm->text('action');
 
 		// TODO: LOGIC to see if code was removed
-		$code = $input->$rm->text('code');
+		$code  = $input->requestMethod('GET') ? $input->$rm->text('code') : false;
 		$code = $action == 'update-notes' || $action == 'delete-notes' ? $code : '';
 		$code = $page->codetable == 'aoptm' ? $input->$rm->text('sysop') : $code;
+		if ( $page->codetable != 'aoptm') {
+			$code = $action == 'remove-code' ? '' : $code;
+		}
 
 		if ($ap_codetables->validate_codetable($page->codetable)) {
 			$module_codetable = $ap_codetables->get_codetable_module($page->codetable);
