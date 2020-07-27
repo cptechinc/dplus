681e575fa3829f9394cda0982cc61e0b070d77cc
Barbara <barbara@cptechinc.com>
2020-07-27 16:44:20 -0500

    Update ar-code-table-trm.php
    - added new CustomerTermsCode::new()
    - added include for javascipt files

--- site/templates/ar-code-table-trm.php
+++ site/templates/ar-code-table-trm.php
@@ -13,7 +13,7 @@
 			$termscode = $module_codetable->get_code($code);
 		} else {
 			$page->title = $page->headline = "Create $page->title";
-			$termscode = new CustomerTermsCode();
+			$termscode = CustomerTermsCode::new();
 
 			if ($code != 'new') {
 				$termscode->setCode($code);
@@ -40,7 +40,8 @@
 		}
 
 		$page->body .= $config->twig->render("code-tables/mar/$page->codetable/form.twig", ['page' => $page, 'table' => $page->codetable, 'code' => $termscode, 'countries' => $countries, 'creditcards' => $creditcards, 'configso' => $configSO, 'recordlocker' => $recordlocker, 'm_trm' => $module_codetable]);
-		$page->js   .= $config->twig->render("code-tables/mar/$page->codetable/js.twig", ['page' => $page, 'configso' => $configSO, 'termscode' => $termscode, 'm_trm' => $module_codetable]);
+		$page->js   .= $config->twig->render("code-tables/mar/$page->codetable/js/js.twig", ['page' => $page, 'configso' => $configSO, 'termscode' => $termscode, 'm_trm' => $module_codetable]);
+		$page->js   .= $config->twig->render("code-tables/mar/$page->codetable/js/splits.js.twig", ['page' => $page, 'configso' => $configSO, 'termscode' => $termscode, 'm_trm' => $module_codetable]);
 	} else {
 		$page->title = $page->headline = "Customer Terms Code";
 		$recordlocker->remove_lock($page->codetable);
