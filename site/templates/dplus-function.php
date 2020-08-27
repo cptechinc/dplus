02599f0ff857c4939623360cf3e5ed43b51018a9
Paul Gomez <pauldro952@hotmail.com>
2020-08-27 14:11:23 -0500

    Use dplus_function and dplus_permission for validation

--- site/templates/dplus-function.php
+++ site/templates/dplus-function.php
@@ -7,10 +7,12 @@
 		$template = str_replace('.php', '', $page->pw_template) . '.php';
 
 		if (file_exists("./$template")) {
-			if ($user->has_function($page->dplus_function) || empty($page->dplus_function)) {
+			$permission = empty($page->dplus_function) ? $page->dplus_permission : $page->dplus_function;
+
+			if ($user->has_function($permission) || empty($permission)) {
 				include("./$template");
 			} else {
-				$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "You don't have access to this function", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Function: $page->dplus_function"]);
+				$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "You don't have access to this function", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Permission: $permission"]);
 				include __DIR__ . "/basic-page.php";
 			}
 		} else {
