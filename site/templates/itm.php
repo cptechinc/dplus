ef975cf97573aea5925a22d6a495af51cf674e61
Paul Gomez <pauldro952@hotmail.com>
2020-09-11 15:35:36 -0500

    $qnoets variable shortening

--- site/templates/itm.php
+++ site/templates/itm.php
@@ -68,21 +68,13 @@
 			if ($itm->item_exists($itemID)) {
 				$page->body .= $html->div('class=mb-3', '&nbsp;');
 				$page->body .= $html->div('class=mb-3', '&nbsp;');
-				$module_notes = $modules->get('QnotesItem');
+				$qnotes = $modules->get('QnotesItem');
 
-				$page->body .= $config->twig->render('items/itm/notes/notes.twig', ['page' => $page, 'item' => $item, 'm_notes' => $module_notes, 'user' => $user]);
-				$page->body .= $config->twig->render('items/itm/notes/internal/modal.twig', ['page' => $page, 'item' => $item, 'm_notes' => $module_notes, 'user' => $user]);
-				$page->body .= $config->twig->render('items/itm/notes/revision/modal.twig', ['page' => $page, 'item' => $item, 'm_notes' => $module_notes, 'user' => $user]);
-				$page->body .= $config->twig->render('items/itm/notes/inspection/modal.twig', ['page' => $page, 'item' => $item, 'm_notes' => $module_notes, 'user' => $user]);
-				$page->body .= $config->twig->render('items/itm/notes/order/modal.twig', ['page' => $page, 'item' => $item, 'm_notes' => $module_notes]);
-				$page->js   .= $config->twig->render("items/itm/notes/js.twig", ['page' => $page, 'item' => $item, 'm_notes' => $module_notes, 'session' => $session]);
-
-				$page->search_notesURL = $pages->get('pw_template=msa-noce-ajax')->url;
-				$page->body .= $config->twig->render('msa/noce/ajax/notes-modal.twig', []);
+				$page->body .= $config->twig->render('items/itm/notes/notes.twig', ['page' => $page, 'item' => $item, 'qnotes' => $qnotes, 'user' => $user]);
+				$page->js   .= $config->twig->render("items/itm/notes/js.twig", ['page' => $page, 'item' => $item, 'qnotes' => $qnotes, 'session' => $session]);
 				$page->js   .= $config->twig->render('msa/noce/ajax/js.twig', ['page' => $page]);
 				$session->remove('qnotes_itm');
 			}
-			$page->body .= $config->twig->render("util/ajax-modal.twig");
 			$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
 		} else {
 			$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Error!", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Item ID '$itemID' not found in the Item Master"]);
