01bfa95a89dd12bb625ce46539ad9fe66d035f43
Paul Gomez <pauldro952@hotmail.com>
2020-10-09 10:18:28 -0500

    User Login Redirect after warehouse login

--- site/templates/warehouse-menu.php
+++ site/templates/warehouse-menu.php
@@ -2,7 +2,7 @@
 	if (WhsesessionQuery::create()->sessionExists(session_id())) {
 		include('./dplus-menu.php');
 	} else {
-		$url = $page->get_loginURL();
-		$modules->get('DplusRequest')->self_request($url);
+		$loginm = $modules->get('DplusUser');
+		$loginm->request_login_whse($user->loginid);
 		$session->redirect($page->url, $http301 = false);
 	}
