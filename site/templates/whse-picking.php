a8444d77cc2dd72e090e158f99a731e61e057d4e
Paul Gomez <pauldro952@hotmail.com>
2020-11-03 13:41:46 -0600

    Use $ordn

--- site/templates/whse-picking.php
+++ site/templates/whse-picking.php
@@ -21,7 +21,7 @@
 		$ordn = SalesOrder::get_paddedordernumber($values->text('ordn') ? $values->text('ordn') : $input->get->text('ordn'));
 		$pickingsession = $modules->get('Picking');
 		$pickingsession->set_sessionID(session_id());
-		$pickingsession->set_ordn($values->text('ordn'));
+		$pickingsession->set_ordn($ordn);
 		$pickingsession->handle_action($input);
 		$session->redirect($page->fullURL->getUrl(), $http301 = false);
 	}
