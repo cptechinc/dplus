d304d252ab04b561927e342ff1455f5c76b9f7d9
Paul Gomez <pauldro952@hotmail.com>
2020-11-05 15:14:54 -0600

    Create api-ajax-json-inv.php

---
+++ site/templates/api-ajax-json-inv.php
@@ -0,0 +1,88 @@
+<?php
+	/**
+	 * CI JSON
+	 * This template is made for validating, getting values for Customer
+	 * NOTE: the response values can be formatted to be used by Jquery Validate's remote validation method
+	 */
+	$response   = '';
+	$rm = strtolower($input->requestMethod());
+	$values = $input->$rm;
+	$validate = $modules->get('ValidateIn');
+
+	// NOTE USE WHEN NEEDED FOR JQUERYVALIDATE
+	// $returntype = $values->return ? $values->text('return') : 'jqueryvalidate';
+
+	if ($values->action) {
+		switch ($values->text('action')) {
+			case 'validate-tariffcode':
+				$code = $values->text('code') ? $values->text('code') : $values->text('tariffcode');
+
+				if ($validate->tariffcode($code)) {
+					$response = true;
+				} else {
+					$response = "Tariff Code $code not found";
+				}
+				break;
+			case 'get-tariffcode': // NOT FOR JQUERYVALIDATE
+				$code = $values->text('code');
+
+				if ($validate->tariffcode($code)) {
+					$tariff = TariffCodeQuery::create()->findOneByCode($code);
+					$response = array(
+						'code'        => $code,
+						'number'      => $tariff->number,
+						'rate'        => $tariff->duty_rate,
+						'description' => $tariff->description
+					);
+				} else {
+					$response = false;
+				}
+				break;
+			case 'validate-countrycode':
+				$code = $values->text('code') ? $values->text('code') : $values->text('countrycode');
+
+				if ($validate->countrycode($code)) {
+					$response = true;
+				} else {
+					$response = "Country Code $code not found";
+				}
+				break;
+			case 'get-countrycode':
+				$code = $values->text('code');
+
+				if ($validate->countrycode($code)) {
+					$tariff = CountryCodeQuery::create()->findOneByCode($code);
+					$response = array(
+						'code'        => $code,
+						'description' => $tariff->description
+					);
+				} else {
+					$response = false;
+				}
+				break;
+			case 'validate-msdscode':
+				$code = $values->text('code') ? $values->text('code') : $values->text('msdscode');
+
+				if ($validate->msdscode($code)) {
+					$response = true;
+				} else {
+					$response = "MSDS $code not found";
+				}
+				break;
+			case 'get-msdscode':
+				$code = $values->text('code');
+
+				if ($validate->msdscode($code)) {
+					$msds = $modules->get('CodeTablesMsdsm')->get_code($code);
+					$response = array(
+						'code'        => $code,
+						'description' => $msds->description
+					);
+				} else {
+					$response = false;
+				}
+				break;
+		}
+	}
+
+	$page->body = json_encode($response);
