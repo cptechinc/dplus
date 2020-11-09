0d313c646240f2c9ae66e60d2e0702ba2e816deb
Paul Gomez <pauldro952@hotmail.com>
2020-11-09 14:24:54 -0600

    Itm Misc

--- site/templates/itm-misc.php
+++ site/templates/itm-misc.php
@@ -1,18 +1,16 @@
 <?php
 	$rm = strtolower($input->requestMethod());
 	$values = $input->$rm;
-	$itm = $modules->get('Itm');
-	$recordlocker = $modules->get('RecordLockerUser');
+	$itm = $modules->get('ItmMisc');
 
 	if ($input->get->itemID) {
 		$itemID = $input->get->text('itemID');
 
 		if ($itm->item_exists($itemID)) {
 			if ($values->action) {
-				$rm = strtolower($input->requestMethod());
 				$itm->process_input($input);
-				$itemID = $input->$rm->text('itemID');
-				//$session->redirect($page->itm_costingURL($itemID));
+				$itemID = $values->text('itemID');
+				$session->redirect($page->itm_miscURL($itemID));
 			}
 
 			if ($session->response_itm) {
@@ -27,19 +25,19 @@
 			 *  3. Userid does not match the lock
 			 * Otherwise if not locked, create lock
 			 */
-			if ($recordlocker->function_locked($page->lockcode, $itemID) && !$recordlocker->function_locked_by_user($page->lockcode, $itemID)) {
-				$msg = "ITM Item $itemID is being locked by " . $recordlocker->get_locked_user($page->lockcode, $itemID);
+			if ($itm->recordlocker->function_locked($itemID) && !$itm->recordlocker->function_locked_by_user($itemID)) {
+				$msg = "ITM Item $itemID is being locked by " . $itm->recordlocker->get_locked_user($page->lockcode, $itemID);
 				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "ITM Item $itemID is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
 				$page->body .= $html->div('class=mb-3');
-			} elseif (!$recordlocker->function_locked($page->lockcode, $itemID)) {
-				$recordlocker->create_lock($page->lockcode, $itemID);
+			} elseif (!$itm->recordlocker->function_locked($page->lockcode, $itemID)) {
+				$itm->recordlocker->create_lock($page->lockcode, $itemID);
 			}
 
 			$page->headline = "Misc for $itemID";
 			$item = $itm->get_item($itemID);
 			$page->body .= $config->twig->render('items/itm/itm-links.twig', ['page' => $page, 'page_itm' => $page->parent]);
 			$page->body .= $config->twig->render('items/itm/description.twig', ['page' => $page, 'item' => $item]);
-			$page->body .= $config->twig->render('items/itm/misc/page.twig', ['page' => $page, 'item' => $item, 'recordlocker' => $recordlocker]);
+			$page->body .= $config->twig->render('items/itm/misc/page.twig', ['page' => $page, 'itm' => $itm, 'item' => $item, 'recordlocker' => $itm->recordlocker]);
 			$page->js   .= $config->twig->render('items/itm/misc/js.twig', ['page' => $page]);
 			$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
 		} else {
