fc9b69317f216a2c280694b506fb3df6e6d02c0b
Paul Gomez <pauldro952@hotmail.com>
2020-11-18 10:27:18 -0600

    Use itm-prepend

--- site/templates/itm-costing.php
+++ site/templates/itm-costing.php
@@ -1,52 +1,31 @@
 <?php
-	$rm = strtolower($input->requestMethod());
-	$values = $input->$rm;
-	$itm = $modules->get('Itm');
+	include_once('./itm-prepend.php');
 	$itm_costing = $modules->get('ItmCosting');
 	$itm_costing->init_configs();
-	$recordlocker = $modules->get('RecordLockerUser');
 
-	if ($input->get->itemID) {
-		$itemID = $input->get->text('itemID');
-
-		if ($itm->item_exists($itemID)) {
-			if ($values->action) {
-				$rm = strtolower($input->requestMethod());
-				$itm_costing->process_input($input);
-				$itemID = $input->$rm->text('itemID');
-				$session->redirect($page->itm_costingURL($itemID));
-			}
-
-			if ($session->response_itm) {
-				$page->body .= $config->twig->render('items/itm/response-alert.twig', ['response' => $session->response_itm]);
-				$session->remove('response_itm');
-			}
-
-			/**
-			 * Show alert that warehouse is locked if
-			 *  1. Warehouse isn't new
-			 *  2. The warehouse has a record lock
-			 *  3. Userid does not match the lock
-			 * Otherwise if not locked, create lock
-			 */
-			if ($recordlocker->function_locked($page->lockcode, $itemID) && !$recordlocker->function_locked_by_user($page->lockcode, $itemID)) {
-				$msg = "ITM Item $itemID is being locked by " . $recordlocker->get_locked_user($page->lockcode, $itemID);
-				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "ITM Item $itemID is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
-				$page->body .= $html->div('class=mb-3');
-			} elseif (!$recordlocker->function_locked($page->lockcode, $itemID)) {
-				$recordlocker->create_lock($page->lockcode, $itemID);
-			}
-
-			$page->headline = "Costing for $itemID";
-			$item = $itm->get_item($itemID);
-			$page->body .= $config->twig->render('items/itm/itm-links.twig', ['page' => $page, 'page_itm' => $page->parent]);
-			$page->body .= $config->twig->render('items/itm/description.twig', ['page' => $page, 'item' => $item]);
-			$page->body .= $config->twig->render('items/itm/costing/page.twig', ['page' => $page, 'item' => $item, 'm_costing' => $itm_costing, 'recordlocker' => $recordlocker]);
-		} else {
-			$session->redirect($page->itmURL($itemID), $http301 = false);
-		}
-	} else {
-		$session->redirect($page->itmURL(), $http301 = false);
+	if ($values->action) {
+		$rm = strtolower($input->requestMethod());
+		$itm_costing->process_input($input);
+		$itemID = $input->$rm->text('itemID');
+		$session->redirect($page->itm_costingURL($itemID));
 	}
+
+	/**
+	 * Show alert that Item is locked
+	 */
+	if ($itm->recordlocker->function_locked($itemID) && !$itm->recordlocker->function_locked_by_user($itemID)) {
+		$msg = "ITM Item $itemID is being locked by " . $itm->recordlocker->get_locked_user($itemID);
+		$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "ITM Item $itemID is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
+		$page->body .= $html->div('class=mb-3');
+	} elseif (!$itm->recordlocker->function_locked($itemID)) {
+		$itm->recordlocker->create_lock($itemID);
+	}
+
+	$page->headline = "Costing for $itemID";
+	$item = $itm->get_item($itemID);
+	$page->body .= $config->twig->render('items/itm/itm-links.twig', ['page' => $page, 'page_itm' => $page->parent]);
+	$page->body .= $config->twig->render('items/itm/description.twig', ['page' => $page, 'item' => $item]);
+	$page->body .= $config->twig->render('items/itm/costing/page.twig', ['page' => $page, 'itm' => $itm, 'item' => $item, 'm_costing' => $itm_costing, 'recordlocker' => $itm->recordlocker]);
+	$session->remove('response_itm');
 
 	include __DIR__ . "/basic-page.php";
--- site/templates/itm-pricing.php
+++ site/templates/itm-pricing.php
@@ -1,53 +1,35 @@
 <?php
-	$itm = $modules->get('Itm');
+	include_once('./itm-prepend.php');
+
 	$itm_pricing = $modules->get('ItmPricing');
 	$itm_pricing->init_configs();
-	$recordlocker = $modules->get('RecordLockerUser');
 
-	if ($input->get->itemID) {
-		$itemID = $input->get->text('itemID');
-
-		if ($itm->item_exists($itemID)) {
-			if ($input->requestMethod('POST') || $input->get->action) {
-				$rm = strtolower($input->requestMethod());
-				$itm_pricing->process_input($input);
-				$itemID = $input->$rm->text('itemID');
-				$session->redirect($page->itm_pricingURL($itemID));
-			}
-
-			if ($session->response_itm) {
-				$page->body .= $config->twig->render('items/itm/response-alert.twig', ['response' => $session->response_itm]);
-				$session->remove('response_itm');
-			}
-
-			/**
-			 * Show alert that warehouse is locked if
-			 *  1. Warehouse isn't new
-			 *  2. The warehouse has a record lock
-			 *  3. Userid does not match the lock
-			 * Otherwise if not locked, create lock
-			 */
-			if ($recordlocker->function_locked($page->lockcode, $itemID) && !$recordlocker->function_locked_by_user($page->lockcode, $itemID)) {
-				$msg = "ITM Item $itemID is being locked by " . $recordlocker->get_locked_user($page->lockcode, $itemID);
-				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "ITM Item $itemID is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
-				$page->body .= $html->div('class=mb-3');
-			} elseif (!$recordlocker->function_locked($page->lockcode, $itemID)) {
-				$recordlocker->create_lock($page->lockcode, $itemID);
-			}
-
-			$page->headline = "Pricing for $itemID";
-			$itm_pricing = $modules->get('ItmPricing');
-			$item = $itm->get_item($itemID);
-			$itempricing = $itm_pricing->get_pricing($itemID);
-
-			$page->body .= $config->twig->render('items/itm/itm-links.twig', ['page' => $page, 'page_itm' => $page->parent]);
-			$page->body .= $config->twig->render('items/itm/pricing/form.twig', ['page' => $page, 'item' => $item, 'pricingm' => $itm_pricing, 'item_pricing' => $itempricing]);
-			$page->js   .= $config->twig->render('items/itm/pricing/js.twig', ['page' => $page, 'item_pricing' => $itempricing]);
-		} else {
-			$session->redirect($page->itmURL($itemID));
-		}
-	} else {
-		$session->redirect($page->itmURL());
+	if ($values->action) {
+		$rm = strtolower($input->requestMethod());
+		$itm_pricing->process_input($input);
+		$itemID = $input->$rm->text('itemID');
+		$session->redirect($page->itm_pricingURL($itemID));
 	}
+
+	/**
+	 * Show alert that Item is locked
+	 */
+	if ($itm->recordlocker->function_locked($itemID) && !$itm->recordlocker->function_locked_by_user($itemID)) {
+		$msg = "ITM Item $itemID is being locked by " . $itm->recordlocker->get_locked_user($itemID);
+		$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "ITM Item $itemID is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
+		$page->body .= $html->div('class=mb-3');
+	} elseif (!$itm->recordlocker->function_locked($itemID)) {
+		$itm->recordlocker->create_lock($itemID);
+	}
+
+	$page->headline = "Pricing for $itemID";
+	$itm_pricing = $modules->get('ItmPricing');
+	$item = $itm->get_item($itemID);
+	$itempricing = $itm_pricing->get_pricing($itemID);
+
+	$page->body .= $config->twig->render('items/itm/itm-links.twig', ['page' => $page, 'page_itm' => $page->parent]);
+	$page->body .= $config->twig->render('items/itm/pricing/form.twig', ['page' => $page, 'item' => $item, 'pricingm' => $itm_pricing, 'item_pricing' => $itempricing]);
+	$page->js   .= $config->twig->render('items/itm/pricing/js.twig', ['page' => $page, 'item_pricing' => $itempricing]);
+	$session->remove('response_itm');
 
 	include __DIR__ . "/basic-page.php";
