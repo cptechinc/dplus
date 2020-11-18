837e119048944e1cca01d359649ffba2d3b1844d
Paul Gomez <pauldro952@hotmail.com>
2020-11-18 14:35:31 -0600

    Update upcx.php

--- site/templates/upcx.php
+++ site/templates/upcx.php
@@ -4,8 +4,6 @@
 	$html = $modules->get('HtmlWriter');
 	$upcx = $modules->get('XrefUpc');
 	$filter = $modules->get('FilterXrefItemUpc');
-	$recordlocker = $modules->get('RecordLockerUser');
-
 
 	if ($values->action) {
 		$upcx->process_input($input);
@@ -29,33 +27,29 @@
 
 	if ($input->get->upc) {
 		$code = $input->get->text('upc');
+		$page->headline = "UPCX: UPC $code";
 
-		if ($upcx->upc_exists($code)) {
-			$upc = $upcx->get_upc($code);
-			$page->headline = "UPCX: UPC $code";
-		} else {
-			$upc = new ItemXrefUpc();
-			$upc->setQty(1);
+		$upc = $upcx->get_create_upc($code);
 
-			if ($input->get->itemID) {
-				$itemID = $input->get->text('itemID');
+		if ($input->get->itemID) {
+			$itemID = $input->get->text('itemID');
 
-				if ($upcx->validate_itemID($itemID)) {
-					$page->headline = "Adding UPC X-ref for $itemID";
-					$upc->setItemid($itemID);
-				} else {
-					$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Error!", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Item ID $itemID not found in the Item Master"]);
-					$page->body .= $html->div('class=mb-3');
-				}
-			}
-
-			if ($code == 'new') {
-				$page->headline = "Adding UPC X-ref";
+			if ($upcx->validate_itemID($itemID)) {
+				$page->headline = "Adding UPC X-ref for $itemID";
+				$upc->setItemid($itemID);
 			} else {
-				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Error!", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "UPC $code not found, you may create it below"]);
+				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Error!", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Item ID $itemID not found in the Item Master"]);
 				$page->body .= $html->div('class=mb-3');
 			}
 		}
+
+		if ($code == 'new') {
+			$page->headline = "Adding UPC X-ref";
+		} else {
+			$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Error!", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "UPC $code not found, you may create it below"]);
+			$page->body .= $html->div('class=mb-3');
+		}
+
 
 		if (!$upc->isNew()) {
 			/**
@@ -65,20 +59,20 @@
 			 *  3. Userid does not match the lock
 			 * Otherwise if not locked, create lock
 			 */
-			if ($recordlocker->function_locked($page->name, $code) && !$recordlocker->function_locked_by_user($page->name, $code)) {
-				$msg = "UPC $code is being locked by " . $recordlocker->get_locked_user($page->name, $code);
+			if ($upcx->recordlocker->function_locked($code) && !$upcx->recordlocker->function_locked_by_user($code)) {
+				$msg = "UPC $code is being locked by " . $upcx->recordlocker->get_locked_user($code);
 				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "UPC $code is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
 				$page->body .= $html->div('class=mb-3');
-			} elseif (!$recordlocker->function_locked($page->name, $code)) {
-				$recordlocker->create_lock($page->name, $code);
+			} elseif (!$upcx->recordlocker->function_locked($code)) {
+				$upcx->recordlocker->create_lock($code);
 			}
 		}
 
-		$page->body .= $config->twig->render('items/upcx/form.twig', ['page' => $page, 'upcx' => $upcx, 'upc' => $upc, 'recordlocker' => $recordlocker]);
+		$page->body .= $config->twig->render('items/upcx/form.twig', ['page' => $page, 'upcx' => $upcx, 'upc' => $upc, 'recordlocker' => $upcx->recordlocker]);
 		$page->js   .= $config->twig->render('items/upcx/js.twig', ['page' => $page, 'upc' => $upc]);
 	} else {
 		$filter = $modules->get('FilterXrefItemUpc');
-		$recordlocker->remove_lock($page->name);
+		$upcx->recordlocker->remove_lock($page->name);
 		$itemID = strtoupper($input->get->text('itemID'));
 		$filter->filter_query($input);
 		$filter->apply_sortby($page);
@@ -91,7 +85,7 @@
 		}
 
 		$page->body .= $config->twig->render('items/upcx/upc-filters.twig', ['page' => $page, 'input' => $input]);
-		$page->body .= $config->twig->render('items/upcx/upc-list.twig', ['page' => $page, 'upcs' => $upcs, 'itemID' => $itemID, 'recordlocker' => $recordlocker]);
+		$page->body .= $config->twig->render('items/upcx/upc-list.twig', ['page' => $page, 'upcx' => $upcx, 'upcs' => $upcs, 'itemID' => $itemID]);
 		$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $upcs->getNbResults()]);
 	}
 
