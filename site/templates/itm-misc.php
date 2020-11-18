004a445f2b13ffaccf4129b9188208991061f996
Paul Gomez <pauldro952@hotmail.com>
2020-11-18 14:36:34 -0600

    Use item-prepend

--- site/templates/itm-misc.php
+++ site/templates/itm-misc.php
@@ -1,54 +1,39 @@
 <?php
-	$rm = strtolower($input->requestMethod());
-	$values = $input->$rm;
+	include_once('./itm-prepend.php');
 	$itm = $modules->get('ItmMisc');
 	$itm->init_configs();
 
-	if ($input->get->itemID) {
-		$itemID = $input->get->text('itemID');
+	if ($values->action) {
+		$itm->process_input($input);
+		$itemID = $values->text('itemID');
+		$session->redirect($page->itm_miscURL($itemID));
+	}
+	/**
+	 * Show alert that ITM Item is locked if
+	 *  1. ITM Item  isn't new
+	 *  2. The ITM Item  has a record lock
+	 *  3. Userid does not match the lock
+	 * Otherwise if not locked, create lock
+	 */
+	if ($itm->recordlocker->function_locked($itemID) && !$itm->recordlocker->function_locked_by_user($itemID)) {
+		$msg = "ITM Item $itemID is being locked by " . $itm->recordlocker->get_locked_user($itemID);
+		$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "ITM Item $itemID is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
+		$page->body .= $html->div('class=mb-3');
+	} elseif (!$itm->recordlocker->function_locked($itemID)) {
+		$itm->recordlocker->create_lock($itemID);
+	}
 
-		if ($itm->item_exists($itemID)) {
-			if ($values->action) {
-				$itm->process_input($input);
-				$itemID = $values->text('itemID');
-				$session->redirect($page->itm_miscURL($itemID));
-			}
+	$page->headline = "Misc for $itemID";
+	$item = $itm->get_item($itemID);
+	$hazmat = $item->has_hazmat() ? $item->get_hazmat() : new InvHazmatItem();
+	$page->body .= $config->twig->render('items/itm/itm-links.twig', ['page' => $page, 'page_itm' => $page->parent]);
+	$page->body .= $config->twig->render('items/itm/description.twig', ['page' => $page, 'item' => $item]);
+	$page->body .= $config->twig->render('items/itm/misc/page.twig', ['page' => $page, 'itm' => $itm, 'item' => $item, 'recordlocker' => $itm->recordlocker]);
+	$page->js   .= $config->twig->render('items/itm/misc/js.twig', ['page' => $page, 'itm' => $itm]);
+	$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
 
-			if ($session->response_itm) {
-				$page->body .= $config->twig->render('items/itm/response-alert.twig', ['response' => $session->response_itm]);
-			}
-
-			/**
-			 * Show alert that warehouse is locked if
-			 *  1. Warehouse isn't new
-			 *  2. The warehouse has a record lock
-			 *  3. Userid does not match the lock
-			 * Otherwise if not locked, create lock
-			 */
-			if ($itm->recordlocker->function_locked($itemID) && !$itm->recordlocker->function_locked_by_user($itemID)) {
-				$msg = "ITM Item $itemID is being locked by " . $itm->recordlocker->get_locked_user($itemID);
-				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "ITM Item $itemID is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
-				$page->body .= $html->div('class=mb-3');
-			} elseif (!$itm->recordlocker->function_locked($itemID)) {
-				$itm->recordlocker->create_lock($itemID);
-			}
-
-			$page->headline = "Misc for $itemID";
-			$item = $itm->get_item($itemID);
-			$hazmat = $item->has_hazmat() ? $item->get_hazmat() : new InvHazmatItem();
-			$page->body .= $config->twig->render('items/itm/itm-links.twig', ['page' => $page, 'page_itm' => $page->parent]);
-			$page->body .= $config->twig->render('items/itm/description.twig', ['page' => $page, 'item' => $item]);
-			$page->body .= $config->twig->render('items/itm/misc/page.twig', ['page' => $page, 'itm' => $itm, 'item' => $item, 'recordlocker' => $itm->recordlocker]);
-			$page->js   .= $config->twig->render('items/itm/misc/js.twig', ['page' => $page, 'itm' => $itm]);
-			$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
-			if ($session->response_itm) {
-				$session->remove('response_itm');
-			}
-		} else {
-			$session->redirect($page->itmURL($itemID), $http301 = false);
-		}
-	} else {
-		$session->redirect($page->itmURL(), $http301 = false);
+	if ($session->response_itm) {
+		$session->remove('response_itm');
 	}
 
 	include __DIR__ . "/basic-page.php";
--- site/templates/itm-xrefs-upcx.php
+++ site/templates/itm-xrefs-upcx.php
@@ -1,92 +1,73 @@
 <?php
-	$rm = strtolower($input->requestMethod());
-	$values = $input->$rm;
-	$itm = $modules->get('Itm');
-	$recordlocker = $modules->get('RecordLockerUser');
+	include_once('./itm-prepend.php');
 	$upcx = $modules->get('XrefUpc');
 	$validate = $modules->get('ValidateUpcx');
-
 	$html = $modules->get('HtmlWriter');
 
 	if ($values->action) {
 		$upcx->process_input($input);
 		$rm = strtolower($input->requestMethod());
 		$values = $input->$rm;
-		$code = $values->text('action') == 'remove-upcx' ? '' : $values->text('upc');
+		$code = $values->text('action') == 'delete-upcx' ? '' : $values->text('upc');
 		$session->redirect($page->upcURL($code));
 	}
-
-	$page->show_breadcrumbs = false;
-	$page->body .= $config->twig->render('items/itm/bread-crumbs.twig', ['page' => $page, 'page_itm' => $page->parent, 'input' => $input]);
 
 	if ($session->response_xref) {
 		$page->body .= $config->twig->render('items/itm/response-alert.twig', ['response' => $session->response_xref]);
 		$session->remove('response_xref');
 	}
 
-	if ($input->get->itemID) {
-		$itemID = strtoupper($input->get->text('itemID'));
+	$item = $itm->get_item($itemID);
+	if ($input->get->upc) {
+		$code = $input->get->text('upc');
 
-		if ($validate->itemid($itemID)) {
-			$item = $itm->get_item($itemID);
-
-			if ($input->get->upc) {
-				$code = $input->get->text('upc');
-
-				if ($upcx->upc_exists($code)) {
-					$upc = $upcx->get_upc($code);
-					$page->title = "ITM: $itemID UPC $code";
-				} else {
-					$upc = new ItemXrefUpc();
-
-					if ($input->get->itemID) {
-						if ($validate->itemid($itemID)) {
-							$page->title = "Adding UPC X-ref for $itemID";
-							$upc->setItemid($itemID);
-						} else {
-							$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Error!", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Item ID $itemID not found in the Item Master"]);
-							$page->body .= $html->div('class=mb-3');
-						}
-					}
-				}
-
-				if (!$upc->isNew()) {
-					/**
-					 * Show alert that UPC is locked if
-					 * NOTE $page->lockcode is defined in Itm.module
-					 *  1. UPC isn't new
-					 *  2. The UPC has a record lock
-					 *  3. Userid does not match the lock
-					 * Otherwise if not locked, create lock
-					 */
-					if ($recordlocker->function_locked($page->lockcode, $code) && !$recordlocker->function_locked_by_user($page->lockcode, $code)) {
-						$msg = "UPC $code is being locked by " . $recordlocker->get_locked_user($page->lockcode, $code);
-						$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "UPC $code is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
-						$page->body .= $html->div('class=mb-3');
-					} elseif (!$recordlocker->function_locked($page->lockcode, $code)) {
-						$recordlocker->create_lock($page->lockcode, $code);
-					}
-				}
-
-				$page->body .= $config->twig->render('items/itm/xrefs/upcx/form.twig', ['page' => $page, 'upcx' => $upcx, 'upc' => $upc, 'recordlocker' => $recordlocker]);
-				$page->js .= $config->twig->render('items/upcx/js.twig', ['page' => $page, 'upc' => $upc]);
-				$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
-			} else {
-				$filter = $modules->get('FilterXrefItemUpc');
-				$recordlocker->remove_lock($page->lockcode);
-				$filter->filter_query($input);
-				$filter->apply_sortby($page);
-				$upcs = $filter->query->paginate($input->pageNum, 10);
-
-				$page->title = "ITM: UPCs for $itemID";
-				$page->body .= $config->twig->render('items/itm/xrefs/upcx/list.twig', ['page' => $page, 'upcs' => $upcs, 'itemID' => $itemID, 'recordlocker' => $recordlocker]);
-				$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $upcs->getNbResults()]);
-			}
+		if ($upcx->upc_exists($code)) {
+			$upc = $upcx->get_upc($code);
+			$page->title = "ITM: $itemID UPC $code";
 		} else {
-			$session->redirect($page->itmURL($itemID), $http301 = false);
-		}
-	} else {
-		$session->redirect($page->itmURL(), $http301 = false);
-	}
+			$upc = new ItemXrefUpc();
 
+			if ($input->get->itemID) {
+				if ($validate->itemid($itemID)) {
+					$page->title = "Adding UPC X-ref for $itemID";
+					$upc->setItemid($itemID);
+				} else {
+					$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "Error!", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Item ID $itemID not found in the Item Master"]);
+					$page->body .= $html->div('class=mb-3');
+				}
+			}
+		}
+
+		if (!$upc->isNew()) {
+			/**
+			 * Show alert that UPC is locked if
+			 * NOTE $page->lockcode is defined in Itm.module
+			 *  1. UPC isn't new
+			 *  2. The UPC has a record lock
+			 *  3. Userid does not match the lock
+			 * Otherwise if not locked, create lock
+			 */
+			if ($upcx->recordlocker->function_locked($code) && !$upcx->recordlocker->function_locked_by_user($code)) {
+				$msg = "UPC $code is being locked by " . $upcx->recordlocker->get_locked_user($code);
+				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "UPC $code is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
+				$page->body .= $html->div('class=mb-3');
+			} elseif (!$upcx->recordlocker->function_locked($code)) {
+				$upcx->recordlocker->create_lock($code);
+			}
+		}
+
+		$page->body .= $config->twig->render('items/itm/xrefs/upcx/form.twig', ['page' => $page, 'upcx' => $upcx, 'upc' => $upc]);
+		$page->js .= $config->twig->render('items/upcx/js.twig', ['page' => $page, 'upc' => $upc]);
+		$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
+	} else {
+		$filter = $modules->get('FilterXrefItemUpc');
+		$upcx->recordlocker->remove_lock($page->lockcode);
+		$filter->filter_query($input);
+		$filter->apply_sortby($page);
+		$upcs = $filter->query->paginate($input->pageNum, 10);
+
+		$page->title = "ITM: UPCs for $itemID";
+		$page->body .= $config->twig->render('items/itm/xrefs/upcx/list.twig', ['page' => $page, 'upcs' => $upcs, 'itemID' => $itemID, 'upcx' => $upcx]);
+		$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $upcs->getNbResults()]);
+	}
 	include __DIR__ . "/basic-page.php";
