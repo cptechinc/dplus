4af967f104f02993c6218b12cfa720d06c7ad550
Paul Gomez <pauldro952@hotmail.com>
2020-11-23 10:24:04 -0600

    Update purchase-orders.php

--- site/templates/purchase-orders.php
+++ site/templates/purchase-orders.php
@@ -12,4 +12,5 @@
 	$page->body .= $html->h3('', $orders->getNbResults() . " Purchase Orders");
 	$page->body .= $config->twig->render('purchase-orders/purchase-orders-list-links.twig', ['page' => $page, 'config' => $config, 'purchaseorders' => $orders, 'orderpage' => $pages->get('pw_template=purchase-order-view')->url]);
 	$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'pagenbr' => $input->pageNum, 'resultscount'=> $orders->getNbResults()]);
+	$page->js   .= $config->twig->render('purchase-orders/list.js.twig');
 	include __DIR__ . "/basic-page.php";
