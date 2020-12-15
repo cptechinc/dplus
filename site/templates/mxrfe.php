9d34131a163508e4ae104329871c9a8cf74b17d1
Paul Gomez <pauldro952@hotmail.com>
2020-12-15 10:22:59 -0600

    Update mxrfe.php
    Lock MXRFE X-ref

--- site/templates/mxrfe.php
+++ site/templates/mxrfe.php
@@ -6,15 +6,34 @@ $values = $input->$rm;
 $filter = $modules->get('FilterXrefItemMxrfe');
 $mxrfe  = $modules->get('XrefMxrfe');
 
-
 $page->show_breadcrumbs = false;
 $page->body .= $config->twig->render('items/mxrfe/bread-crumbs.twig', ['page' => $page]);
 
 if ($values->vendorID) {
 	$vendorID = $values->text('vendorID');
 	$vendor = $mxrfe->vendor($vendorID);
-	
-	if ($values->vendoritemID) {
+
+	if ($values->itemID) {
+		$vendoritemID = $values->text('vendoritemID');
+		$xrefID = $values->text('itemID');
+		$xref = $mxrfe->get_create_xref($vendorID, $vendoritemID, $xrefID);
+		
+		if (!$xref->isNew()) {
+			/**
+			 * Show alert that MXRFE is locked if
+			 *  1. MXRFE isn't new
+			 *  2. The MXRFE has a record lock
+			 *  3. Userid does not match the lock
+			 * Otherwise if not locked, create lock
+			 */
+			 if (!$mxrfe->lockrecord($xref)) {
+				$msg = "MXRFE ". $mxrfe->get_recordlocker_key($xref) ." is being locked by " . $mxrfe->recordlocker->get_locked_user($mxrfe->get_recordlocker_key($xref));
+				$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "MXRFE ".$mxrfe->get_recordlocker_key($xref)." is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
+				$page->body .= $html->div('class=mb-3');
+			}
+		}
+
+		$page->body .= $config->twig->render('items/mxrfe/item/form/display.twig', ['page' => $page, 'mxrfe' => $mxrfe, 'vendor' => $vendor, 'xref' => $xref]);
 
 	} else {
 		$filter->vendorid($vendorID);
