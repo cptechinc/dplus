e9e1a9e0db945ebf6c85b2e6864e80603303c5de
Paul Gomez <pauldro952@hotmail.com>
2020-12-28 14:49:14 -0600

    cio for ci permissions

--- site/templates/ci-function.php
+++ site/templates/ci-function.php
@@ -1,2 +1,9 @@
 <?php
-	include('./dplus-function.php');
+	$cio = $modules->get('Cio');
+
+	if ($user->has_function('ci') && $cio->is_user_allowed_template($user, $page->pw_template)) {
+		include("./dplus-function.php");
+	} else {
+		$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "You don't have access to this function", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Permission: Ci $page->name"]);
+		include __DIR__ . "/basic-page.php";
+	}
--- site/templates/ii-function.php
+++ site/templates/ii-function.php
@@ -4,6 +4,6 @@
 	if ($user->has_function('ii') && $iio->is_user_allowed_template($user, $page->pw_template)) {
 		include("./dplus-function.php");
 	} else {
-		$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "You don't have access to this function", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Permission: ITM $page->name"]);
+		$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "You don't have access to this function", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Permission: II $page->name"]);
 		include __DIR__ . "/basic-page.php";
 	}
