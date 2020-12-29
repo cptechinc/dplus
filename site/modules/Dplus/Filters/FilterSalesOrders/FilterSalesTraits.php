b356a911d53bcc4201f7596fee3357db7b5452b8
Paul Gomez <pauldro952@hotmail.com>
2020-12-29 09:17:18 -0600

    Add Request Date search filter

--- site/modules/Dplus/Filters/FilterSalesOrders/FilterSalesTraits.php
+++ site/modules/Dplus/Filters/FilterSalesOrders/FilterSalesTraits.php
@@ -5,120 +5,240 @@ use Propel\Runtime\ActiveQuery\Criteria;
 use SalesOrderQuery, SalesOrder;
 
 trait FilterSalesTraits {
+/* =============================================================
+	Input Functions
+============================================================= */
 	/**
-	 * Filters Query by Order Number
-	 *
-	 * @param  WireInput $input Object that Contains the $_GET array for values to filter on
-	 * @return void
+	 * Filter the Query on the Customer PO column
+	 * @param  WireInput $input
+	 * @return self
 	 */
-	public function filter_ordernumber(WireInput $input) {
-		if ($input->get->text('ordernumber_from')) {
-			$from = SalesOrder::get_paddedordernumber($input->get->text('ordernumber_from'));
-			$this->query->filterByOrdernumber($from, Criteria::GREATER_EQUAL);
-		}
-		if ($input->get->text('ordernumber_through')) {
-			$through = SalesOrder::get_paddedordernumber($input->get->text('ordernumber_through'));
-			$this->query->filterByOrdernumber($through, Criteria::LESS_EQUAL);
-		}
+	public function custpo_input(WireInput $input) {
+		$rm = strtolower($input->requestMethod());
+		$values = $input->$rm;
+		$custpo = $values->text('custpo');
+		$this->custpo($custpo);
+		return $this;
 	}
 
 	/**
-	 * Filters Query by Customer PO
+	 * Filter the Query by the Order Number column
 	 *
-	 * @param  WireInput $input Object that Contains the $_GET array for values to filter on
-	 * @return void
+	 * @param  WireInput $input
+	 * @return self
 	 */
-	public function filter_custpo(WireInput $input) {
-		if ($input->get->text('custpo')) {
-			$custpo = $input->get->text('custpo');
-			$this->query->filterByCustpo("%$custpo%", Criteria::LIKE);
+	public function ordernumber_input(WireInput $input) {
+		$rm = strtolower($input->requestMethod());
+		$values = $input->$rm;
+
+		if ($values->text('ordernumber_from')) {
+			$this->ordernumber($values->text('ordernumber_from'), Criteria::GREATER_EQUAL);
 		}
+
+		if ($values->text('ordernumber_through')) {
+			$this->ordernumber($values->text('ordernumber_through'), Criteria::LESS_EQUAL);
+		}
+		return $this;
 	}
 
 	/**
-	 * Filters Query by Order Date
-	 *
-	 * @param  WireInput $input Object that Contains the $_GET array for values to filter on
-	 * @return void
+	 * Filter the Query on the Order Date column
+	 * @param  WireInput $input
+	 * @return self
 	 */
-	public function filter_orderdate(WireInput $input) {
-		if ($input->get->text('orderdate_from') || $input->get->text('orderdate_through')) {
-			$orderdate_from = date("Ymd", strtotime($input->get->text('orderdate_from')));
+	public function orderdate_input(WireInput $input) {
+		$rm = strtolower($input->requestMethod());
+		$values = $input->$rm;
 
-			if (empty($input->get->text('orderdate_through'))) {
+		if ($values->text('orderdate_from') || $values->text('orderdate_through')) {
+			$orderdate_from = date("Ymd", strtotime($values->text('orderdate_from')));
+
+			if (empty($values->text('orderdate_through'))) {
 				$orderdate_through = date('Ymd');
 			} else {
-				$orderdate_through = date("Ymd", strtotime($input->get->text('orderdate_through')));
+				$orderdate_through = date("Ymd", strtotime($values->text('orderdate_through')));
 			}
 
 			if ($orderdate_from) {
-				$this->query->filterByOrderdate($orderdate_from, Criteria::GREATER_EQUAL);
+				$this->orderdate($orderdate_from, Criteria::GREATER_EQUAL);
 			}
 
 			if ($orderdate_through) {
-				$this->query->filterByOrderdate($orderdate_through, Criteria::LESS_EQUAL);
+				$this->orderdate($orderdate_through, Criteria::LESS_EQUAL);
 			}
 		}
+		return $this;
 	}
 
 	/**
-	 * Filters Query by Order Date
-	 *
-	 * @param  WireInput $input Object that Contains the $_GET array for values to filter on
-	 * @return void
+	 * Filter the Query on the Request Date column
+	 * @param  WireInput $input
+	 * @return self
 	 */
-	public function filter_ordertotal(WireInput $input) {
-		if ($input->get->text('order_total_from')) {
-			$this->query->filterByOrdertotal($input->get->text('order_total_from'), Criteria::GREATER_EQUAL);
-		}
+	public function requestdate_input(WireInput $input) {
+		$rm = strtolower($input->requestMethod());
+		$values = $input->$rm;
 
-		if ($input->get->text('order_total_through')) {
-			$this->query->filterByOrdertotal($input->get->text('order_total_through'), Criteria::LESS_EQUAL);
-		}
-	}
+		if ($values->text('requestdate_from') || $values->text('requestdate_through')) {
+			$requestdate_from = date("Ymd", strtotime($values->text('requestdate_from')));
 
-	/**
-	 * Filters Query by Customer ID
-	 *
-	 * @param  WireInput $input Object that Contains the $_GET array for values to filter on
-	 * @return void
-	 */
-	public function filter_custid($input) {
-		if ($input->get->custID) {
-			if (is_array($input->get->custID)) {
-				$filter = $input->get->array('custID');
+			if (empty($values->text('requestdate_through'))) {
+				$requestdate_through = date('Ymd');
 			} else {
-				$filter = array($input->get->text('custID'));
+				$requestdate_through = date("Ymd", strtotime($values->text('requestdate_through')));
 			}
 
-			if (sizeof($filter) == 2) {
-				if (!empty($filter[0])) {
-					$this->query->filterByCustid($filter[0], Criteria::GREATER_EQUAL);
+			if ($requestdate_from) {
+				$this->requestdate($requestdate_from, Criteria::GREATER_EQUAL);
+			}
+
+			if ($requestdate_through) {
+				$this->requestdate($requestdate_through, Criteria::LESS_EQUAL);
+			}
+		}
+		return $this;
+	}
+
+	/**
+	 * Filter the Query on the Order total column
+	 * @param  WireInput $input
+	 * @return self
+	 */
+	public function ordertotal_input(WireInput $input) {
+		$rm = strtolower($input->requestMethod());
+		$values = $input->$rm;
+
+		if ($values->text('order_total_from')) {
+			$this->ordertotal($values->text('order_total_from'), Criteria::GREATER_EQUAL);
+		}
+
+		if ($values->text('order_total_through')) {
+			$this->ordertotal($values->text('order_total_through'), Criteria::LESS_EQUAL);
+		}
+		return $this;
+	}
+
+	/**
+	 * Filter the Query on the Customer ID column
+	 * @param  WireInput $input
+	 * @return self
+	 */
+	public function custid_input($input) {
+		$rm = strtolower($input->requestMethod());
+		$values = $input->$rm;
+
+		if ($values->custID) {
+			$custIDs = is_array($values->custID) ? $values->array('custID') : array($values->text('custID'));
+
+			if (sizeof($custIDs) == 2) {
+				if (!empty($custIDs[0])) {
+					$this->custid($custIDs[0], Criteria::GREATER_EQUAL);
 				}
 
 				if (!empty($filter[1])) {
-					$this->query->filterByCustid($filter[1], Criteria::LESS_EQUAL);
+					$this->custid($custIDs[1], Criteria::LESS_EQUAL);
 				}
 			} else {
-				$this->query->filterByCustid($filter);
+				$this->custid($custIDs);
 			}
 		}
+		return $this;
 	}
 
 	/**
-	 * Filters Query by Customer ShiptoID
-	 *
-	 * @param  WireInput $input Object that Contains the $_GET array for values to filter on
+	 * Filter the Query by the Customer Shipto column
+	 * @param  WireInput $input
+	 */
+	public function shiptoid_input($input) {
+		$rm = strtolower($input->requestMethod());
+		$values = $input->$rm;
+
+		if ($values->custID && $values->shiptoID) {
+			$shiptoID = is_array($values->shiptoID) ? $values->array('shiptoID') : $values->text('shiptoID');
+			$this->shiptoid($shiptoID);
+		}
+	}
+
+/* =============================================================
+	Base Filter Functions
+============================================================= */
+	/**
+	 * Filter the Query on the Order Number column
+	 * @param  string $ordn       Order Number
+	 * @param  string $comparison
+	 * @return self
+	 */
+	public function ordernumber($ordn, $comparison = null) {
+		$ordn = SalesOrder::get_paddedordernumber($ordn);
+		$this->query->filterByOrdernumber($ordn, $comparison);
+		return $this;
+	}
+
+	/**
+	 * Filter the Query on the Customer PO column
+	 * @param  string $custpo
+	 * @return self
+	 */
+	public function custpo($custpo = '') {
+		if ($custpo) {
+			$this->query->filterByCustpo("%$custpo%", Criteria::LIKE);
+		}
+		return $this;
+	}
+
+	/**
+	 * Filter the Query on the Order Date column
+	 * @param  string $date       Order Date
+	 * @param  string $comparison Criteria Comparasion
 	 * @return void
 	 */
-	public function filter_shiptoid($input) {
-		if ($input->get->shiptoID && $input->get->custID) {
-			if (is_array($input->get->shiptoID)) {
-				$filter = $input->get->array('shiptoID');
-			} else {
-				$filter = $input->get->text('shiptoID');
-			}
-			$this->query->filterByShiptoid($filter);
+	public function orderdate($date, $comparison = null) {
+		$this->query->filterByOrderdate($date, $comparison);
+	}
+	/**
+	 * Filter the Query on the Request Date column
+	 * @param  string $date       Request Date
+	 * @param  string $comparison Criteria Comparasion
+	 * @return void
+	 */
+	public function requestdate($date, $comparison = null) {
+		$this->query->filterByRequestdate($date, $comparison);
+	}
+
+	/**
+	 * Filter The Query on the Order Total Column
+	 * @param  float  $total       Order Total
+	 * @param  string $comparison
+	 * @return self
+	 */
+	public function ordertotal($total, $comparison = null) {
+		$this->query->filterByOrdertotal($total, $comparison);
+		return $this;
+	}
+
+	/**
+	 * Filter the Query on the Customer ID column
+	 * @param  string $custID      Customer ID
+	 * @param  string $comparison
+	 * @return self
+	 */
+	public function custid($custID, $comparison = null) {
+		if ($custID)  {
+			$this->query->filterByCustid($custID, $comparison);
 		}
+		return $this;
+	}
+
+	/**
+	 * Filter the Query on the Customer Shipto ID column
+	 * @param  string $shiptoID      Customer Shipto ID
+	 * @param  string $comparison
+	 * @return self
+	 */
+	public function shiptoid($shiptoID, $comparison = null) {
+		if ($shiptoID)  {
+			$this->query->filterByShiptoid($shiptoID, $comparison);
+		}
+		return $this;
 	}
 }
