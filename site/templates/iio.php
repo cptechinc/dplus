a5c069dc32240ec2ef7fd3ef7de05b0792fb5337
Paul Gomez <pauldro952@hotmail.com>
2021-01-05 13:28:14 -0600

    Update IIo template, show record lock error

--- site/templates/iio.php
+++ site/templates/iio.php
@@ -13,6 +13,23 @@
 
 	$iiuser = $iio->get_create($values->text('userID'));
 
+	if (!$iiuser->isNew()) {
+		/**
+		 * Show alert that CXM is locked if
+		 *  1. CXM isn't new
+		 *  2. The CXM has a record lock
+		 *  3. Userid does not match the lock
+		 * Otherwise if not locked, create lock
+		 */
+		 if (!$iio->lockrecord($iiuser->userid)) {
+			$msg = "IIO $iiuser->userid is being locked by " . $iio->recordlocker->get_locked_user($iiuser->userid);
+			$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "IIO $iiuser->userid is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
+			$page->body .= $html->div('class=mb-3');
+		}
+	} else {
+		$iio->recordlocker->remove_lock();
+	}
+
 	$page->body .= $config->twig->render('mii/iio/page.twig', ['page' => $page, 'iio' => $iio, 'user' => $iiuser]);
 	// $page->js   .= $config->twig->render('mii/iio/js.twig', ['page' => $page, 'iio' => $iio]);
 	$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
