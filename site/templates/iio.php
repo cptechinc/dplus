8382aac2b903db64f4490c0cae0ead52852e6355
Paul Gomez <pauldro952@hotmail.com>
2021-01-05 13:28:43 -0600

    Add JS to iio

--- site/templates/iio.php
+++ site/templates/iio.php
@@ -5,7 +5,6 @@
 
 	if ($values->action) {
 		$iio->process_input($input);
-		// $session->redirect($page->iioURL($values->text('loginID')), $http301 = false);
 		$session->sql = $db_dplusdata->getLastExecutedQuery();
 		$session->redirect($page->iioURL($values->text('userID')), $http301 = false);
 	}
@@ -34,6 +33,6 @@
 	}
 
 	$page->body .= $config->twig->render('mii/iio/page.twig', ['page' => $page, 'iio' => $iio, 'user' => $iiuser]);
-	// $page->js   .= $config->twig->render('mii/iio/js.twig', ['page' => $page, 'iio' => $iio]);
+	$page->js   .= $config->twig->render('mii/iio/js.twig', ['page' => $page, 'iio' => $iio]);
 	$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
 	include __DIR__ . "/basic-page.php";
