88582d85c2c547a8c3f9f91c61e925e1009be132
Paul Gomez <pauldro952@outlook.com>
2021-01-25 10:45:10 -0600

    CI Sales Orders

---
+++ site/modules/Dplus/ScreenFormatter/src/ci/SalesOrders.php
@@ -0,0 +1,20 @@
+<?php namespace Dplus\ScreenFormatters\Ci;
+
+use Dplus\ScreenFormatters\ScreenFormatter;
+
+class SalesOrders extends ScreenFormatter {
+	protected $code = 'ci-sales-orders';
+
+	/**
+	 * Key Value array of Sections that exist I.E. header => Header, detail => Detail
+	 * @var string
+	 */
+	protected $datasections = array(
+		"header" => "Header",
+		"detail" => "Detail",
+		"itemstatus" => "Item Status",
+		"purchaseorder" => "Purchase Order",
+		"total" => "Total",
+		"shipments" => "Shipments"
+	);
+}
