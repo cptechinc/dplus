c6ae179b73626e733c9e4a8508794477b4a028ca
Paul Gomez <pauldro952@outlook.com>
2021-01-25 11:21:27 -0600

    II Item, Stock

---
+++ site/modules/Dplus/ScreenFormatters/src/ii/Stock.php
@@ -0,0 +1,15 @@
+<?php namespace Dplus\ScreenFormatters\Ii;
+
+use Dplus\ScreenFormatters\ScreenFormatter;
+
+class Stock extends ScreenFormatter {
+	protected $code = 'ii-stock';
+
+	/**
+	 * Key Value array of Sections that exist I.E. header => Header, detail => Detail
+	 * @var string
+	 */
+	protected $datasections = array(
+		"detail" => "Detail"
+	);
+}
