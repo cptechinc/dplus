a7dae0053a30008e4ae71a7df380c4888a098ba9
Paul Gomez <pauldro952@outlook.com>
2021-01-25 09:54:04 -0600

    Composer Init

---
+++ site/modules/Dplus/ScreenFormatter/src/ci/OpenInvoices.php
@@ -0,0 +1,18 @@
+<?php namespace Dplus\ScreenFormatters\Ci;
+
+use Dplus\ScreenFormatters\ScreenFormatter;
+
+class PurchaseOrders extends ScreenFormatter {
+
+	protected $code = 'ci-customer-po';
+
+	/**
+	 * Key Value array of Sections that exist I.E. header => Header, detail => Detail
+	 * @var string
+	 */
+	protected $datasections = array(
+		'header'  => 'Header',
+		"details" => "Details",
+		"totals"  => "Totals"
+	);
+}
