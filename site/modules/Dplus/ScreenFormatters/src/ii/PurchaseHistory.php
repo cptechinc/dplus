aa52bb552fe16a07a1a8db8447772c77efc636be
Paul Gomez <pauldro952@outlook.com>
2021-01-25 11:48:29 -0600

    II Purchase History

---
+++ site/modules/Dplus/ScreenFormatters/src/ii/PurchaseHistory.php
@@ -0,0 +1,16 @@
+<?php namespace Dplus\ScreenFormatters\Ii;
+
+use Dplus\ScreenFormatters\ScreenFormatter;
+
+class PurchaseHistory extends ScreenFormatter {
+	protected $code = 'ii-purchase-history';
+
+	/**
+	 * Key Value array of Sections that exist I.E. header => Header, detail => Detail
+	 * @var string
+	 */
+	protected $datasections = array(
+		"detail"    => "Detail",
+		"lotserial" => "Lot / Serial"
+	);
+}
--- site/modules/Dplus/ScreenFormatters/vendor/composer/autoload_classmap.php
+++ site/modules/Dplus/ScreenFormatters/vendor/composer/autoload_classmap.php
@@ -14,6 +14,7 @@ return array(
     'Dplus\\ScreenFormatters\\Ci\\SalesOrders' => $baseDir . '/src/ci/SalesOrders.php',
     'Dplus\\ScreenFormatters\\Ii\\Item' => $baseDir . '/src/ii/Item.php',
     'Dplus\\ScreenFormatters\\Ii\\Lotserial' => $baseDir . '/src/ii/Lotserial.php',
+    'Dplus\\ScreenFormatters\\Ii\\PurchaseHistory' => $baseDir . '/src/ii/PurchaseHistory.php',
     'Dplus\\ScreenFormatters\\Ii\\Quotes' => $baseDir . '/src/ii/Quotes.php',
     'Dplus\\ScreenFormatters\\Ii\\Stock' => $baseDir . '/src/ii/Stock.php',
     'Dplus\\ScreenFormatters\\ScreenFormatter' => $baseDir . '/src/ScreenFormatter.php',
