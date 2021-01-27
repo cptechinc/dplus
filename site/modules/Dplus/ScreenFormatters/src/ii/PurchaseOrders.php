2ce93f270e4873626c1b07247ba0e074a0ac80ec
Paul Gomez <pauldro952@outlook.com>
2021-01-27 13:58:44 -0600

    Update PurchaseOrders.php

--- site/modules/Dplus/ScreenFormatters/src/ii/PurchaseOrders.php
+++ site/modules/Dplus/ScreenFormatters/src/ii/PurchaseOrders.php
@@ -4,8 +4,8 @@ use Dplus\ScreenFormatters\ScreenFormatter;
 
 class PurchaseOrders extends ScreenFormatter {
 	const URI = 'ii:purchase-orders';
-	
-	protected $code = 'ii-purchase-history';
+
+	protected $code = 'ii-purchase-orders';
 
 	/**
 	 * Key Value array of Sections that exist I.E. header => Header, detail => Detail
