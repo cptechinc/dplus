92f3263094254c6f98daf03fc478796c3451872e
Paul Gomez <pauldro952@outlook.com>
2021-02-01 09:49:18 -0600

    Add lookups

--- site/controllers/classes/ajax/Lookup.php
+++ site/controllers/classes/ajax/Lookup.php
@@ -10,17 +10,109 @@ class Lookup extends AbstractController {
 		return 'test';
 	}
 
+	/**
+	 * Search Tariff Codes
+	 * @param  object $data
+	 *                     q   Search Term
+	 * @return void
+	 */
 	public static function tariffCodes($data) {
 		$data = self::sanitizeParameters($data, self::FIELDS_LOOKUP);
 		$wire = self::pw();
 		$page = $wire->wire('page');
 		$filter = $wire->wire('modules')->get('FilterInvTariffCodes');
 		$filter->init_query();
-		$filter->filter_input($wire->wire('input'));
 		$page->headline = "Tariff Codes";
 		self::filterResults($filter, $wire, $data);
 	}
 
+	/**
+	 * Search MSDS Codes
+	 * @param  object $data
+	 *                     q   Search Term
+	 * @return void
+	 */
+	public static function msdsCodes($data) {
+		$data = self::sanitizeParameters($data, self::FIELDS_LOOKUP);
+		$wire = self::pw();
+		$page = $wire->wire('page');
+		$filter = $wire->wire('modules')->get('FilterInvMsdsCodes');
+		$filter->init_query();
+		$page->headline = "Msds Codes";
+		self::filterResults($filter, $wire, $data);
+	}
+
+	/**
+	 * Search Freight Codes
+	 * @param  object $data
+	 *                     q   Search Term
+	 * @return void
+	 */
+	public static function freightCodes($data) {
+		$data = self::sanitizeParameters($data, self::FIELDS_LOOKUP);
+		$wire = self::pw();
+		$page = $wire->wire('page');
+		$filter = $wire->wire('modules')->get('FilterMsoFreightCodes');
+		$filter->init_query();
+		$page->headline = "Freight Codes";
+		self::filterResults($filter, $wire, $data);
+	}
+
+	/**
+	 * Search VXM
+	 * @param  object $data
+	 *                     vendorID Vendor ID
+	 *                     q        Search Term
+	 * @return void
+	 */
+	public static function vxm($data) {
+		$data = self::sanitizeParameters($data, self::FIELDS_LOOKUP);
+		$wire = self::pw();
+		$page = $wire->wire('page');
+		$filter = $wire->wire('modules')->get('FilterXrefItemVxm');
+		$filter->init_query();
+		$page->headline = "VXM";
+		self::filterResults($filter, $wire, $data);
+	}
+
+	/**
+	 * Search Warehouses
+	 * @param  object $data
+	 *                     q   Search Term
+	 * @return void
+	 */
+	public static function warehouses($data) {
+		$data = self::sanitizeParameters($data, self::FIELDS_LOOKUP);
+		$wire = self::pw();
+		$page = $wire->wire('page');
+		$filter = $wire->wire('modules')->get('FilterWarehouses');
+		$filter->init_query();
+		$page->headline = "Warehouses";
+		self::filterResults($filter, $wire, $data);
+	}
+
+	/**
+	 * Search Users
+	 * @param  object $data
+	 *                     q   Search Term
+	 * @return void
+	 */
+	public static function users($data) {
+		$data = self::sanitizeParameters($data, self::FIELDS_LOOKUP);
+		$wire = self::pw();
+		$page = $wire->wire('page');
+		$filter = $wire->wire('modules')->get('FilterDplusUsers');
+		$filter->init_query();
+		$page->headline = "Users";
+		self::filterResults($filter, $wire, $data);
+	}
+
+	/**
+	 * Search Items
+	 * @param  object $data
+	 *                     q   Search Term
+	 * @return void
+	 */
 	public static function itmItems($data) {
 		$data = self::sanitizeParameters($data, self::FIELDS_LOOKUP);
 		$wire = self::pw();
@@ -46,6 +138,6 @@ class Lookup extends AbstractController {
 
 		$path = $input->urlSegment(count($input->urlSegments()));
 		$page->body .= $wire->wire('config')->twig->render("api/lookup/$path/search.twig", ['results' => $results, 'datamatcher' => $wire->wire('modules')->get('RegexData'), 'q' => $data->q]);
-		$page->body .= $wire->wire('config')->twig->render('util/paginator.twig', ['resultscount'=> $results->getNbResults()]);
+		$page->body .= $wire->wire('config')->twig->render('util/paginator.twig', ['resultscount'=> $results->getNbResults() != $query->count() ? $query->count() : $results->getNbResults()]);
 	}
 }
--- site/templates/ajax-lookups.php
+++ site/templates/ajax-lookups.php
@@ -4,9 +4,27 @@
 	$page->searchURL = $input->url();
 	$routes = [
 		['GET', $page->url, AjaxLookup::class, 'test'],
+		// Tariff Codes
 		['GET', $page->url.'tariff-codes/', AjaxLookup::class, 'tariffCodes'],
+		['GET', $page->url.'tariff-codes/page{d:\d+}/', AjaxLookup::class, 'tariffCodes'],
+		// Itm
 		['GET', $page->url.'items', AjaxLookup::class, 'itmItems'],
-		['GET', $page->url.'items/page{id:\d+}/', AjaxLookup::class, 'itmItems'],
+		['GET', $page->url.'items/page{nbr:\d+}/', AjaxLookup::class, 'itmItems'],
+		// Msds Codes
+		['GET', $page->url.'msds-codes/', AjaxLookup::class, 'msdsCodes'],
+		['GET', $page->url.'msds-codes/page{nbr:\d+}/', AjaxLookup::class, 'msdsCodes'],
+		// Freight Codes
+		['GET', $page->url.'freight-codes/', AjaxLookup::class, 'freightCodes'],
+		['GET', $page->url.'freight-codes/page{nbr:\d+}/', AjaxLookup::class, 'freightCodes'],
+		// VXM
+		['GET', $page->url.'vxm/', AjaxLookup::class, 'vxm'],
+		['GET', $page->url.'vxm/page{nbr:\d+}/', AjaxLookup::class, 'vxm'],
+		// Warehouse
+		['GET', $page->url.'warehouses/', AjaxLookup::class, 'warehouses'],
+		['GET', $page->url.'warehouses/page{nbr:\d+}/', AjaxLookup::class, 'warehouses'],
+		// User
+		['GET', $page->url.'users/', AjaxLookup::class, 'users'],
+		['GET', $page->url.'users/page{nbr:\d+}/', AjaxLookup::class, 'users'],
 	];
 
 	$router = new Mvc\Router();
