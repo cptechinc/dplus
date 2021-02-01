d7f9c1f78e581da6f6618df9db2b94260028fd62
Paul Gomez <pauldro952@outlook.com>
2021-02-01 14:17:46 -0600

    Update ajax-lookups.php

--- site/templates/ajax-lookups.php
+++ site/templates/ajax-lookups.php
@@ -3,32 +3,33 @@
 	use Controllers\Ajax\Lookup as AjaxLookup;
 	$page->searchURL = $input->url();
 	$routes = [
-		['GET', $page->url, AjaxLookup::class, 'test'],
+		['GET', '', AjaxLookup::class, 'test'],
 		// Tariff Codes
-		['GET', $page->url.'tariff-codes/', AjaxLookup::class, 'tariffCodes'],
-		['GET', $page->url.'tariff-codes/page{d:\d+}/', AjaxLookup::class, 'tariffCodes'],
+		['GET', 'tariff-codes/', AjaxLookup::class, 'tariffCodes'],
+		['GET', 'tariff-codes/page{d:\d+}/', AjaxLookup::class, 'tariffCodes'],
 		// Itm
-		['GET', $page->url.'items', AjaxLookup::class, 'itmItems'],
-		['GET', $page->url.'items/page{nbr:\d+}/', AjaxLookup::class, 'itmItems'],
+		['GET', 'items', AjaxLookup::class, 'itmItems'],
+		['GET', 'items/page{nbr:\d+}/', AjaxLookup::class, 'itmItems'],
 		// Msds Codes
-		['GET', $page->url.'msds-codes/', AjaxLookup::class, 'msdsCodes'],
-		['GET', $page->url.'msds-codes/page{nbr:\d+}/', AjaxLookup::class, 'msdsCodes'],
+		['GET', 'msds-codes/', AjaxLookup::class, 'msdsCodes'],
+		['GET', 'msds-codes/page{nbr:\d+}/', AjaxLookup::class, 'msdsCodes'],
 		// Freight Codes
-		['GET', $page->url.'freight-codes/', AjaxLookup::class, 'freightCodes'],
-		['GET', $page->url.'freight-codes/page{nbr:\d+}/', AjaxLookup::class, 'freightCodes'],
+		['GET', 'freight-codes/', AjaxLookup::class, 'freightCodes'],
+		['GET', 'freight-codes/page{nbr:\d+}/', AjaxLookup::class, 'freightCodes'],
 		// VXM
-		['GET', $page->url.'vxm/', AjaxLookup::class, 'vxm'],
-		['GET', $page->url.'vxm/page{nbr:\d+}/', AjaxLookup::class, 'vxm'],
+		['GET', 'vxm/', AjaxLookup::class, 'vxm'],
+		['GET', 'vxm/page{nbr:\d+}/', AjaxLookup::class, 'vxm'],
 		// Warehouse
-		['GET', $page->url.'warehouses/', AjaxLookup::class, 'warehouses'],
-		['GET', $page->url.'warehouses/page{nbr:\d+}/', AjaxLookup::class, 'warehouses'],
+		['GET', 'warehouses/', AjaxLookup::class, 'warehouses'],
+		['GET', 'warehouses/page{nbr:\d+}/', AjaxLookup::class, 'warehouses'],
 		// User
-		['GET', $page->url.'users/', AjaxLookup::class, 'users'],
-		['GET', $page->url.'users/page{nbr:\d+}/', AjaxLookup::class, 'users'],
+		['GET', 'users/', AjaxLookup::class, 'users'],
+		['GET', 'users/page{nbr:\d+}/', AjaxLookup::class, 'users'],
 	];
 
 	$router = new Mvc\Router();
 	$router->setRoutes($routes);
+	$router->setRoutePrefix($page->url);
 	$router->route();
 
 	if ($config->ajax) {
