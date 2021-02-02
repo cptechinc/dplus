1f52a92f6b51e92a8f7719f0ee36933a910636f7
Paul Gomez <pauldro952@outlook.com>
2021-02-02 09:42:29 -0600

    Update Min.php

--- site/controllers/classes/ajax/json/Min.php
+++ site/controllers/classes/ajax/json/Min.php
@@ -4,7 +4,7 @@ use ProcessWire\Module, ProcessWire\ProcessWire;
 
 use Mvc\Controllers\AbstractController;
 
-use Dplus\CodeValidators\Min as InValidator;
+use Dplus\CodeValidators\Min as MinValidator;
 
 use TariffCodeQuery, TariffCode;
 use CountryCodeQuery, CountryCode;
@@ -148,6 +148,6 @@ class Min extends AbstractController {
 
 
 	private static function validator() {
-		return new InValidator();
+		return new MinValidator();
 	}
 }
