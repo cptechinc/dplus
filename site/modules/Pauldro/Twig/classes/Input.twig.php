691135857ccdf9f8e54e9400efbac1d5e373ebe8
Paul Gomez <pauldro952@outlook.com>
2021-02-11 08:38:53 -0600

    Update Input.twig.php

--- site/modules/Pauldro/Twig/classes/Input.twig.php
+++ site/modules/Pauldro/Twig/classes/Input.twig.php
@@ -1,12 +1,15 @@
 <?php namespace Twig\Html;
 
 class TwigInput extends TwigBaseHtml {
+	const SIZES = ['xs', 'sm', 'md', 'lg', 'xl'];
+
 	const DEFAULTS = [
 		'type'       => 'text',
 		'inputclass' => 'form-control',
 		'name'       => '',
 		'id'         => '',
 		'value'      => '',
+		'size'       => '',
 		'addclasses' => [],
 		'attributes' => []
 	];
@@ -19,6 +22,7 @@ class TwigInput extends TwigBaseHtml {
 	public function __construct() {
 		$this->type = 'text';
 		$this->inputclass = 'form-control';
+		$this->size       = '';
 		$this->name = '';
 		$this->id   = '';
 		$this->value = '';
@@ -41,7 +45,12 @@ class TwigInput extends TwigBaseHtml {
 	 * @return string
 	 */
 	public function class() {
+		$base = self::DEFAULTS['inputclass'];
 		$class = $this->inputclass;
+
+		if (in_array($this->size, self::SIZES)) {
+			$class .= " $base-$this->size";
+		}
 		$class .= ' ' . implode(' ', $this->addclasses);
 		return trim($class);
 	}
