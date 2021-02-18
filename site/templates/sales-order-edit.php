5e8e0f5b156245df3d74bad65b3f43847c150550
Paul Gomez <pauldro952@outlook.com>
2021-02-18 09:18:36 -0600

    Add / Handle Route for Editing Line Item

--- site/templates/sales-order-edit.php
+++ site/templates/sales-order-edit.php
@@ -1,9 +1,9 @@
 <?php
 	include($modules->get('Mvc')->controllersPath().'vendor/autoload.php');
 	use Controllers\Mso\Eso;
-
 	$routes = [
 		['GET',  '', Eso::class, 'index'],
+		['GET',  'line/', Eso::class, 'editItem'],
 		['POST', '', Eso::class, 'handleCRUD'],
 	];
 	$router = new Mvc\Router();
@@ -19,4 +19,9 @@
 		$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
 		$session->removeFor('response', 'eso');
 	}
-	include __DIR__ . "/basic-page.php";
+	
+	if ($config->ajax) {
+		echo $page->body;
+	} else {
+		include __DIR__ . "/basic-page.php";
+	}
