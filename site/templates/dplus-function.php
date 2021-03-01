0976ca3100bb5236e3237a8ab782f1f7f6ef1113
Paul <pauldro952@outlook.com>
2021-03-01 09:57:51 -0600

    Use controllers for dplus-(menu&functions)

--- site/templates/dplus-function.php
+++ site/templates/dplus-function.php
@@ -1,23 +1,24 @@
 <?php
-	if (empty($page->pw_template)) {
-		$page->headline = $page->title = "Cannot Render Page";
-		$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => $page->title, 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Template is not defined"]);
+	include($modules->get('Mvc')->controllersPath().'vendor/autoload.php');
+	use Controllers\Dplus\Process as Dprocess;
+
+	$routes = [
+		['GET',  '', Dprocess::class, 'index'],
+	];
+
+	$router = new Mvc\Router();
+	$router->setRoutes($routes);
+	$router->setRoutePrefix($page->url);
+	$response = $router->route();
+
+	if ($router->hasError()) {
+		$page->body = $response;
+	}
+
+	if (Dprocess::$permitted === false || Dprocess::$templateExists === false || $router->hasError()) {
 		include __DIR__ . "/basic-page.php";
-	} else {
-		$template = str_replace('.php', '', $page->pw_template) . '.php';
+	}
 
-		if (file_exists("./$template")) {
-			$permission = empty($page->dplus_function) ? $page->dplus_permission : $page->dplus_function;
-
-			if ($user->has_function($permission) || empty($permission)) {
-				include("./$template");
-			} else {
-				$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "You don't have access to this function", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Permission: $permission"]);
-				include __DIR__ . "/basic-page.php";
-			}
-		} else {
-			$page->headline = $page->title = "Cannot Render Page";
-			$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => $page->title, 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Template can not be found"]);
-			include __DIR__ . "/basic-page.php";
-		}
+	if (Dprocess::$permitted && Dprocess::$templateExists) {
+		include Dprocess::templateFileName($page);
 	}
