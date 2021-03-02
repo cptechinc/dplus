4e095b722820a0d65c92528bd32087e78866cb68
Paul <pauldro952@outlook.com>
2021-03-02 10:06:58 -0600

    Update Menu.php

--- site/controllers/classes/dplus/Menu.php
+++ site/controllers/classes/dplus/Menu.php
@@ -16,12 +16,13 @@ class Menu extends AbstractController {
 			$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "You don't have access to this function", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Permission: $permission"]);
 			return $page->body;
 		}
+		$config = self::pw('config');
 
 		$permission_list = implode("|", $user->get_functions());
 		$page->pagetitle = "Menu: $page->title";
 		$items = $page->children("template!=redir|dplus-json, dplus_function=$permission_list");
 
-		$page->body .= $config->twig->render('dplus-menu/menu-search-form.twig', ['page' => $pages->get('template=menu'), 'items' => $items]);
+		$page->body .= $config->twig->render('dplus-menu/menu-search-form.twig', ['page' => self::pw('pages')->get('template=menu'), 'items' => $items]);
 		$page->body .= $config->twig->render('dplus-menu/menu-list.twig', ['page' => $page, 'items' => $items]);
 		return $page->body;
 	}
