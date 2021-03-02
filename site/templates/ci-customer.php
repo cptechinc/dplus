92068f4dbbfb0ee450485978a13f90e8a9deb28e
Paul <pauldro952@outlook.com>
2021-03-02 08:16:51 -0600

    Update ci-customer.php

--- site/templates/ci-customer.php
+++ site/templates/ci-customer.php
@@ -1,88 +1,19 @@
 <?php
-	use Map\CustomerTableMap;
-	use Propel\Runtime\ActiveQuery\Criteria;
+	include($modules->get('Mvc')->controllersPath().'vendor/autoload.php');
+	use Controllers\Mci\Ci;
+	
+	$routes = [
+		['GET',  '', Ci::class, 'index'],
+		['GET',  'page{nbr:\d+}/', Ci::class, 'list'],
+	];
 
-	$module_useractions = $modules->get('FilterUserActions');
-	$html = $modules->get('HtmlWriter');
-	$validate = new Dplus\CodeValidators\Mar();
+	$router = new Mvc\Router();
+	$router->setRoutes($routes);
+	$router->setRoutePrefix($page->url);
+	$response = $router->route();
 
-	if ($input->get->custID) {
-		$custID = $input->get->text('custID');
-
-		if ($validate->custid($custID)) {
-			if ($user->has_customer($custID)) {
-				$modules->get('DpagesMci')->init_customer_hooks();
-				$modules->get('DpagesMci')->init_cipage();
-				$load_customer = $modules->get('CiLoadCustomerShipto');
-				$load_customer->set_custID($custID);
-				$customer = $load_customer->get_customer();
-				$actions = $load_customer->get_useractions($input);
-				$contacts = $load_customer->get_contacts();
-				$sales_orders = $load_customer->get_salesorders();
-				$sales_history = $load_customer->get_saleshistory();
-				$quotes = $load_customer->get_quotes();
-				$page->show_breadcrumbs = false;
-				$page->body .= $config->twig->render('customers/ci/bread-crumbs.twig', ['page' => $page, 'customer' => $customer]);
-				$page->headline = "CI: $customer->name";
-
-				if (!$customer->is_active()) {
-					$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => 'Inactive Customer', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Customer $custID is not active"]);
-					$page->body .= $html->div('class=mb-3');
-				}
-
-				if ($customer->has_credithold()) {
-					$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => 'Credit Hold', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Customer $custID has a credit hold"]);
-					$page->body .= $html->div('class=mb-3');
-				}
-
-				$toolbar = $config->twig->render('customers/ci/customer/toolbar.twig', ['page' => $page, 'custID' => $customer->id]);
-				$header =  $config->twig->render('customers/ci/customer/header.twig', ['page' => $page, 'customer' => $customer]);
-
-				$page->body .= "<div class='row'>";
-					$page->body .= $html->div('class=col-sm-2 pl-0', $toolbar);
-					$page->body .= $html->div('class=col-sm-10', $header);
-				$page->body .= "</div>";
-				$page->body .= $config->twig->render('customers/ci/customer/actions-panel.twig', ['page' => $page, 'module_useractions' => $module_useractions, 'actions' => $actions, 'resultscount'=> $actions->getNbResults()]);
-				$page->body .= $config->twig->render('customers/ci/customer/contacts-panel.twig', ['page' => $page, 'customer' => $customer, 'contacts' => $contacts, 'resultscount'=> $contacts->getNbResults()]);
-				$page->body .= $config->twig->render('customers/ci/customer/sales-orders-panel.twig', ['page' => $page, 'customer' => $customer, 'orders' => $sales_orders, 'resultscount'=> $sales_orders->getNbResults(), 'orderpage' => $pages->get('pw_template=sales-order-view')->url, 'sales_orders_list' => $page->cust_salesordersURL($customer->id)]);
-				$page->body .= $config->twig->render('customers/ci/customer/sales-history-panel.twig', ['page' => $page, 'customer' => $customer, 'orders' => $sales_history, 'resultscount'=> $sales_history->getNbResults(), 'orderpage' => $pages->get('pw_template=sales-order-view')->url, 'shipped_orders_list' => $page->cust_saleshistoryURL($customer->id)]);
-
-				if ($user->has_function('mqo')) {
-					$page->body .= $config->twig->render('customers/ci/customer/quotes-panel.twig', ['page' => $page, 'customer' => $customer, 'quotes' => $quotes, 'resultscount'=> $quotes->getNbResults(), 'quotepage' => $pages->get('pw_template=quote-view')->url, 'quotes_list' => $page->cust_quotesURL($customer->id)]);
-				}
-				$config->scripts->append(hash_templatefile('scripts/customer/ci-customer.js'));
-			} else {
-				$page->searchURL = $page->url;
-				$page->headline = "User $user->name Does Not Have Access to $custID";
-				$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => $page->headline, 'iconclass' => 'fa fa-warning fa-2x', 'message' => "You doe not have permission to access this customer"]);
-				$page->body .= $html->div('class=mb-3');
-				$page->body .= $config->twig->render('customers/search-form.twig', ['page' => $page]);
-			}
-		} else {
-			$page->body .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Customer $custID not found"]);
-		}
-	} else {
-		$filter_customers = $modules->get('FilterCustomers');
-		$filter_customers->init_query($user);
-		$filter_customers->search($input->get->text('q'));
-
-		if ($input->get->q) {
-			$q = strtoupper($input->get->text('q'));
-
-			if ($validate->custid($q)) {
-				$session->redirect($page->url."?custID=$q", $http301 = false);
-			}
-			$filter_customers->search($q);
-			$page->headline = "CI: Searching for '$q'";
-		}
-
-		$filter_customers->apply_sortby($page);
-		$query = $filter_customers->get_query();
-		$customers = $query->paginate($input->pageNum, 10);
-
-		$page->searchURL = $page->url;
-		$page->body = $config->twig->render('customers/customer-search.twig', ['page' => $page, 'customers' => $customers, 'datamatcher' => $modules->get('RegexData'), 'q' => $input->get->text('q')]);
-		$page->body .= $config->twig->render('util/paginator.twig', ['page' => $page, 'resultscount'=> $customers->getNbResults()]);
+	if ($router->hasError()) {
+		$page->body = $response;
 	}
 
 	include __DIR__ . "/basic-page.php";
