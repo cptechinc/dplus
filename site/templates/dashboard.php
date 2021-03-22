842a1b802a7cb3f225fd9d1e49a188a6c6fa8fea
Paul Gomez <pauldro952@hotmail.com>
2021-03-22 10:48:37 -0500

    Create and Use Dashboard Controllers
    Add ConfigureCI module

--- site/templates/dashboard.php
+++ site/templates/dashboard.php
@@ -1,2 +1,14 @@
 <?php
-	include __DIR__ . "/dashboard-$user->dplusrole.php";
+	include($modules->get('Mvc')->controllersPath().'vendor/autoload.php');
+	use Controllers\Dplus\Dashboard as DashboardController;
+
+	$routes = [
+		['GET',  '', DashboardController::class, 'dashboard'],
+	];
+
+	$router = new Mvc\Router();
+	$router->setRoutes($routes);
+	$router->setRoutePrefix($page->url);
+	$page->body = $router->route();
+
+	include __DIR__ . "/basic-page.php";
