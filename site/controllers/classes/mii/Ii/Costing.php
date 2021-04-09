7dcc522c75d0d6f031834f795e7f16530c82549b
Paul <pauldro952@outlook.com>
2021-04-09 09:34:33 -0500

    refactor: reorganize Ii\Costing

--- site/controllers/classes/mii/Ii/Costing.php
+++ site/controllers/classes/mii/Ii/Costing.php
@@ -10,6 +10,9 @@ class Costing extends IiFunction {
 	const JSONCODE       = 'ii-costing';
 	const PERMISSION_IIO = 'costing';
 
+/* =============================================================
+	1. Indexes
+============================================================= */
 	public static function index($data) {
 		$fields = ['itemID|text', 'refresh|bool'];
 		self::sanitizeParametersShort($data, $fields);
@@ -26,12 +29,34 @@ class Costing extends IiFunction {
 		return self::costing($data);
 	}
 
+	public static function costing($data) {
+		if (self::validateItemidPermission($data) === false) {
+			return self::alertInvalidItemPermissions($data);
+		}
+		self::pw('modules')->get('DpagesMii')->init_iipage();
+		$data = self::sanitizeParametersShort($data, ['itemID|text']);
+
+		self::getData($data);
+		$page    = self::pw('page');
+		$page->headline = "$data->itemID Costing";
+		$html = '';
+		$html .= self::breadCrumbs();
+		$html .= self::display($data);
+		return $html;
+	}
+
+/* =============================================================
+	2. Data Requests
+============================================================= */
 	public static function requestJson($itemID, $sessionID) {
 		$sessionID = $sessionID ? $sessionID : session_id();
 		$data = array('IICOST', "ITEMID=$itemID");
 		self::sendRequest($data, $vars->sessionID);
 	}
 
+/* =============================================================
+	3. URLs
+============================================================= */
 	public static function costingUrl($itemID = '', $refreshdata = false) {
 		$url = new Purl(self::pw('pages')->get('pw_template=ii-item')->url);
 		$url->path->add('costing');
@@ -45,35 +70,14 @@ class Costing extends IiFunction {
 		return $url->getUrl();
 	}
 
-	public static function costing($data) {
-		if (self::validateItemidPermission($data) === false) {
-			return self::alertInvalidItemPermissions($data);
-		}
-		self::pw('modules')->get('DpagesMii')->init_iipage();
-		$data = self::sanitizeParametersShort($data, ['itemID|text']);
-		$html = '';
-
-		$page    = self::pw('page');
-		$config  = self::pw('config');
-		$pages   = self::pw('pages');
-		$modules = self::pw('modules');
-		$htmlwriter = $modules->get('HtmlWriter');
-		$jsonM      = $modules->get('JsonDataFiles');
-
-		$page->headline = "$data->itemID Costing";
-		$html .= self::breadCrumbs();
-		$html .= self::costingData($data);
-		return $html;
-	}
-
-	private static function costingData($data) {
+/* =============================================================
+	4. Data Retrieval
+============================================================= */
+	private static function getData($data) {
 		$data    = self::sanitizeParametersShort($data, ['itemID|text']);
 		$jsonm   = self::getJsonModule();
 		$json    = $jsonm->getFile(self::JSONCODE);
-		$page    = self::pw('page');
-		$config  = self::pw('config');
 		$session = self::pw('session');
-		$html = '';
 
 		if ($jsonm->exists(self::JSONCODE)) {
 			if ($json['itemid'] != $data->itemID) {
@@ -81,24 +85,24 @@ class Costing extends IiFunction {
 				$session->redirect(self::costingUrl($data->itemID, $refreshdata = true), $http301 = false);
 			}
 			$session->setFor('ii', 'costing', 0);
-			$refreshurl = self::costingUrl($data->itemID, $refreshdata = true);
-			$html .= self::costingDataDisplay($data, $json);
-			return $html;
+			return true;
 		}
 
 		if ($session->getFor('ii', 'costing') > 3) {
-			$page->headline = "Costing File could not be loaded";
-			$html .= self::costingDataDisplay($data, $json);
-			return $html;
-		} else {
-			$session->setFor('ii', 'costing', ($session->getFor('ii', 'costing') + 1));
-			$session->redirect(self::costingUrl($data->itemID, $refreshdata = true), $http301 = false);
+			return false;
 		}
+		$session->setFor('ii', 'costing', ($session->getFor('ii', 'costing') + 1));
+		$session->redirect(self::costingUrl($data->itemID, $refreshdata = true), $http301 = false);
 	}
 
-	protected static function costingDataDisplay($data, $json) {
-		$jsonm  = self::getJsonModule();
+/* =============================================================
+	5. Displays
+============================================================= */
+	protected static function display($data) {
 		$config = self::pw('config');
+		$jsonm  = self::getJsonModule();
+		$json   = $jsonm->getFile(self::JSONCODE);
+
 
 		if ($jsonm->exists(self::JSONCODE) === false) {
 			return $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => 'Costing File Not Found']);
@@ -109,10 +113,8 @@ class Costing extends IiFunction {
 		}
 		$iim  = self::pw('modules')->get('DpagesMii');
 		$page = self::pw('page');
-		$page->refreshurl = self::costingUrl($data->itemID, $refreshdata = true);
+		$page->refreshurl   = self::costingUrl($data->itemID, $refreshdata = true);
 		$page->lastmodified = $jsonm->lastModified(self::JSONCODE);
-		$html = '';
-		$html .= $config->twig->render('items/ii/costing/display.twig', ['item' => self::getItmItem($data->itemID), 'json' => $json, 'module_json' => $jsonm->jsonm]);
-		return $html;
+		return $config->twig->render('items/ii/costing/display.twig', ['item' => self::getItmItem($data->itemID), 'json' => $json, 'module_json' => $jsonm->jsonm]);
 	}
 }
