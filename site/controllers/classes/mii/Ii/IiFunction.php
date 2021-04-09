001204b0191575da3662cc77b02db97b1931c27b
Paul <pauldro952@outlook.com>
2021-04-09 15:14:23 -0500

    Update IiFunction.php

--- site/controllers/classes/mii/Ii/IiFunction.php
+++ site/controllers/classes/mii/Ii/IiFunction.php
@@ -21,21 +21,10 @@ abstract class IiFunction extends AbstractController {
 	private static $jsonm;
 	private static $filehasher;
 
-	protected static function alertInvalidItemPermissions($data) {
-		self::sanitizeParametersShort($data, ['itemID|text']);
-		if (empty($data->itemID) === false && self::validateItemid($data->itemID) === false) {
-			return self::pw('config')->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Item $data->itemID could not be found"]);
-		}
-		if (self::validateUserPermission($data) === false) {
-			return self::pw('config')->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "You don't have Permission for this Function"]);
-		}
-		return '';
-	}
 
-	protected static function breadCrumbs() {
-		return self::pw('config')->twig->render('items/ii/bread-crumbs.twig');
-	}
-
+/* =============================================================
+	Validations
+============================================================= */
 	protected static function validateItemidPermission($data) {
 		self::sanitizeParametersShort($data, ['itemID|text']);
 
@@ -68,6 +57,35 @@ abstract class IiFunction extends AbstractController {
 		return true;
 	}
 
+/* =============================================================
+	Data Requests
+============================================================= */
+	public static function requestIiItem($itemID, $sessionID = '') {
+		$sessionID = $sessionID ? $sessionID : session_id();
+		$db = self::pw('modules')->get('DplusOnlineDatabase')->db_name;
+		$data = array('IISELECT', "ITEMID=$itemID");
+		self::sendRequest($data, $sessionID);
+	}
+
+	protected static function sendRequest(array $data, $sessionID = '') {
+		$sessionID = $sessionID ? $sessionID : session_id();
+		$db = self::pw('modules')->get('DplusOnlineDatabase')->db_name;
+		$data = array_merge(["DBNAME=$db"], $data);
+		$requestor = self::pw('modules')->get('DplusRequest');
+		$requestor->write_dplusfile($data, $sessionID);
+		$requestor->cgi_request(self::pw('config')->cgis['default'], $sessionID);
+	}
+
+/* =============================================================
+	Classes, Module Getters
+============================================================= */
+	protected static function getFileHasher() {
+		if (empty(self::$filehasher)) {
+			self::$filehasher = self::pw('modules')->get('FileHasher');
+		}
+		return self::$filehasher;
+	}
+
 	public static function getValidator() {
 		if (empty(self::$validator)) {
 			self::$validator = new MinValidator();
@@ -93,26 +111,21 @@ abstract class IiFunction extends AbstractController {
 		return self::pw('modules')->get('Itm')->item($itemID);
 	}
 
-	public static function requestIiItem($itemID, $sessionID = '') {
-		$sessionID = $sessionID ? $sessionID : session_id();
-		$db = self::pw('modules')->get('DplusOnlineDatabase')->db_name;
-		$data = array('IISELECT', "ITEMID=$itemID");
-		self::sendRequest($data, $sessionID);
-	}
-
-	protected static function sendRequest(array $data, $sessionID = '') {
-		$sessionID = $sessionID ? $sessionID : session_id();
-		$db = self::pw('modules')->get('DplusOnlineDatabase')->db_name;
-		$data = array_merge(["DBNAME=$db"], $data);
-		$requestor = self::pw('modules')->get('DplusRequest');
-		$requestor->write_dplusfile($data, $sessionID);
-		$requestor->cgi_request(self::pw('config')->cgis['default'], $sessionID);
-	}
-
-	protected static function getFileHasher() {
-		if (empty(self::$filehasher)) {
-			self::$filehasher = self::pw('modules')->get('FileHasher');
+/* =============================================================
+	Displays
+============================================================= */
+	protected static function alertInvalidItemPermissions($data) {
+		self::sanitizeParametersShort($data, ['itemID|text']);
+		if (empty($data->itemID) === false && self::validateItemid($data->itemID) === false) {
+			return self::pw('config')->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Item $data->itemID could not be found"]);
 		}
-		return self::$filehasher;
+		if (self::validateUserPermission($data) === false) {
+			return self::pw('config')->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "You don't have Permission for this Function"]);
+		}
+		return '';
+	}
+
+	protected static function breadCrumbs() {
+		return self::pw('config')->twig->render('items/ii/bread-crumbs.twig');
 	}
 }
