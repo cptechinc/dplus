c3c1608cc9c68ebfc8a62c362b41345dda971473
Paul <pauldro952@outlook.com>
2021-04-12 14:39:50 -0500

    CHORE: remove ii-sales-history

--- site/templates/redir/ii.php
+++ site/templates/redir/ii.php
@@ -22,13 +22,6 @@
 	*		IISELECT
 	*		ITEMID=$itemID
 	*		break;
-	*	case 'ii-sales-history':
-	* 		Request II Sales History JSON file
-	* 		Response: Creates II Sales History JSON file
-	*		DBNAME=$dplusdb
-	*		IISALESHIST
-	*		ITEMID=$itemID
-	*		break;
 	* }
 	**/
 
@@ -43,24 +36,6 @@
 				$session->loc = $url->getUrl();
 			} else {
 				$url = $pages->get('pw_template=ii-item')->httpUrl."?itemID=$itemID";
-				$session->loc = $url;
-			}
-			break;
-		case 'ii-sales-history':
-			$data = array("DBNAME=$dplusdb", 'IISALESHIST', "ITEMID=$itemID");
-			$date = $input->$requestmethod->text('date');
-
-			if (!empty($date)) {
-				$date_ymd = date('Ymd', strtotime($date));
-				$data[] = "DATE=$date_ymd";
-			}
-
-			if ($input->$requestmethod->page) {
-				$url = new Purl\Url($input->$requestmethod->text('page'));
-				$url->query->set('date', $date);
-				$session->loc = $url->getUrl();
-			} else {
-				$url = $pages->get('pw_template=ii-sales-history')->httpUrl."?itemID=$itemID&date=$date";
 				$session->loc = $url;
 			}
 			break;
