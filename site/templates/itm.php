e6ba1320c5afb39c879fe8a1a836fa4cea3503b1
Paul <pauldro952@outlook.com>
2021-04-14 16:24:38 -0500

    Implement Itm\Costing Controller to itm template

--- site/templates/itm.php
+++ site/templates/itm.php
@@ -1,11 +1,18 @@
 <?php
 	include($modules->get('Mvc')->controllersPath().'vendor/autoload.php');
 	use Controllers\Min\Itm\Itm as ItmController;
+	use Controllers\Min\Itm\Costing as CostingController;
+
+	ItmController::initHooks();
 
 	$routes = [
 		['GET',  '', ItmController::class, 'index'],
 		['GET',  'page{d:\d+}', ItmController::class, 'list'],
 		['POST', '', ItmController::class, 'handleCRUD'],
+		'costing' => [
+			['GET',  '', CostingController::class, 'index'],
+			['POST', '', CostingController::class, 'handleCRUD'],
+		]
 	];
 	$router = new Mvc\Router();
 	$router->setRoutes($routes);
