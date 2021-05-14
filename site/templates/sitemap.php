df933414e0739329106f36b084428bdb484053eb
Paul <pauldro952@outlook.com>
2021-05-14 14:44:22 -0500

    Update sitemap.php

--- site/templates/sitemap.php
+++ site/templates/sitemap.php
@@ -4,12 +4,12 @@ include("./_head.php"); ?>
 
 <div id='content'>
 
-	<?php 
-	
-	$maxDepth = 4; 
-	renderNavTree($pages->get('/'), $maxDepth); 
+	<?php
+
+	$maxDepth = 4;
+	renderNavTree($pages->get('/'), $maxDepth);
 	// see the _init.php for the renderNavTree function
-	
+
 	?>
 
 </div>
