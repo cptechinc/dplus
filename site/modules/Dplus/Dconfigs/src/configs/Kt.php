b1fbe622f1a042aec3e52bd938e73527ae1647bd
Paul <pauldro952@outlook.com>
2021-05-28 09:11:06 -0500

    Add KT config

---
+++ site/modules/Dplus/Dconfigs/src/configs/Kt.php
@@ -0,0 +1,14 @@
+<?php namespace Dplus\Configs;
+
+use ConfigKtQuery, ConfigKt;
+
+use Propel\Runtime\ActiveQuery\ModelCriteria as Query;
+use Propel\Runtime\ActiveRecord\ActiveRecordInterface as Model;
+
+/**
+ * ConfigAp
+ * Class for getting AP config
+ */
+class Kt extends AbstractConfig {
+	const MODEL = 'ConfigKt';
+}
--- site/modules/Dplus/Dconfigs/vendor/composer/autoload_static.php
+++ site/modules/Dplus/Dconfigs/vendor/composer/autoload_static.php
@@ -11,6 +11,7 @@ class ComposerStaticInit4ed415e031ba38684c0dc56114a11d32
         'Dplus\\Configs\\Ap' => __DIR__ . '/../..' . '/src/configs/Ap.php',
         'Dplus\\Configs\\Ci' => __DIR__ . '/../..' . '/src/configs/Ci.php',
         'Dplus\\Configs\\In' => __DIR__ . '/../..' . '/src/configs/In.php',
+        'Dplus\\Configs\\Kt' => __DIR__ . '/../..' . '/src/configs/Kt.php',
         'Dplus\\Configs\\Po' => __DIR__ . '/../..' . '/src/configs/Po.php',
         'Dplus\\Configs\\So' => __DIR__ . '/../..' . '/src/configs/So.php',
         'Dplus\\Configs\\Sys' => __DIR__ . '/../..' . '/src/configs/Sys.php',
