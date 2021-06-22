2a6b63edc204e2eb6afa08286d378a8313eb951d
Paul <pauldro952@outlook.com>
2021-06-22 13:57:00 -0500

    Separate Display from logic

--- site/templates/controllers/classes/mar/Spm.php
+++ site/templates/controllers/classes/mar/Spm.php
@@ -1,4 +1,6 @@
 <?php namespace Controllers\Mar;
+// Propel ORM Ljbrary
+use Propel\Runtime\Util\PropelModelPager;
 // Dplus Model
 use SalesPerson;
 // ProcessWire Classes, Modules
@@ -9,6 +11,8 @@ use Dplus\Filters\Mar\SalesPerson as FilterSalesPerson;
 use Mvc\Controllers\AbstractController;
 
 class Spm extends AbstractController {
+	private static $spm;
+
 	public static function index($data) {
 		$fields = ['id|text', 'action|text'];
 		$data = self::sanitizeParametersShort($data, $fields);
@@ -43,31 +47,36 @@ class Spm extends AbstractController {
 		if ($data->action) {
 			return self::handleCRUD($data);
 		}
-		$page = self::pw('page');
-		$spm  = self::pw('modules')->get('Spm');
-		$person = $spm->get_create($data->id);
-		self::lockUser($spm, $person);
-		$page->body .= self::pw('config')->twig->render('mar/armain/spm/form/page.twig', ['person' => $person, 'spm' => $spm]);
-		$page->js   .= self::pw('config')->twig->render('mar/armain/spm/form/.js.twig');
+		$salesrep = self::getSpm()->get_create($data->id);
+		self::pw('page')->js .= self::pw('config')->twig->render('mar/armain/spm/form/.js.twig');
+		return self::salespersonDisplay($data, $salesrep);
+	}
+
+	private static function salespersonDisplay($data, SalesPerson $salesrep) {
+		$spm  = self::getSpm();
+		$html =  self::lockUser($spm, $salesrep);
+		$html .= self::pw('config')->twig->render('mar/armain/spm/form/page.twig', ['person' => $salesrep, 'spm' => $spm]);
+		return $html;
 	}
 
 	private static function lockUser(SpmManager $spm, SalesPerson $person) {
-		$page = self::pw('page');
+		$spm = self::getSpm();
+		$html = '';
+
 		if ($spm->recordlocker->function_locked($person->id) && !$spm->recordlocker->function_locked_by_user($person->id)) {
 			$msg = "Sales Person $person->id is being locked by " . $spm->recordlocker->get_locked_user($person->id);
-			$page->body .= $config->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "Saless Person $person->id is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
-			$page->body .= $html->div('class=mb-3');
+			$html .= self::pw('config')->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "Saless Person $person->id is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
+			$html .= '<div class="mb-3"></div>';
 		} elseif (!$spm->recordlocker->function_locked($person->id)) {
 			$spm->recordlocker->create_lock($person->id);
 		}
-		return $page->body;
+		return $html;
 	}
 
 	public static function list($data) {
 		$data = self::sanitizeParametersShort($data, ['q|text']);
-		$wire = self::pw();
-		$page = $wire->wire('page');
-		$spm = $wire->modules->get('Spm');
+		$page = self::pw('page');
+		$spm  = self::getSpm();
 		$spm->recordlocker->remove_lock();
 
 		$filter = new FilterSalesPerson();
@@ -78,12 +87,23 @@ class Spm extends AbstractController {
 			$filter->search(strtoupper($data->q));
 		}
 		$filter->sortby($page);
-		$people = $filter->query->paginate($wire->wire('input')->pageNum, $wire->wire('session')->display);
-		$config = $wire->wire('config');
+		$reps = $filter->query->paginate(self::pw('input')->pageNum, self::pw('session')->display);
+		$page->js .= self::pw('config')->twig->render('mar/armain/spm/list/.js.twig');
+		return self::listDisplay($data, $reps);
+	}
 
-		$page->body .= $config->twig->render('mar/armain/spm/list/page.twig', ['spm' => $spm, 'people' => $people]);
-		$page->body .= $config->twig->render('util/paginator/propel.twig', ['pager' => $people]);
-		$page->js   .= $config->twig->render('mar/armain/spm/list/.js.twig');
-		return $page->body;
+	private static function listDisplay($data, PropelModelPager $reps) {
+		$config = self::pw('config');
+		$html = '';
+		$html .= $config->twig->render('mar/armain/spm/list/page.twig', ['spm' => self::pw('modules')->get('Spm'), 'people' => $reps]);
+		$html .= $config->twig->render('util/paginator/propel.twig', ['pager' => $reps]);
+		return $html;
+	}
+
+	public static function getSpm() {
+		if (empty(self::$spm)) {
+			self::$spm = self::pw('modules')->get('Spm');
+		}
+		return self::$spm;
 	}
 }
