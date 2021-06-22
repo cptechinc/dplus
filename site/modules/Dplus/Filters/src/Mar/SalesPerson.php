013bf1f0d01673c993e23939e4cb53d1a243f958
Paul <pauldro952@outlook.com>
2021-06-22 14:54:55 -0500

    FEAT: add positionQuick for SPM

--- site/modules/Dplus/Filters/src/Mar/SalesPerson.php
+++ site/modules/Dplus/Filters/src/Mar/SalesPerson.php
@@ -22,4 +22,25 @@ class SalesPerson extends AbstractFilter {
 		];
 		$this->query->search_filter($columns, strtoupper($q));
 	}
+
+/* =============================================================
+	4. Misc Query Functions
+============================================================= */
+	/**
+	 * Return Position of Item in results
+	 * @param  Model|string $rep SalesPerson|Sales Rep ID
+	 * @return int
+	 */
+	public function positionQuick($rep) {
+		$repID = $rep;
+		if (is_object($rep)) {
+			$repID = $rep->id;
+		}
+		$q = $this->getQueryClass();
+		$q->execute_query('SET @rownum = 0');
+		$table = $q->getTableMap()::TABLE_NAME;
+		$sql = "SELECT x.position FROM (SELECT ArspSalePer1, @rownum := @rownum + 1 AS position FROM $table) x WHERE ArspSalePer1 = :repid";
+		$stmt = $q->execute_query($sql, [':repid' => $repID]);
+		return $stmt->fetchColumn();
+	}
 }
