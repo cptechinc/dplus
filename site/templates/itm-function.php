5175f37ee16ce88fa236a6bfc430907cb5c01c3d
Paul <pauldro952@outlook.com>
2021-06-23 09:12:53 -0500

    CHORE: removed unused template files

--- site/templates/itm-function.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$itmp = $modules->get('Itmp');
-	if ($user->has_function('itm') && $itmp->is_user_allowed_template($user, $page->pw_template)) {
-		include("./dplus-function.php");
-	} else {
-		$page->body = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "You don't have access to this function", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Permission: ITM $page->name"]);
-		include __DIR__ . "/basic-page.php";
-	}
