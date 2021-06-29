f1188c751c493a3f6e78c70dfc6bcb6cc775aa9e
Paul <pauldro952@outlook.com>
2021-06-29 08:31:01 -0500

    Add Validate User Access

--- site/modules/Dplus/ValidateCodes/src/Mqo/Mqo.php
+++ site/modules/Dplus/ValidateCodes/src/Mqo/Mqo.php
@@ -1,6 +1,6 @@
 <?php namespace Dplus\CodeValidators;
 
-use ProcessWire\WireData;
+use ProcessWire\WireData, ProcessWire\User;
 
 use QuoteQuery, Quote;
 
@@ -18,5 +18,34 @@ class Mqo extends WireData {
 		$q = QuoteQuery::create();
 		$q->filterByQuoteid($qnbr);
 		return boolval($q->count());
+	}
+
+	/**
+	 * Return If User has access to Sales Order
+	 * @param  string $qnbr Quote #
+	 * @param  User   $user Check if User is Sales Rep
+	 * @return bool
+	 */
+	public function quoteUser($qnbr, User $user) {
+		if ($user->hasRole('slsrep') === false) {
+			return true;
+		}
+		$q = QuoteQuery::create();
+		$q->filterByOrdernumber($this->wire('sanitizer')->qnbr($qnbr));
+		$q->filterBySalesPerson($user->repid);
+		return boolval($q->count());
+	}
+
+	/**
+	 * Validate User's Access to Quote
+	 * @param  string $qnbr Quote Number
+	 * @param  User   $user USer
+	 * @return bool
+	 */
+	public function quoteAccess($qnbr, User $user) {
+		if ($this->quote($qnbr)) {
+			return $this->quoteUser($qnbr, $user);
+		}
+		return false;
 	}
 }
