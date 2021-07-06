6ad90df78cfde15f75b0291564aaa84a644bc3bc
Paul <pauldro952@outlook.com>
2021-07-06 15:02:45 -0500

    Update quote.php

--- site/templates/redir/quote.php
+++ site/templates/redir/quote.php
@@ -21,12 +21,6 @@
 	*		EDITQUOTE
 	*		QUOTENO=$qnbr
 	*		break;
-	*	case 'edit-quote':
-	*		- Saves changes to Quothed
-	*		DBNAME=$dplusdb
-	*		UPDATEQUOTEHEAD
-	*		QUOTENO=$qnbr
-	*		break;
 	* 	case 'remove-line':
 	*		- Removes Detail Line
 	*		DBNAME=$dplusdb
@@ -43,34 +37,6 @@
 			$qnbr = $user->get_lockedID();
 			$data = array("DBNAME=$dplusdb", 'EDITQUOTE', "QUOTENO=$qnbr");
 			$session->loc = $pages->get('pw_template=quote-edit')->url."?qnbr=$qnbr";
-			break;
-		case 'edit-quote';
-			$qnbr = $input->$requestmethod->text('qnbr');
-			$editquote = QuothedQuery::create()->findOneBySessionidQuote(session_id(), $qnbr);
-			$editquote->setShipto_name($input->$requestmethod->text('shipto_name'));
-			$editquote->setShipto_address($input->$requestmethod->text('shipto_address'));
-			$editquote->setShipto_address2($input->$requestmethod->text('shipto_address2'));
-			$editquote->setShipto_city($input->$requestmethod->text('shipto_city'));
-			$editquote->setShipto_state($input->$requestmethod->text('shipto_state'));
-			$editquote->setShipto_zip($input->$requestmethod->text('shipto_zip'));
-			$editquote->setContact($input->$requestmethod->text('contact'));
-			$editquote->setPhone(str_replace('-', '', $input->$requestmethod->text('phone')));
-			$editquote->setFaxnbr(str_replace('-', '', $input->$requestmethod->text('fax')));
-			$editquote->setEmail($input->$requestmethod->text('email'));
-			$editquote->setCustpo($input->$requestmethod->text('custpo'));
-			$editquote->setShipviacd($input->$requestmethod->text('shipvia'));
-			$editquote->setFob($input->$requestmethod->text('fob'));
-			$editquote->setDelivery($input->$requestmethod->text('delivery'));
-			$editquote->setCareof($input->$requestmethod->text('careof'));
-			$editquote->setWarehouse($input->$requestmethod->text('warehouse'));
-			$editquote->save();
-			$data = array("DBNAME=$dplusdb", 'UPDATEQUOTEHEAD', "QUOTENO=$qnbr");
-
-			if ($input->$requestmethod->exit) {
-				$session->loc = $pages->get('template=dplus-menu,name=mqo')->child('template=redir')->url."?action=unlock-quote&qnbr=$qnbr";
-			} else {
-				$session->loc = $pages->get('pw_template=quote-edit')->url."?qnbr=$qnbr";
-			}
 			break;
 		case 'unlock-quote':
 			$qnbr = $input->$requestmethod->text('qnbr');
