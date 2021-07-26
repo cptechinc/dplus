497d0068f3f9d83e33cceba512523a5d271a30c3
Paul <pauldro952@outlook.com>
2021-07-26 14:51:11 -0500

    Create MPO controllers

---
+++ site/templates/controllers/classes/mpo/PurchaseOrder/Base.php
@@ -0,0 +1,80 @@
+<?php namespace Controllers\Mpo\PurchaseOrder;
+// Purl URI Library
+use Purl\Url as Purl;
+// Dplus Model
+use PurchaseOrder;
+// ProcessWire Classes, Modules
+use ProcessWire\Page;
+// Dplus Document Management
+use Dplus\DocManagement\Finders as DocFinders;
+// Dplus Validators
+use Dplus\CodeValidators\Mpo as MpoValidator;
+// Mvc Controllers
+use Mvc\Controllers\AbstractController;
+
+abstract class Base extends AbstractController {
+	private static $validate;
+	private static $docm;
+
+/* =============================================================
+	URLs
+============================================================= */
+	public static function poUrl($ponbr = '') {
+		$url = new Purl(self::pw('pages')->get('pw_template=purchase-order-view')->url);
+		if ($ponbr) {
+			$url->query->set('ponbr', $ponbr);
+		}
+		return $url->getUrl();
+	}
+
+	public static function receivedUrl($ponbr) {
+		$url = new Purl(self::poUrl($ponbr));
+		$url->path->add('received');
+		return $url->getUrl();
+	}
+
+/* =============================================================
+	Displays
+============================================================= */
+	protected static function invalidPo($data) {
+		$config = self::pw('config');
+		$html = '';
+		$html .= $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Purchase Order Not Found', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "PO # $data->ponbr can not be found"]);
+		$html .= '<div class="mb-3"></div>';
+		$html .= self::lookupForm();
+		return $html;
+	}
+
+	protected static function lookupForm() {
+		$page = self::pw('page');
+		$config = self::pw('config');
+		$page->body .= $config->twig->render('purchase-orders/purchase-order/lookup-form.twig');
+		return $page->body;
+	}
+
+/* =============================================================
+	Supplemental
+============================================================= */
+	/**
+	 * Return Mpo Validator
+	 * @return MpoValidator
+	 */
+	protected static function validator() {
+		if (empty(self::$validate)) {
+			self::$validate = new MpoValidator();
+		}
+		return self::$validate;
+	}
+
+	/**
+	 * Return Document Management
+	 * @return DocFinders\PurchaseOrder
+	 */
+	public static function docm() {
+		if (empty(self::$docm)) {
+			self::$docm = new DocFinders\PurchaseOrder();
+		}
+		return self::$docm;
+	}
+
+}
---
+++ site/templates/controllers/classes/mpo/PurchaseOrder/PurchaseOrder.php
@@ -0,0 +1,148 @@
+<?php namespace Controllers\Mpo\PurchaseOrder;
+// Dplus Model
+use PurchaseOrderQuery, PurchaseOrder as PoModel;
+// Dplus Configs
+use Dplus\Configs;
+// ProcessWire Classes, Modules
+use ProcessWire\Page, ProcessWire\WireData;
+// Mvc Controllers
+use Mvc\Controllers\AbstractController;
+use Controllers\Mpo\PurchaseOrder\Base;
+
+class PurchaseOrder extends Base {
+/* =============================================================
+	Indexes
+============================================================= */
+	public static function index($data) {
+		$fields = ['ponbr|text', 'action|text'];
+		self::sanitizeParametersShort($data, $fields);
+
+		if (empty($data->action) === false) {
+			return self::handleCRUD($data);
+		}
+
+		if (empty($data->ponbr) === false) {
+			return self::po($data);
+		}
+		return self::lookupScreen($data);
+	}
+
+	public static function po($data) {
+		self::sanitizeParametersShort($data, ['ponbr|ponbr']);
+		$page = self::pw('page');
+		$config   = self::pw('config');
+		$validate = self::validator();
+
+		if ($validate->po($data->ponbr) === false) {
+			return self::invalidPo($data);
+		}
+
+		$page->headline = "Purchase Order #$data->ponbr";
+
+		return self::purchaseorder($data);
+	}
+
+
+
+
+	public static function handleCRUD($data) {
+		self::sanitizeParametersShort($data, ['ponbr|ponbr', 'action|text']);
+		switch ($data->action) {
+			case 'print-invoice':
+				self::requestPrintInvoice($data);
+				break;
+		}
+		self::pw('session')->redirect(self::orderUrl($data->ponbr));
+	}
+
+/* =============================================================
+	Displays
+============================================================= */
+	private static function purchaseorder($data) {
+		self::sanitizeParametersShort($data, ['ponbr|ponbr']);
+
+		if (self::validator()->po($data->ponbr) === false) {
+			return self::invalidPo($data);
+		}
+		$config = self::pw('config');
+		$order = PurchaseOrderQuery::create()->findOneByPonbr($data->ponbr);
+		$qnotes = self::getQnotes();
+		$html = '';
+		$html  .= $config->twig->render('purchase-orders/purchase-order/links-header.twig', ['purchaseorder' => $purchaseorder]);
+		$html  .= $config->twig->render('purchase-orders/purchase-order/purchase-order.twig', ['config' => self::getConfigs(), 'user' => self::pw('user'), 'purchaseorder' => $purchaseorder, 'qnotes' => $qnotes]);
+		$html  .= $config->twig->render('purchase-orders/purchase-order/documents.twig', ['ponbr' => $data->ponbr, 'documents' => self::docm()->getDocumentsPo($data->ponbr)]);
+		$html  .= $config->twig->render('purchase-orders/purchase-order/qnotes.twig', ['ponbr' => $data->ponbr, 'qnotes' => $qnotes]);
+		$html  .= $config->twig->render('purchase-orders/purchase-order/invoices.twig', ['purchaseorder' => $purchaseorder]);
+		return $html;
+	}
+
+/* =============================================================
+	Supplemental
+============================================================= */
+	public static function initHooks() { // TODO HOOKS for CI
+		$m = self::pw('modules')->get('DpagesMso');
+
+		$m->addHook('Page(pw_template=purchase-order-view|sales-order-edit)::poUrl', function($event) {
+			$event->return = self::orderUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view)::poListUrl', function($event) {
+			$event->return = self::orderListUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view)::orderHistoryListUrl', function($event) {
+			$event->return = self::orderHistoryListUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view|sales-order-edit)::orderPrintUrl', function($event) {
+			$event->return = self::orderPrintUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view|sales-order-edit)::orderEditUrl', function($event) {
+			$event->return = self::orderEditUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view|sales-order-edit)::orderEditUnlockUrl', function($event) {
+			$event->return = self::orderEditUnlockUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view|sales-order-edit)::orderNotesUrl', function($event) {
+			$event->return = self::orderNotesUrl($event->arguments(0), $event->arguments(1));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view|sales-order-edit)::orderDocumentsUrl', function($event) {
+			$event->return = self::orderDocumentsUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view|sales-order-edit)::documentUrl', function($event) {
+			$event->return = self::documentUrl($event->arguments(0), $event->arguments(1), $event->arguments(2));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view|sales-order-edit)::iiUrl', function($event) {
+			$event->return = Ii::iiUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view)::printInvoiceUrl', function($event) {
+			$event->return = self::orderPrintInvoiceUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view|sales-order-edit)::ciUrl', function($event) {
+			$event->return = Ci::ciUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=purchase-order-view|sales-order-edit)::ciShiptoUrl', function($event) {
+			$event->return = Ci::ciShiptoUrl($event->arguments(0), $event->arguments(1));
+		});
+	}
+
+	public static function getQnotes() {
+		return self::pw('modules')->get('QnotesPo');
+	}
+
+	public static function getConfigs() {
+		$configs = new WireData();
+		$configs->so = Configs\So::config();
+		$configs->po = Configs\Po::config();
+		return $configs;
+	}
+}
