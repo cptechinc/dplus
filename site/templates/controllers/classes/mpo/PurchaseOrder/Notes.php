199db104f9d4d0f86caf29b12b7a777ff4d3d85f
Paul <pauldro952@outlook.com>
2021-08-02 13:47:35 -0500

    Update PurchaseOrder Notes

--- site/templates/controllers/classes/mpo/PurchaseOrder/Notes.php
+++ site/templates/controllers/classes/mpo/PurchaseOrder/Notes.php
@@ -4,6 +4,7 @@ use Propel\Runtime\Util\PropelModelPager as ModelPager;
 use Propel\Runtime\Collection\ObjectCollection;
 // Dplus Model
 use DocumentQuery, Document;
+use PurchaseOrderQuery, PurchaseOrder;
 // Dplus CodeValidators
 use Dplus\CodeValidators\Mpo as MpoValidator;
 // Dplus Document Finders
@@ -11,7 +12,7 @@ use Dplus\DocManagement\Finders\PurchaseOrder as DocumentsPo;
 // Mvc Controllers
 use Mvc\Controllers\AbstractController;
 
-class Documents extends Base {
+class Notes extends Base {
 /* =============================================================
 	Indexes
 ============================================================= */
@@ -29,38 +30,39 @@ class Documents extends Base {
 			$docm->moveDocument($data->folder, $data->document);
 			self::pw('session')->redirect(self::pw('config')->url_webdocs.$data->document, $http301 = false);
 		}
-		return self::so($data);
+		return self::po($data);
 	}
 
-	public static function so($data) {
+	public static function po($data) {
 		self::sanitizeParametersShort($data, ['ponbr|ponbr']);
 		/** @var MpoValidator **/
 		$validate = self::validator();
 
-		if ($validate->po($data->ponbr) === false && $validate->invoice($data->ponbr) === false) {
+		if ($validate->po($data->ponbr) === false) {
 			return self::invalidSo($data);
 		}
-		self::pw('page')->headline = "Purchase Order #$data->ponbr Documents";
+		self::pw('page')->headline = "Purchase Order #$data->ponbr Notes";
 
-		if ($validate->invoice($data->ponbr) || $validate->po($data->ponbr)) {
-			return self::documents($data);
+		if ($validate->po($data->ponbr)) {
+			return self::notes($data);
 		}
 	}
 
-	public static function documents($data) {
+	public static function notes($data) {
 		self::sanitizeParametersShort($data, ['ponbr|ponbr']);
 		$page = self::pw('page');
 		$config   = self::pw('config');
 		/** @var MpoValidator **/
 		$validate = self::validator();
 
-		if ($validate->po($data->ponbr) === false && $validate->invoice($data->ponbr) === false) {
+		if ($validate->po($data->ponbr) === false) {
 			return self::invalidPo($data);
 		}
-		/** @var DocumentsPo **/
-		$docm      = self::docm();
-		$documents = $docm->getDocumentsPo($data->ponbr);
-		return self::documentsDisplay($data, $documents);
+
+		$po = PurchaseOrderQuery::create()->findOneByPonbr($data->ponbr);
+		self::qnotesJs();
+		$config->scripts->append(self::getFileHasher()->getHashUrl('scripts/lib/jquery-validate.js'));
+		return self::qnotesDisplay($data, $po);
 	}
 
 /* =============================================================
@@ -71,9 +73,16 @@ class Documents extends Base {
 		return parent::lookupScreen($data);
 	}
 
-	private static function documentsDisplay($data, ObjectCollection $documents) {
-		$html  = self::breadCrumbs();
-		$html .= self::pw('config')->twig->render('purchase-orders/purchase-order/documents.twig', ['documents' => $documents, 'ponbr' => $data->ponbr]);
+	private static function qnotesDisplay($data, PurchaseOrder $po) {
+		/** @var DocumentsPo **/
+		$qnotes = self::qnotes();
+		$html   = self::breadCrumbs();
+		$html   .= self::pw('config')->twig->render('purchase-orders/purchase-order/qnotes/qnotes.twig', ['ponbr' => $data->ponbr, 'purchaseorder' => $po, 'qnotes' => $qnotes]);
 		return $html;
+	}
+
+	public static function qnotesJs() {
+		self::pw('page')->js .= self::pw('config')->twig->render('msa/noce/ajax/js.twig');
+		self::pw('page')->js .= self::pw('config')->twig->render('purchase-orders/purchase-order/qnotes/js.twig');
 	}
 }
