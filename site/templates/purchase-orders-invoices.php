be77715351aa873a0d91c15bad92ec4c1f434364
Paul <pauldro952@outlook.com>
2021-08-04 14:28:12 -0500

    Create purchase-orders-invoices.php

---
+++ site/templates/purchase-orders-invoices.php
@@ -0,0 +1,21 @@
+<?php
+	use Controllers\Mpo\ApInvoice\Lists;
+
+	$routes = [
+		['GET', '', Lists\ApInvoice::class, 'index'],
+		['GET', 'page{d:\d+}', Lists\ApInvoice::class, 'list'],
+		'vendor' => [
+			['GET', '', Lists\Vendor::class, 'index'],
+			['GET', 'page{d:\d+}', Lists\Vendor::class, 'index'],
+		]
+	];
+	$router = new Mvc\Router();
+	$router->setRoutes($routes);
+	$router->setRoutePrefix($page->url);
+	$page->body = $router->route();
+
+	if ($config->ajax) {
+		echo $page->body;
+	} else {
+		include __DIR__ . "/basic-page.php";
+	}
