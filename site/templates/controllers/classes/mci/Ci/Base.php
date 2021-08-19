355cd7793605a6880086f971e6b8ad8205228a44
Paul <pauldro952@outlook.com>
2021-08-19 09:02:15 -0500

    Update Base.php

--- site/templates/controllers/classes/mci/Ci/Base.php
+++ site/templates/controllers/classes/mci/Ci/Base.php
@@ -5,21 +5,38 @@ use Purl\Url as Purl;
 use Mvc\Controllers\AbstractController;
 
 abstract class Base extends AbstractController {
-
-	static public function ciUrl($custID = '') {
-		$url = new Purl(self::pw('pages')->get('pw_template=ci-customer')->url);
+/* =============================================================
+	URLs
+============================================================= */
+	public static function ciUrl($custID = '') {
+		$url = new Purl(self::pw('pages')->get('pw_template=ci')->url);
 		if ($custID) {
 			$url->query->set('custID', $custID);
 		}
 		return $url->getUrl();
 	}
 
-	static public function ciShiptoUrl($custID, $shiptoID = '') {
+	public static function ciShiptoUrl($custID, $shiptoID = '') {
 		$url = new Purl(self::ciUrl($custID));
 		$url->path->add('shiptos');
 		if ($shiptoID) {
 			$url->query->set('shiptoID', $custID);
 		}
 		return $url->getUrl();
+	}
+
+/* =============================================================
+	Displays
+============================================================= */
+	protected static function displayInvalidCustid($data) {
+		return self::pw('config')->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Error!', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Customer $data->custID not found"]);
+	}
+
+	protected static function displayUserNotAllowedCustomer($data) {
+		$config = self::pw('config');
+		$html = $config->twig->render('util/alert.twig', ['type' => 'danger', 'title' => 'Access Denied', 'iconclass' => 'fa fa-warning fa-2x', 'message' => "You do not have permission to access to $data->custID"]);
+		$html .= '<div class="mb-3"></div>';
+		$html .= $config->twig->render('customers/search-form.twig');
+		return $html;
 	}
 }
