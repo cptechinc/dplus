c14fe75981aa238b775bd7cf4b5438f5a30975a2
Paul <pauldro952@outlook.com>
2021-08-31 16:21:15 -0500

    Create MainMenu controller, use on menu template

---
+++ site/templates/controllers/classes/dplus/MainMenu.php
@@ -0,0 +1,54 @@
+<?php namespace Controllers\Dplus;
+// ProcessWire Classes, Modules
+use ProcessWire\Page;
+// Dplus Function Routers
+use Controllers\Routers;
+// Mvc Controllers
+use Mvc\Controllers\AbstractController;
+
+class MainMenu extends AbstractController {
+/* =============================================================
+	Indexes
+============================================================= */
+	public static function index($data) {
+		$fields = ['q|text'];
+		self::sanitizeParametersShort($data, $fields);
+		if ($data->q) {
+			self::redirectIfExists($data);
+		}
+		return self::list($data);
+	}
+
+	private static function redirectIfExists($data) {
+		if (empty($data->q)) {
+			return false;
+		}
+		$count = self::pw('pages')->find("dplus_function=$data->q")->count;
+
+		if ($count === 1 && self::pw('user')->has_function($data->q)) {
+			self::pw('session')->redirect(self::pw('pages')->get("dplus_function=$data->q")->url, $http301 = false);
+		}
+
+		$routers = new Routers\Factory();
+
+		if ($routers->exists($data->q)) {
+			if (self::pw('user')->has_function($data->q)) {
+				$routers->route($data->q);
+			}
+		}
+	}
+
+	private static function list($data) {
+		$permitted = implode("|", self::pw('user')->get_functions());
+		$menu = self::pw('pages')->get('/');
+		self::pw('page')->headline = "Menu";
+		$results = $menu->children("template=dplus-menu|warehouse-menu, dplus_function=$permitted");
+
+		if ($data->q) {
+			self::pw('page')->headline = "Searching for functions that match '$data->q'";
+			$results = self::pw('pages')->find("dplus_function%=$data->q");
+			$results->filter("dplus_function=$permitted");
+		}
+		return self::pw('config')->twig->render('dplus-menu/menu-search-page.twig', ['page' => $menu, 'items' => $results]);
+	}
+}
