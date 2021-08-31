f2de235e3ffa26610ded36571ac9793004364487
Paul <pauldro952@outlook.com>
2021-08-31 16:21:43 -0500

    Add Dplus\Routers Documentation

--- site/templates/controllers/classes/routers/Base.php
+++ site/templates/controllers/classes/routers/Base.php
@@ -4,24 +4,43 @@
 use Mvc\Controllers\AbstractController;
 use Controllers\Mpo\Poadmn;
 
-class Base extends AbstractController {
+abstract class Base extends AbstractController {
 	const ROUTES = [
+		// '{key}' => ['{url}', '{className}', '{function}']
 		'cnfm' => ['', Poadmn\Cnfm::class, 'cnfmUrl']
 	];
 
-
+	/**
+	 * Return if Function is Found
+	 * @param  string $function Function Code
+	 * @return bool
+	 */
 	public static function exists($function = '') {
-		return array_key_exists($function, self::ROUTES);
+		return array_key_exists($function, static::ROUTES);
 	}
 
+	/**
+	 * Redirect to Function
+	 * @uses static::url
+	 * @param  string $function Function Code
+	 * @return void
+	 */
 	public static function route($function = '') {
 		if (static::exists($function) === false) {
 			return false;
 		}
 		$url = static::url($function);
-		return self::pw('session')->redirect($url, $http301);
+		return static::pw('session')->redirect($url, $http301);
 	}
 
+	/**
+	 * Return Url to Function
+	 * NOTE: if Route path is defined, use it
+	 *
+	 * EXAMPLE: ['', Poadmn\Cnfm::class, 'cnfmUrl']
+	 * @param  string $function Function Code
+	 * @return string
+	 */
 	protected static function url($function = '') {
 		if (static::exists($function) === false) {
 			return '';
@@ -34,6 +53,12 @@ class Base extends AbstractController {
 		return static::urlFromClass($function);
 	}
 
+	/**
+	 * Return Url From Route Data using a class function
+	 * EXAMPLE: ['', Poadmn\Cnfm::class, 'cnfmUrl']
+	 * @param  string $function  Function Code
+	 * @return string
+	 */
 	protected static function urlFromClass($function = '') {
 		if (static::exists($function) === false) {
 			return '';
