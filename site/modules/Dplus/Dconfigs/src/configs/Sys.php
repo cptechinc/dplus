bf316f163c7af57367c18ad62acafa9f34941116
Paul <pauldro952@outlook.com>
2021-09-22 09:42:55 -0500

    Update Sys.php

--- site/modules/Dplus/Dconfigs/src/configs/Sys.php
+++ site/modules/Dplus/Dconfigs/src/configs/Sys.php
@@ -9,4 +9,21 @@ use Propel\Runtime\ActiveRecord\ActiveRecordInterface as Model;
  */
 class Sys extends AbstractConfig {
 	const MODEL = 'ConfigSys';
+
+	const CUSTOMERS = array(
+		'ALUMAC' => 'alumacraft',
+		'LINDST' => 'lindstrom',
+	);
+
+	/**
+	 * Return the CustID from SysConfig
+	 * NOTE: Defined in syscm
+	 * @return string
+	 */
+	public static function custid() {
+		$class = self::MODEL;
+		$q = self::query();
+		$q->select($class::aliasproperty('custid'));
+		return $q->findOne();
+	}
 }
