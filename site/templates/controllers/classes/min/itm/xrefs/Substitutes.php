f0a07c6c0348f2246662c1748ddf784a77141203
Paul <pauldro952@outlook.com>
2021-09-23 09:06:22 -0500

    Update Substitutes.php

--- site/templates/controllers/classes/min/itm/xrefs/Substitutes.php
+++ site/templates/controllers/classes/min/itm/xrefs/Substitutes.php
@@ -5,6 +5,7 @@ use Purl\Url as Purl;
 use Propel\Runtime\Util\PropelModelPager;
 // Dplus Model
 use ItemMasterItemQuery, ItemMasterItem;
+use ItemSubstituteQuery, ItemSubstitute;
 // ProcessWire Classes, Modules
 use ProcessWire\Page;
 // Dplus Filters
@@ -16,7 +17,7 @@ class Substitutes extends Base {
 	private static $crud;
 
 	public static function index($data) {
-		$fields = ['itemID|text', 'action|text'];
+		$fields = ['itemID|text', 'subitemID|text', 'action|text'];
 		self::sanitizeParametersShort($data, $fields);
 
 		if (self::validateItemidAndPermission($data) === false) {
@@ -29,21 +30,10 @@ class Substitutes extends Base {
 			return self::handleCRUD($data);
 		}
 
-		if (empty($data->vendoritemID) == false) {
-			return self::xref($data);
+		if (empty($data->subitemID) == false) {
+			return self::sub($data);
 		}
 		return self::list($data);
-	}
-
-	private static function list($data) {
-		self::pw('page')->headline = "ITM: $data->itemID Substitutes";
-
-		$itm = self::pw('modules')->get('Itm');
-		$item = $itm->item($data->itemID);
-		$filter = new Filters\Min\ItemSubstitute();
-		$filter->itemid($data->itemID);
-		$xrefs = $filter->query->paginate(self::pw('input')->pageNum, 10);
-		return self::displaySubstitutes($data, $item, $xrefs);
 	}
 
 	public static function handleCRUD($data) {
@@ -54,17 +44,65 @@ class Substitutes extends Base {
 		self::sanitizeParameters($data, $fields);
 	}
 
+	private static function list($data) {
+		self::pw('page')->headline = "ITM: $data->itemID Substitutes";
+
+		$filter = new Filters\Min\ItemSubstitute();
+		$filter->itemid($data->itemID);
+		$xrefs = $filter->query->paginate(self::pw('input')->pageNum, 10);
+		return self::displayList($data, $xrefs);
+	}
+
+	private static function sub($data) {
+		self::pw('page')->headline = "ITM: $data->itemID Substitute $data->subitemID";
+
+		$itmSub = self::getItmSubstitutes();
+		$item   = $itmSub->getItm()->item($data->itemID);
+		$sub    = $itmSub->getOrCreateSubstitute($data->itemID, $data->subitemID);
+
+		return self::displaySub($data, $item, $sub);
+	}
+
 
 /* =============================================================
 	Url Functions
 ============================================================= */
+	public static function subUrl($itemID, $subitemID) {
+		$url = new Purl(self::xrefUrlSubstitutes($itemID));
+		$url->query->set('subitemID', $subitemID);
+		return $url->getUrl();
+	}
+
+	public static function subDeleteUrl($itemID, $subitemID) {
+		$url = new Purl(self::subUrl($itemID, $subitemID));
+		$url->query->set('action', 'delete');
+		return $url->getUrl();
+	}
 
 /* =============================================================
 	Display Functions
 ============================================================= */
+	private static function displayList($data, PropelModelPager $xrefs) {
+		self::initHooks();
+		$itm  = self::pw('modules')->get('Itm');
+		$item = $itm->item($data->itemID);
+		$html  = self::breadCrumbs();
+		$html .= self::displaySubstitutes($data, $item, $xrefs);
+		return $html;
+	}
+
 	private static function displaySubstitutes($data, ItemMasterItem $item, PropelModelPager $xrefs) {
 		$itmSub = self::getItmSubstitutes();
 		return self::pw('config')->twig->render('items/itm/xrefs/substitutes/list/display.twig', ['item' => $item, 'substitutes' => $xrefs, 'itmSub' => $itmSub]);
+	}
+
+	private static function displaySub($data, ItemMasterItem $item, ItemSubstitute $sub) {
+		self::initHooks();
+		$itmSub = self::getItmSubstitutes();
+
+		$html   = self::breadCrumbs();
+		$html  .= self::pw('config')->twig->render('items/itm/xrefs/substitutes/sub/display.twig', ['item' => $item, 'sub' => $sub, 'itmSub' => $itmSub]);
+		return $html;
 	}
 
 /* =============================================================
@@ -72,6 +110,18 @@ class Substitutes extends Base {
 ============================================================= */
 	public static function initHooks() {
 		$m = self::pw('modules')->get('DpagesMin');
+
+		$m->addHook('Page(pw_template=itm)::subListUrl', function($event) {
+			$event->return = self::xrefUrlSubstitutes($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=itm)::subUrl', function($event) {
+			$event->return = self::subUrl($event->arguments(0), $event->arguments(1));
+		});
+
+		$m->addHook('Page(pw_template=itm)::subDeleteUrl', function($event) {
+			$event->return = self::subDeleteUrl($event->arguments(0), $event->arguments(1));
+		});
 	}
 
 /* =============================================================
