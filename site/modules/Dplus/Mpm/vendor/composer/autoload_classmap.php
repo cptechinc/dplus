04a69124a3562cf1514497bbc78f43e168353fe7
Paul <pauldro952@outlook.com>
2021-10-05 08:31:59 -0500

    Autload for bmm response

--- site/modules/Dplus/Mpm/vendor/composer/autoload_classmap.php
+++ site/modules/Dplus/Mpm/vendor/composer/autoload_classmap.php
@@ -9,4 +9,5 @@ return array(
     'Dplus\\Mpm\\Pmmain\\Bmm' => $baseDir . '/src/pmmain/Bmm/Bmm.php',
     'Dplus\\Mpm\\Pmmain\\Bmm\\Components' => $baseDir . '/src/pmmain/Bmm/Component.php',
     'Dplus\\Mpm\\Pmmain\\Bmm\\Header' => $baseDir . '/src/pmmain/Bmm/Header.php',
+    'Dplus\\Mpm\\Pmmain\\Bmm\\Response' => $baseDir . '/src/pmmain/Bmm/Response.php',
 );
--- site/modules/Dplus/Mpm/vendor/composer/autoload_static.php
+++ site/modules/Dplus/Mpm/vendor/composer/autoload_static.php
@@ -10,6 +10,7 @@ class ComposerStaticInit5294399c1667d09eec658aa3396e7693
         'Dplus\\Mpm\\Pmmain\\Bmm' => __DIR__ . '/../..' . '/src/pmmain/Bmm/Bmm.php',
         'Dplus\\Mpm\\Pmmain\\Bmm\\Components' => __DIR__ . '/../..' . '/src/pmmain/Bmm/Component.php',
         'Dplus\\Mpm\\Pmmain\\Bmm\\Header' => __DIR__ . '/../..' . '/src/pmmain/Bmm/Header.php',
+        'Dplus\\Mpm\\Pmmain\\Bmm\\Response' => __DIR__ . '/../..' . '/src/pmmain/Bmm/Response.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
