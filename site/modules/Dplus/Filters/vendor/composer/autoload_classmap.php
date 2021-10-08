3603cc71ba1b1ff654ad5eb1eee461c0ac3084c6
Paul <pauldro952@outlook.com>
2021-10-08 16:03:16 -0500

    Create Bom Header filter

--- site/modules/Dplus/Filters/vendor/composer/autoload_classmap.php
+++ site/modules/Dplus/Filters/vendor/composer/autoload_classmap.php
@@ -27,6 +27,7 @@ return array(
     'Dplus\\Filters\\Misc\\CountryCode' => $baseDir . '/src/Misc/CountryCode.php',
     'Dplus\\Filters\\Misc\\PhoneBook' => $baseDir . '/src/Misc/PhoneBook.php',
     'Dplus\\Filters\\Mki\\Kim' => $baseDir . '/src/Mki/Kim.php',
+    'Dplus\\Filters\\Mpm\\Bom\\Header' => $baseDir . '/src/Mpm/Bom/Header.php',
     'Dplus\\Filters\\Mpo\\ApInvoice' => $baseDir . '/src/Mpo/ApInvoice.php',
     'Dplus\\Filters\\Mpo\\PurchaseOrder' => $baseDir . '/src/Mpo/PurchaseOrder.php',
     'Dplus\\Filters\\Mqo\\Quote' => $baseDir . '/src/Mqo/Quote.php',
