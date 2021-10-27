0f22e1e189773742732e8ba75a40455f35e8822f
Paul <pauldro952@outlook.com>
2021-10-27 13:32:51 -0500

    Create codetable class for TTM

---
+++ site/modules/Dplus/CodeTables/src/mgl/Ttm.php
@@ -0,0 +1,106 @@
+<?php namespace Dplus\Codes\Mgl;
+// Propel Classes
+use Propel\Runtime\Collection\ObjectCollection;
+use Propel\Runtime\ActiveRecord\ActiveRecordInterface as Code;
+// ProcessWire
+use ProcessWire\WireData, ProcessWire\WireInput;
+// Dplus Models
+use GLTextCodeQuery, GLTextCode;
+// Dplus Codes
+use Dplus\Codes\Base;
+use Dplus\Codes\Response;
+
+/**
+ * Class that handles the CRUD of the TTM code table
+ */
+class Ttm extends Base {
+	const MODEL              = 'GLTextCode';
+	const MODEL_KEY          = 'id';
+	const MODEL_TABLE        = 'gl_text_code';
+	const DESCRIPTION        = 'Statement Text Code';
+	const DESCRIPTION_RECORD = 'Statement Text Code';
+	const RESPONSE_TEMPLATE  = 'Statement Text Code {code} {not} {crud}';
+	const RECORDLOCKER_FUNCTION = 'text';
+	const DPLUS_TABLE           = 'TTM';
+	const FIELD_ATTRIBUTES = [
+		'code'        => ['type' => 'text', 'maxlength' => 6],
+		'text'        => ['type' => 'text', 'maxlength' => 20],
+	];
+
+	protected static $instance;
+
+	/**
+	 * Return Array ready for JSON
+	 * @param  Code  $code Code
+	 * @return array
+	 */
+	public function codeJson(Code $code) {
+		return [
+			'code' => $code->code,
+			'text1' => $code->text1,
+			'text2' => $code->text2,
+			'text3' => $code->text3,
+			'text4' => $code->text4,
+		];
+	}
+
+/* =============================================================
+	CRUD Read, Validate Functions
+============================================================= */
+	/**
+	 * Return the IDs for the Work Center Confirm Code
+	 * @return array
+	 */
+	public function ids() {
+		$q = $this->query();
+		$q->select(GLTextCode::aliasproperty('id'));
+		return $q->find()->toArray();
+	}
+
+	/**
+	 * Return the Code records from Database
+	 * @return ObjectCollection
+	 */
+	public function codes() {
+		$q = $this->getQueryClass();
+		return $q->find();
+	}
+
+/* =============================================================
+	CRUD Creates
+============================================================= */
+	/**
+	 * Return New Code
+	 * @return GLTextCode
+	 */
+	public function new($id = '') {
+		$code = new GLTextCode();
+		if (empty($id) === false && strtolower($id) != 'new') {
+			$id = $this->wire('sanitizer')->text($id, ['maxLength' => $this->fieldAttribute('code', 'maxlength')]);
+			$code->setId($id);
+		}
+		return $code;
+	}
+
+/* =============================================================
+	CRUD Processing
+============================================================= */
+	/**
+	 * Update Record with Input Data
+	 * @param  WireInput $input Input Data
+	 * @param  Code      $code
+	 * @return array
+	 */
+	protected function _inputUpdate(WireInput $input, Code $code) {
+		$rm = strtolower($input->requestMethod());
+		$values = $input->$rm;
+		$invalidfields = parent::_inputUpdate($input, $code);
+		$dcm = Dcm::getInstance();
+		if ($values->text('workcenterid') != '' && $dcm->exists($values->text('workcenterid')) === false) {
+			$invalidfields['workcenterid'] = "Work Center";
+			return $invalidfields;
+		}
+		$code->setWorkcenterid($values->text('workcenterid'));
+		return $invalidfields;
+	}
+}
--- site/modules/Dplus/CodeTables/vendor/composer/autoload_classmap.php
+++ site/modules/Dplus/CodeTables/vendor/composer/autoload_classmap.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Dplus\\Codes\\Base' => $baseDir . '/src/Base.php',
+    'Dplus\\Codes\\Mgl\\Ttm' => $baseDir . '/src/mgl/Ttm.php',
     'Dplus\\Codes\\Mpm\\Dcm' => $baseDir . '/src/mpm/Dcm.php',
     'Dplus\\Codes\\Mpm\\Rcm' => $baseDir . '/src/mpm/Rcm.php',
     'Dplus\\Codes\\Mpo\\Cnfm' => $baseDir . '/src/mpo/Cnfm.php',
