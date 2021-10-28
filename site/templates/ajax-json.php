4ffb1a31814cb045bd6141a19c28f0fcde68bf4e
Paul <pauldro952@outlook.com>
2021-10-28 13:50:42 -0500

    Update ajax-json.php

--- site/templates/ajax-json.php
+++ site/templates/ajax-json.php
@@ -103,6 +103,7 @@
 				['GET', 'ttm/code/', Json\Mgl::class, 'validateStmtCode'],
 				['GET', 'dtm/code/', Json\Mgl::class, 'validateDistCode'],
 			],
+			['GET', 'gl-code/', Json\Mgl::class, 'getGlCode'],
 			['GET', 'ttm/code/', Json\Mgl::class, 'getStmtCode'],
 			['GET', 'dtm/code/', Json\Mgl::class, 'getDistCode'],
 		],
