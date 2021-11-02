5c4ce26fcb8bb35b6c2bcd4d8fb12abe4d44e778
Paul <pauldro952@outlook.com>
2021-11-02 12:15:18 -0500

    FEAT: add mhm code table

--- site/modules/Dplus/CodeTables/vendor/composer/autoload_classmap.php
+++ site/modules/Dplus/CodeTables/vendor/composer/autoload_classmap.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'Dplus\\Codes\\Base' => $baseDir . '/src/Base.php',
     'Dplus\\Codes\\Mgl\\Dtm' => $baseDir . '/src/mgl/Dtm.php',
+    'Dplus\\Codes\\Mgl\\Mhm' => $baseDir . '/src/mgl/Mhm.php',
     'Dplus\\Codes\\Mgl\\Ttm' => $baseDir . '/src/mgl/Ttm.php',
     'Dplus\\Codes\\Mpm\\Dcm' => $baseDir . '/src/mpm/Dcm.php',
     'Dplus\\Codes\\Mpm\\Rcm' => $baseDir . '/src/mpm/Rcm.php',
