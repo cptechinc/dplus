0d8a4f6fe0d72d0ca40490b3e0bda79aac70dc47
Paul <pauldro952@outlook.com>
2021-11-03 09:38:43 -0500

    FIX: add position and exists queries

--- site/modules/Dplus/Filters/src/Msa/NotePredefined.php
+++ site/modules/Dplus/Filters/src/Msa/NotePredefined.php
@@ -1,4 +1,6 @@
 <?php namespace Dplus\Filters\Msa;
+// PHP Core
+use PDO;
 // Dplus Model
 use NotePreDefinedQuery, NotePreDefined as Model;
 // ProcessWire Classes
@@ -42,5 +44,49 @@ class NotePreDefined extends AbstractFilter {
 /* =============================================================
 	4. Misc Query Functions
 ============================================================= */
+	/**
+	 * Return if Note ID Exists
+	 * @param  string $id Note ID
+	 * @return bool
+	 */
+	public function exists($id) {
+		return boolval($this->getQueryClass()->filterById($id)->count());
+	}
 
+	/**
+	 * Return Position of Item in results
+	 * @param  Model|string $note NotePredefined|Note ID
+	 * @return int
+	 */
+	public function positionQuick($note) {
+		$id = $note;
+		if (is_object($note)) {
+			$id = $note->id;
+		}
+		$q = $this->getQueryClass()->executeQuery('SET @rownum = 0');
+		$table = $this->getPositionSubSql();
+		$col = Model::aliasproperty('id');
+
+		$sql = "SELECT x.position FROM ($table) x WHERE $col = :itemid";
+		$stmt = $this->getPreparedStatementWrapper($sql);
+		$stmt->bindValue(':itemid', $id, PDO::PARAM_STR);
+		$stmt->execute();
+		return $stmt->fetchColumn();
+	}
+
+	/**
+	 * Return Sub Query for getting result set with custid and position
+	 * @return string
+	 */
+	private function getPositionSubSql() {
+		$table = $this->query->getTableMap()::TABLE_NAME;
+		$col = Model::aliasproperty('id');
+		$sql = "SELECT $col, @rownum := @rownum + 1 AS position FROM $table";
+		$whereClause = $this->getWhereClauseString();
+
+		if (empty($whereClause) === false) {
+			$sql .= ' WHERE ' . $whereClause;
+		}
+		return $sql;
+	}
 }
