cf930daeeeaefc7af872df5e8fed2fc8b0de1e4e
Paul <pauldro952@outlook.com>
2021-11-03 14:30:56 -0500

    FEAT: add MSA options

---
+++ site/modules/Dplus/Filters/src/Msa/SysLoginGroup.php
@@ -0,0 +1,31 @@
+<?php namespace Dplus\Filters\Msa;
+// Dplus Model
+use SysLoginGroupQuery, SysLoginGroup as Model;
+// ProcessWire Classes
+use ProcessWire\WireData, ProcessWire\WireInput, ProcessWire\Page;
+// Dplus Filters
+use Dplus\Filters\CodeFilter;
+
+/**
+* Wrapper Class for SysLoginGroupQuery
+*/
+class SysLoginGroup extends CodeFilter {
+	const MODEL = 'SysLoginGroup';
+
+/* =============================================================
+	1. Abstract Contract / Extensible Functions
+============================================================= */
+
+/* =============================================================
+	2. Base Filter Functions
+============================================================= */
+
+/* =============================================================
+	3. Input Filter Functions
+============================================================= */
+
+/* =============================================================
+	4. Misc Query Functions
+============================================================= */
+
+}
