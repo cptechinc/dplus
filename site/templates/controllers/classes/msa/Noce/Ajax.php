48f86a716709bf2d834546229a7c878530e780d5
Paul <pauldro952@outlook.com>
2021-11-08 10:03:35 -0600

    FEAT: create controller for NOCE  AJAX

---
+++ site/templates/controllers/classes/msa/Noce/Ajax.php
@@ -0,0 +1,81 @@
+<?php namespace Controllers\Msa\Noce;
+// External Libraries, classes
+use Purl\Url as Purl;
+// Propel ORM Library
+use Propel\Runtime\Util\PropelModelPager;
+// Dplus Models
+use ProspectSource;
+// Dplus Filters
+use Dplus\Filters;
+// Dplus CRUD
+use Dplus\Qnotes\Noce as Qnotes;
+// Controllers
+use Controllers\Msa\Base;
+use Controllers\Msa\Noce;
+
+class Ajax extends Base {
+	const DPLUSPERMISSION = '';
+	const SHOWONPAGE = 10;
+
+/* =============================================================
+	Indexes
+============================================================= */
+	public static function index($data) {
+		$fields = ['code|text', 'action|text'];
+		self::sanitizeParametersShort($data, $fields);
+		self::pw('page')->show_breadcrumbs = false;
+		return self::list($data);
+	}
+
+
+	private static function list($data) {
+		$fields = ['q|text'];
+		self::sanitizeParametersShort($data, $fields);
+		$page   = self::pw('page');
+		$filter = new Filters\Msa\NotePreDefined();
+		$filter->filterSummarized();
+
+		$page->headline = "Pre-defined Notes";
+
+		if (empty($data->q) === false) {
+			$filter->search($data->q);
+			$page->headline = "NOCE: Searching for '$data->q'";
+		}
+
+		$filter->sortby($page);
+		$notes = $filter->query->paginate(self::pw('input')->pageNum, self::SHOWONPAGE);
+		self::initHooks();
+
+		$html = self::displayList($data, $notes);
+		return $html;
+	}
+
+/* =============================================================
+	URLs
+============================================================= */
+
+
+/* =============================================================
+	Displays
+============================================================= */
+	private static function displayList($data, PropelModelPager $notes) {
+		$config = self::pw('config');
+		$qnotes = self::getQnotes();
+
+		$html  = '';
+		$html .= $config->twig->render('msa/noce/ajax/form.twig');
+		$html .= $config->twig->render('msa/noce/ajax/list.twig', ['qnotes' => $qnotes, 'notes' => $notes]);
+		return $html;
+	}
+
+/* =============================================================
+	Hooks
+============================================================= */
+	public static function initHooks() {
+
+	}
+
+/* =============================================================
+	Supplemental
+============================================================= */
+}
