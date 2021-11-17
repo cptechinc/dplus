e3bd0fe3aa42d457b9765081507d9f766e8bf117
Paul <pauldro952@outlook.com>
2021-11-17 11:08:44 -0600

    Add js variable for api

--- site/templates/_foot-blank.php
+++ site/templates/_foot-blank.php
@@ -4,6 +4,9 @@
 		<?php foreach($config->scripts->unique() as $script) : ?>
 			<script src="<?= $script; ?>"></script>
 		<?php endforeach; ?>
+		<script>
+			var api  = <?= json_encode($config->js('api')); ?>;
+		</script>
 		<?php if ($page->js) : ?>
 			<script>
 				<?= $page->js; ?>
---
+++ site/templates/_init.js.php
@@ -0,0 +1,11 @@
+<?php
+
+$config->js('api', [
+	'urls' => [
+		'mdm' => [
+			'docs' => [
+				'copier' => $page->jsonApiUrl('mdm/docs/copier')
+			]
+		]
+	]
+]);
