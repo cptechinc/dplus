84cf47e2e28cb64fcca8f4d8596278d2ae839d58
Paul <pauldro952@outlook.com>
2022-01-25 14:30:07 -0600

    Update Roptm.php

--- site/templates/controllers/classes/mar/armain/Roptm.php
+++ site/templates/controllers/classes/mar/armain/Roptm.php
@@ -3,9 +3,9 @@ use Purl\Url as Purl;
 // Propel ORM Ljbrary
 use Propel\Runtime\Util\PropelModelPager;
 // Dplus Model
-use SalesPerson;
+use MsaSysopCode;
 // ProcessWire Classes, Modules
-use ProcessWire\Page, ProcessWire\Roptm as RoptmManager;
+use ProcessWire\Page;
 // Dplus Validators
 use Dplus\Filters;
 // Dplus Codes
@@ -14,13 +14,13 @@ use Dplus\Codes;
 use Mvc\Controllers\Controller;
 
 class Roptm extends Controller {
-	private static $roptm;
+	const SYSTEM = 'AR';
 
 /* =============================================================
 	Indexes
 ============================================================= */
 	public static function index($data) {
-		$fields = ['id|text', 'action|text'];
+		$fields = ['sysop|text', 'code|text', 'action|text'];
 		$data = self::sanitizeParametersShort($data, $fields);
 		$page = self::pw('page');
 		$page->show_breadcrumbs = false;
@@ -29,39 +29,44 @@ class Roptm extends Controller {
 			return self::handleCRUD($data);
 		}
 
-		if (empty($data->id) === false) {
-			return self::salesperson($data);
+		if (empty($data->sysop) === false) {
+			return self::sysop($data);
 		}
-		return self::list($data);
+		return self::listSysops($data);
 	}
 
-	public static function salesperson($data) {
-		$data = self::sanitizeParametersShort($data, ['id|text', 'action|text']);
-		if ($data->action) {
-			return self::handleCRUD($data);
-		}
-		$salesrep = self::getRoptm()->get_create($data->id);
-		self::pw('page')->js .= self::pw('config')->twig->render('mar/armain/roptm/form/.js.twig');
-		return self::salespersonDisplay($data, $salesrep);
+	public static function sysop($data) {
+		$page = self::pw('page');
+		$sysop = self::getSysop()->code(self::SYSTEM, $data->sysop);
+
+		$page->headline = "ROPTM: $data->sysop Optional Codes";
+
+		$filter = new Filters\Msa\SysopOptionalCode();
+		$filter->system(self::SYSTEM);
+		$filter->query->filterBySysop($data->sysop);
+		$filter->sortby($page);
+		$codes = $filter->query->paginate(self::pw('input')->pageNum, self::pw('session')->display);
+
+		self::pw('page')->js .= self::pw('config')->twig->render('mar/armain/roptm/sysop/edit/js.twig');
+		return self::displaySysop($data, $sysop, $codes);
 	}
 
-	public static function list($data) {
+	public static function listSysops($data) {
 		$data = self::sanitizeParametersShort($data, ['q|text']);
 		$page = self::pw('page');
-		// $roptm  = self::getRoptm();
-		// $roptm->recordlocker->deleteLock();
+		self::getSysop()->recordlocker->deleteLock();
 
 		$filter = new Filters\Msa\MsaSysopCode();
-		$filter->system('IN');
+		$filter->system(self::SYSTEM);
 
 		if ($data->q) {
-			$page->headline = "ROPTM: Searching '$data->q'";
+			$page->headline = "ROPTM: Searching Sysop '$data->q'";
 			$filter->search(strtoupper($data->q));
 		}
 		$filter->sortby($page);
 		$codes = $filter->query->paginate(self::pw('input')->pageNum, self::pw('session')->display);
 		// $page->js .= self::pw('config')->twig->render('mar/armain/roptm/list/.js.twig');
-		return self::displayList($data, $codes);
+		return self::displaySysopList($data, $codes);
 	}
 
 /* =============================================================
@@ -82,32 +87,19 @@ class Roptm extends Controller {
 /* =============================================================
 	Displays
 ============================================================= */
-	private static function salespersonDisplay($data, SalesPerson $salesrep) {
-		$roptm  = self::getRoptm();
-		$html =  self::lockUser($roptm, $salesrep);
-		$html .= self::pw('config')->twig->render('mar/armain/roptm/form/page.twig', ['person' => $salesrep, 'roptm' => $roptm]);
-		return $html;
-	}
-
-	private static function lockUser(RoptmManager $roptm, SalesPerson $person) {
-		$roptm = self::getRoptm();
-		$html = '';
-
-		if ($roptm->recordlocker->isLocked($person->id) && $roptm->recordlocker->userHasLocked($person->id) === false) {
-			$msg = "Sales Person $person->id is being locked by " . $roptm->recordlocker->getLockingUser($person->id);
-			$html .= self::pw('config')->twig->render('util/alert.twig', ['type' => 'warning', 'title' => "Saless Person $person->id is locked", 'iconclass' => 'fa fa-lock fa-2x', 'message' => $msg]);
-			$html .= '<div class="mb-3"></div>';
-		} elseif ($roptm->recordlocker->isLocked($person->id) === false) {
-			$roptm->recordlocker->lock($person->id);
-		}
-		return $html;
-	}
-
-	private static function displayList($data, PropelModelPager $codes) {
+	private static function displaySysop($data, MsaSysopCode $sysop, PropelModelPager $codes) {
 		$config = self::pw('config');
 
 		$html = '';
-		$html .= $config->twig->render('mar/armain/roptm/list/page.twig', ['sysop' => self::getSysop(), 'codes' => $codes]);
+		$html .= $config->twig->render('mar/armain/roptm/sysop/display.twig', ['sysop' => $sysop, 'codes' => $codes]);
+		return $html;
+	}
+
+	private static function displaySysopList($data, PropelModelPager $codes) {
+		$config = self::pw('config');
+
+		$html = '';
+		$html .= $config->twig->render('mar/armain/roptm/list/page.twig', ['sysopM' => self::getSysop(), 'codes' => $codes]);
 		$html .= $config->twig->render('util/paginator/propel.twig', ['pager' => $codes]);
 		return $html;
 	}
@@ -116,10 +108,7 @@ class Roptm extends Controller {
 	Classes, Module Getters
 ============================================================= */
 	public static function getRoptm() {
-		if (empty(self::$roptm)) {
-			self::$roptm = self::pw('modules')->get('Roptm');
-		}
-		return self::$roptm;
+		return Codes\Mar\Roptm::getInstance();
 	}
 
 	public static function getSysop() {
@@ -129,20 +118,10 @@ class Roptm extends Controller {
 /* =============================================================
 	URLs
 ============================================================= */
-	public static function codeUrl($id) {
+	public static function sysopUrl($id) {
 		$url = new Purl(self::url());
-		$url->query->set('code', $id);
+		$url->query->set('sysop', $id);
 		return $url->getUrl();
-	}
-
-	public static function codeDeleteUrl($id) {
-		$url = new Purl(self::codeUrl($id));
-		$url->query->set('action', 'delete');
-		return $url->getUrl();
-	}
-
-	public static function codeAddUrl() {
-		return self::codeUrl('new');
 	}
 
 	public static function codeListUrl($focus = '') {
@@ -163,39 +142,14 @@ class Roptm extends Controller {
 		return self::pw('pages')->get('pw_template=roptm')->url;
 	}
 
-	public static function redirectUrl($data) {
-		if ($data->action == 'update') {
-			$response = self::pw('session')->getFor('response', 'roptm');
-
-			if ($response) {
-				if ($response->has_success()) {
-					return self::codeListUrl($data->id);
-				}
-				return self::codeUrl($data->id);
-			}
-		}
-		return self::repListUrl();
-	}
-
 /* =============================================================
 	Hooks
 ============================================================= */
 	public static function initHooks() {
 		$m = self::pw('modules')->get('DpagesMar');
 
-		$m->addHook('Page(pw_template=roptm)::codeEditUrl', function($event) {
-			$id = $event->arguments(0);
-			$event->return = self::codeUrl($id);
-		});
-
-		$m->addHook('Page(pw_template=roptm)::codeDeleteUrl', function($event) {
-			$id = $event->arguments(0);
-			$event->return = self::codeDeleteUrl($id);
-		});
-
-		$m->addHook('Page(pw_template=roptm)::codeAddUrl', function($event) {
-			$id = $event->arguments(0);
-			$event->return = self::codeAddUrl();
+		$m->addHook('Page(pw_template=roptm)::sysopUrl', function($event) {
+			$event->return = self::sysopUrl($event->arguments(0));
 		});
 
 		$m->addHook('Page(pw_template=roptm)::codeListUrl', function($event) {
