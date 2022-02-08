b7f83b09579577a493ed3871784566c4340a71f2
Paul <pauldro952@outlook.com>
2022-02-08 08:54:45 -0600

    Update Ioptm.php

--- site/templates/controllers/classes/min/inmain/Ioptm.php
+++ site/templates/controllers/classes/min/inmain/Ioptm.php
@@ -1,1 +1,220 @@
+<?php namespace Controllers\Min\Inmain;
+use Purl\Url as Purl;
+// Propel ORM Ljbrary
+use Propel\Runtime\Util\PropelModelPager;
+// Dplus Model
+use MsaSysopCode;
+// ProcessWire Classes, Modules
+use ProcessWire\Page;
+// Dplus Validators
+use Dplus\Filters;
+// Dplus Codes
+use Dplus\Codes;
+// Mvc Controllers
+use Mvc\Controllers\Controller;
 
+class Ioptm extends Controller {
+	const SYSTEM = 'IN';
+
+/* =============================================================
+	Indexes
+============================================================= */
+	public static function index($data) {
+		$fields = ['sysop|text', 'code|text', 'action|text'];
+		$data = self::sanitizeParametersShort($data, $fields);
+		$page = self::pw('page');
+		$page->show_breadcrumbs = false;
+
+		if (empty($data->action) === false) {
+			return self::handleCRUD($data);
+		}
+
+		if (empty($data->sysop) === false) {
+			return self::sysop($data);
+		}
+		return self::listSysops($data);
+	}
+
+	private static function sysop($data) {
+		$page  = self::pw('page');
+		$sysop = self::getSysop()->code(self::SYSTEM, $data->sysop);
+		$page->headline = "IOPTM: $data->sysop Optional Codes";
+
+		$filter = self::getFilterSysopOptions($data->sysop);
+		$filter->sortby($page);
+		$codes = $filter->query->paginate(self::pw('input')->pageNum, self::pw('session')->display);
+		self::getIoptm()->recordlocker->deleteLock();
+
+		self::pw('page')->js .= self::pw('config')->twig->render('code-tables/min/ioptm/sysop/edit/js.twig', ['ioptm' => self::getIoptm()]);
+		$html = self::displaySysop($data, $sysop, $codes);
+		self::getIoptm()->deleteResponse();
+		return $html;
+	}
+
+	private static function listSysops($data) {
+		self::sanitizeParametersShort($data, ['q|text']);
+		$page = self::pw('page');
+		self::getSysop()->recordlocker->deleteLock();
+
+		$filter = self::getFilterSysop();
+
+		if ($data->q) {
+			$page->headline = "IOPTM: Searching Sysop '$data->q'";
+			$filter->search(strtoupper($data->q));
+		}
+		$filter->sortby($page);
+		$codes = $filter->query->paginate(self::pw('input')->pageNum, self::pw('session')->display);
+
+		self::pw('page')->js .= self::pw('config')->twig->render('code-tables/min/ioptm/list/.js.twig');
+		$html = self::displaySysopList($data, $codes);
+		self::getIoptm()->deleteResponse();
+		return $html;
+	}
+
+/* =============================================================
+	CRUD
+============================================================= */
+	public static function handleCRUD($data) {
+		$fields = ['action|text', 'sysop|text', 'code|text'];
+		self::sanitizeParameters($data, $fields);
+		$url = self::url();
+
+		if ($data->action) {
+			self::getIoptm()->processInput(self::pw('input'));
+
+			switch ($data->action) {
+				case 'update':
+					$url = self::sysopFocusUrl($data->sysop, $data->code);
+					break;
+				default:
+					$url = self::sysopUrl($data->sysop);
+					break;
+			}
+		}
+
+		self::pw('session')->redirect($url, $http301 = false);
+	}
+
+/* =============================================================
+	Displays
+============================================================= */
+	private static function displaySysop($data, MsaSysopCode $sysop, PropelModelPager $codes) {
+		$config = self::pw('config');
+
+		$html = '';
+		$html .= $config->twig->render('code-tables/min/ioptm/sysop/display.twig', ['ioptm' => self::getIoptm(), 'sysop' => $sysop, 'codes' => $codes]);
+		return $html;
+	}
+
+	private static function displaySysopList($data, PropelModelPager $codes) {
+		$config = self::pw('config');
+
+		$html = '';
+		$html .= $config->twig->render('code-tables/min/ioptm/list/page.twig', ['sysopM' => self::getSysop(), 'ioptm' => self::getIoptm(), 'codes' => $codes]);
+		return $html;
+	}
+
+	private static function displayResponse($data) {
+		$response = self::getIoptm()->getResponse();
+
+		if (empty($response)) {
+			return '';
+		}
+		return self::pw('config')->twig->render('code-tables/response.twig', ['response' => $response]);
+	}
+
+
+/* =============================================================
+	Classes, Module Getters
+============================================================= */
+	public static function getIoptm() {
+		return Codes\Min\Ioptm::getInstance();
+	}
+
+	public static function getSysop() {
+		return Codes\Msa\Sysop::getInstance();
+	}
+
+	public static function getFilterSysop() {
+		$filter = new Filters\Msa\MsaSysopCode();
+		$filter->system(self::SYSTEM);
+		return $filter;
+	}
+
+	public static function getFilterSysopOptions($sysop = '') {
+		$filter = new Filters\Msa\SysopOptionalCode();
+		$filter->system(self::SYSTEM);
+
+		if ($sysop) {
+			$filter->query->filterBySysop($sysop);
+		}
+		return $filter;
+	}
+
+/* =============================================================
+	URLs
+============================================================= */
+	public static function sysopUrl($id) {
+		$url = new Purl(self::url());
+		$url->query->set('sysop', $id);
+		return $url->getUrl();
+	}
+
+	public function sysopFocusUrl($sysop, $focus) {
+		if (empty($focus) || self::getIoptm()->exists($sysop, $focus) === false) {
+			return self::sysopUrl($sysop);
+		}
+		$filter   = self::getFilterSysopOptions($sysop);
+		$position = $filter->positionQuick($focus);
+
+		$url = new Purl(self::sysopUrl($sysop));
+		$url = self::pw('modules')->get('Dpurl')->paginate($url, 'ioptm', self::getPagenbrFromOffset($position, self::pw('session')->display));
+		$url->query->set('focus', $focus);
+		return $url->getUrl();
+	}
+
+	public static function codeDeleteUrl($sysop, $code) {
+		$url = new Purl(self::sysopUrl($sysop));
+		$url->query->set('action', 'delete');
+		$url->query->set('code', $code);
+		return $url->getUrl();
+	}
+
+	public static function url() {
+		return self::pw('pages')->get('pw_template=ioptm')->url;
+	}
+
+	public static function urlFocus($focus = '') {
+		$sysopM = self::getSysop();
+
+		if (empty($focus) || $sysopM->exists(self::SYSTEM, $focus) === false) {
+			return self::url();
+		}
+		$sysop = $sysopM->code(self::SYSTEM, $focus);
+		$filter   = self::getFilterSysop();
+		$position = $filter->positionQuick($sysop);
+		$url = new Purl(self::url());
+		$url = self::pw('modules')->get('Dpurl')->paginate($url, 'ioptm', self::getPagenbrFromOffset($position, self::pw('session')->display));
+		$url->query->set('focus', $focus);
+		return $url->getUrl();
+	}
+
+/* =============================================================
+	Hooks
+============================================================= */
+	public static function initHooks() {
+		$m = self::pw('modules')->get('DpagesMar');
+
+		$m->addHook('Page(pw_template=ioptm)::sysopUrl', function($event) {
+			$event->return = self::sysopUrl($event->arguments(0));
+		});
+
+		$m->addHook('Page(pw_template=ioptm)::codeDeleteUrl', function($event) {
+			$event->return = self::codeDeleteUrl($event->arguments(0), $event->arguments(1));
+		});
+
+		$m->addHook('Page(pw_template=ioptm)::ioptmUrl', function($event) {
+			$event->return = self::urlFocus($event->arguments(0));
+		});
+	}
+}
