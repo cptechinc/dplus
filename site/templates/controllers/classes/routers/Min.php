17426f7c3fa7a71c732ffc2d89f6e62e05c4971b
Paul <pauldro952@outlook.com>
2022-02-24 12:13:50 -0600

    Add routes for min codetables

--- site/templates/controllers/classes/routers/Min.php
+++ site/templates/controllers/classes/routers/Min.php
@@ -1,9 +1,21 @@
 <?php namespace Controllers\Routers;
 // Mvc Controllers
 use Controllers\Min\Inproc;
+use Controllers\Min\Inmain;
 
 class Min extends Base {
 	const ROUTES = [
-		'iarn' => ['', Inproc\Iarn::class, 'iarnUrl']
+		'iarn'   => ['', Inproc\Iarn::class, 'iarnUrl'],
+		'addm'   => ['', Inmain\Menu::class, 'addmUrl'],
+		'csccm'  => ['', Inmain\Menu::class, 'csccmUrl'],
+		'iasm'   => ['', Inmain\Menu::class, 'iasmUrl'],
+		'igcm'   => ['', Inmain\Menu::class, 'igcmUrl'],
+		'igm'    => ['', Inmain\Menu::class, 'igmUrl'],
+		'igpm'   => ['', Inmain\Menu::class, 'igpmUrl'],
+		'iplm'   => ['', Inmain\Menu::class, 'iplmUrl'],
+		'msdsm'  => ['', Inmain\Menu::class, 'msdsmUrl'],
+		'spit'   => ['', Inmain\Menu::class, 'spitUrl'],
+		'stcm'   => ['', Inmain\Menu::class, 'stcmUrl'],
+		'umm'    => ['', Inmain\Menu::class, 'ummUrl'],
 	];
 }
