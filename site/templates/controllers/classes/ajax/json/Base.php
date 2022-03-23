e1dad7d520889e5a03179d1291a5277855cc3657
Paul <pauldro952@outlook.com>
2022-03-23 09:39:18 -0500

    Add functions for CRUD manager json

--- site/templates/controllers/classes/ajax/json/Base.php
+++ site/templates/controllers/classes/ajax/json/Base.php
@@ -2,6 +2,8 @@
 // Dplus Codes
 use Dplus\Codes;
 use Dplus\Codes\Base as CodeTable;
+// Dplus Crud
+use Dplus\Crud\Manager as CrudManager;
 // Mvc Controllers
 use Mvc\Controllers\Controller;
 
@@ -59,5 +61,56 @@ class Base extends Controller {
 			return false;
 		}
 		return $manager->codeJson($manager->code($data->code));
+	}
+
+/* =============================================================
+	Abstracted CRUDManager functions
+============================================================= */
+	/**
+	 * Validate Simple Ids using CrudManager
+	 * NOTE: only works for Simple Keys
+	 * @param  CrudManager $manager  CrudManager
+	 * @param  object      $data
+	 *                          ->id  (string) Key
+	 *                          ->jqv   (bool)   Send Response in JqueryValidate format
+	 *                          ->new   (bool)   Validate if key can be used for a new code
+	 * @return mixed
+	 */
+	protected static function validateCrudManagerSimpleId(CrudManager $manager, $data) {
+		$fields = ['id|text', 'jqv|bool', 'new|bool'];
+		self::sanitizeParametersShort($data, $fields);
+
+		$exists = $manager->exists($data->id);
+
+		if (boolval($data->jqv) === false) {
+			return boolval($data->new) ? $exists === false : $exists;
+		}
+
+		if (boolval($data->new) === true) {
+			return $exists === false ? true : $manager::DESCRIPTION . " $data->id already exists";
+		}
+
+		if ($exists === false) {
+			return $manager::DESCRIPTION . " $data->id not found";
+		}
+		return true;
+	}
+
+	/**
+	 * Return Code JSON, using CrudManager
+	 * NOTE: only works for Simple Keys
+	 * @param  CrudManager $manager CrudManager
+	 * @param  object      $data
+	 *                          ->id  (string) Key
+	 * @return false|array
+	 */
+	protected static function getCrudManagerSimpleRecord(CrudManager $manager, $data) {
+		$fields = ['id|text'];
+		self::sanitizeParametersShort($data, $fields);
+
+		if ($manager->exists($data->id) === false) {
+			return false;
+		}
+		return $manager->recordJson($manager->record($data->id));
 	}
 }
