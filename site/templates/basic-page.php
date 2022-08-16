b0c3cf669d4cb1ce5a8d2a4ef57b1de07a12e288
Paul <pauldro952@outlook.com>
2022-08-16 14:22:28 -0500

    Display page title in breadcrumbs

--- site/templates/basic-page.php
+++ site/templates/basic-page.php
@@ -2,12 +2,14 @@
 	$parents = $page->parents();
 ?>
 <?php include('./_head.php'); ?>
-	<div class="jumbotron bg-dark page-banner rounded-0 mb-3">
-		<div class="container">
-			<h1 class="display-4 text-light"><?= $page->get('pagetitle|headline|title'); ?></h1>
+	<?php if (100 == 1) :?>
+		<div class="jumbotron bg-dark page-banner rounded-0 mb-3">
+			<div class="container">
+				<h1 class="display-4 text-light"><?= $page->get('pagetitle|headline|title'); ?></h1>
+			</div>
 		</div>
-	</div>
-	<div class="container">
+	<?php endif; ?>
+	<div class="container mt-3">
 		<?php if ($page->show_breadcrumbs) : ?>
 			<?= $config->twig->render('util/bread-crumbs.twig', ['page' => $page]); ?>
 		<?php endif; ?>
