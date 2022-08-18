27e7a6848782512047bfa3e06d010df2f797e128
Paul <pauldro952@outlook.com>
2022-08-18 09:36:05 -0500

    Create code-table.js

---
+++ site/templates/scripts/code-tables/code-table.js
@@ -0,0 +1,20 @@
+$(function() {
+	var uri = URI();
+	var queryData = uri.query(true);
+
+	$('#edit-code-modal').on('hidden.bs.modal', function (event) {
+		var modal = $(this);
+		var button = $(event.relatedTarget); // Button that triggered the modal
+		var code = modal.find('input[name=code]').val().toString();
+
+		$('#code-table-alert').hide();
+		$('.bg-success').removeClass('bg-success text-white');
+		$('.highlight').removeClass('highlight');
+		$('.code[data-code="'+code+'"]').addClass('highlight');
+	});
+
+	if (queryData.hasOwnProperty('focus') && queryData.focus != 'new') {
+		$('.code[data-code="' + queryData.focus + '"]').addClass('highlight');
+		$('html, body').animate({scrollTop: $('.code[data-code="' + queryData.focus + '"]').offset().top,},700,'linear');
+	}
+});
