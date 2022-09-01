04b236b902f380b4de33f057c130c7b5a489e436
Paul <pauldro952@outlook.com>
2022-09-01 16:18:01 -0500

    Update AbstractCodeTableSimpleEditable.php

--- site/modules/Dplus/CodeTables/src/AbstractCodeTableSimpleEditable.php
+++ site/modules/Dplus/CodeTables/src/AbstractCodeTableSimpleEditable.php
@@ -9,6 +9,9 @@ use ProcessWire\WireInput;
 use Dplus\Codes\Response;
 // Dplus Databases
 use Dplus\Databases\Connectors\Dplus as DbDplus;
+// Dplus Record Locker
+use Dplus\RecordLocker\UserFunction as FunctionLocker;
+
 
 /**
  * AbstractCodeTableSimpleEditable
@@ -16,7 +19,19 @@ use Dplus\Databases\Connectors\Dplus as DbDplus;
  * Handles Editing of single-Keyed CodeTables
  */
 abstract class AbstractCodeTableSimpleEditable extends AbstractCodeTableSimple {
+	const RECORDLOCKER_FUNCTION = '';
+	const RESPONSE_TEMPLATE     = 'Code {code} {not} {crud}';
+	const DPLUS_TABLE           = '';
+	const PERMISSION            = '';
+
 	protected static $instance;
+
+	public function __construct() {
+		$this->sessionID = session_id();
+		$this->recordlocker = new FunctionLocker();
+		$this->recordlocker->setFunction(static::RECORDLOCKER_FUNCTION);
+		$this->recordlocker->setUser($this->wire('user'));
+	}
 
 /* =============================================================
 	CRUD Reads
@@ -59,6 +74,27 @@ abstract class AbstractCodeTableSimpleEditable extends AbstractCodeTableSimple {
 	CRUD Processing
 ============================================================= */
 	/**
+	 * Process Input Data, Update Database
+	 * @param  WireInput $input Input Data
+	 */
+	public function processInput(WireInput $input) {
+		$rm = strtolower($input->requestMethod());
+		$values = $input->$rm;
+
+		switch ($values->text('action')) {
+			case 'delete':
+			case 'delete-code':
+				$this->inputDelete($input);
+				break;
+			case 'update-code':
+			case 'update':
+			case 'edit-code':
+				$this->inputUpdate($input);
+				break;
+		}
+	}
+
+	/**
 	 * Update Code from Input Data
 	 * @param  WireInput $input Input Data
 	 * @return bool
@@ -74,6 +110,25 @@ abstract class AbstractCodeTableSimpleEditable extends AbstractCodeTableSimple {
 		$response      = $this->saveAndRespond($code, $invalidfields);
 		$this->setResponse($response);
 		return $response->hasSuccess();
+	}
+
+	/**
+	 * Update Record with Input Data
+	 * @param  WireInput $input Input Data
+	 * @param  Code      $code
+	 * @return array
+	 */
+	protected function _inputUpdate(WireInput $input, Code $code) {
+		$rm = strtolower($input->requestMethod());
+		$values = $input->$rm;
+
+		if ($code->__isset('description')) { // Some Code tables may not use description
+			$code->setDescription($values->text('description', ['maxLength' => $this->fieldAttribute('description', 'maxlength')]));
+		}
+		$code->setDate(date('Ymd'));
+		$code->setTime(date('His'));
+		$code->setDummy('P');
+		return [];
 	}
 
 	/**
@@ -133,5 +188,119 @@ abstract class AbstractCodeTableSimpleEditable extends AbstractCodeTableSimple {
 	protected function updateDplus($code) {
 		$data = $this->generateRequestData($code);
 		$this->sendDplusRequest($data);
+	}
+
+/* =============================================================
+	CRUD Responses
+============================================================= */
+	/**
+	 * Return Response based on the outcome of the database save
+	 * @param  Code     $code          Code
+	 * @param  array    $invalidfields
+	 * @return Response
+	 */
+	protected function saveAndRespond(Code $code, $invalidfields = []) {
+		$is_new = $code->isDeleted() ? false : $code->isNew();
+		$saved  = $code->isDeleted() ? $code->isDeleted() : $code->save();
+
+		$response = new Response();
+		$response->setCode($code->id);
+		$response->setKey($this->getRecordlockerKey($code));
+
+		if ($saved) {
+			$response->setSuccess(true);
+		} else {
+			$response->setError(true);
+		}
+
+		if ($is_new) {
+			$response->setAction(Response::CRUD_CREATE);
+		} elseif ($code->isDeleted()) {
+			$response->setAction(Response::CRUD_DELETE);
+		} else {
+			$response->setAction(Response::CRUD_UPDATE);
+		}
+		
+		$response->setFields($invalidfields);
+		$this->addResponseMsgReplacements($code, $response);
+		$response->buildMessage(static::RESPONSE_TEMPLATE);
+		
+		if ($response->hasSuccess()) {
+			$this->updateDplus($code);
+		}
+		return $response;
+	}
+
+	/**
+	 * Add Replacements, values for the Response Message
+	 * @param Code     $code      Code
+	 * @param Response $response  Response
+	 */
+	protected function addResponseMsgReplacements(Code $code, Response $response) {
+
+	}
+
+	/**
+	 * Set Session Response
+	 * @param Response $response
+	 */
+	public function setResponse(Response $response) {
+		$this->wire('session')->setFor('response', static::RECORDLOCKER_FUNCTION, $response);
+	}
+
+	/**
+	 * Return Session Response
+	 * @return Response
+	 */
+	public function getResponse() {
+		return $this->wire('session')->getFor('response', static::RECORDLOCKER_FUNCTION);
+	}
+
+	/**
+	 * Delete Session Response
+	 */
+	public function deleteResponse() {
+		$this->wire('session')->removeFor('response', static::RECORDLOCKER_FUNCTION);
+	}
+
+/* =============================================================
+	Permission Functions
+============================================================= */
+	/**
+	 * Validate User Permission
+	 * @return bool
+	 */
+	public function validateUserPermission() {
+		if (empty(static::PERMISSION)) {
+			return true;
+		}
+		$user = $this->user ? $this->user : $this->wire('user');
+		return $user->hasPermissionCode(static::PERMISSION);
+	}
+
+/* =============================================================
+	Record Locker Functions
+============================================================= */
+	/**
+	 * Return Key for Code
+	 * @param  Code   $code
+	 * @return string
+	 */
+	public function getRecordlockerKey(Code $code) {
+		return implode(FunctionLocker::glue(), [$code->id]);
+	}
+
+	/**
+	 * Lock Code
+	 * @param  Code   $code Code
+	 * @return bool
+	 */
+	public function lockrecord(Code $code) {
+		$key = $this->getRecordlockerKey($code);
+
+		if ($this->recordlocker->isLocked($key) === false) {
+			$this->recordlocker->lock($key);
+		}
+		return $this->recordlocker->userHasLocked($key);
 	}
 }
