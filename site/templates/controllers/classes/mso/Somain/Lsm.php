02186473fd82e08ad2ce900a1cd28d0e738a6fd4
Paul <pauldro952@outlook.com>
2022-09-06 14:57:53 -0500

    FEAT: extend AbstractCodeTableController

--- site/templates/controllers/classes/mso/Somain/Lsm.php
+++ site/templates/controllers/classes/mso/Somain/Lsm.php
@@ -1,160 +1,41 @@
 <?php namespace Controllers\Mso\Somain;
-// External Libraries, classes
-use Purl\Url as Purl;
+// ProcessWire 
+use ProcessWire\WireData;
 // Propel ORM Library
 use Propel\Runtime\Util\PropelModelPager;
-// Dplus Models
-use ProspectSource;
-// Dplus Filters
+// Dplus
 use Dplus\Filters;
-// Dplus CRUD
-use Dplus\Codes\Mso\Lsm as LsmManager;
-// Mvc Controllers
-use Controllers\Mso\Somain\Base;
+use Dplus\Codes;
 
-class Lsm extends Base {
+
+class Lsm extends AbstractCodeTableController {
 	const DPLUSPERMISSION = 'lsm';
+	const TITLE      = 'Lost Sales Reason';
+	const SUMMARY    = 'View / Edit Lost Sales Reason Codes';
 	const SHOWONPAGE = 10;
 
-	private static $lsm;
-
-/* =============================================================
-	Indexes
-============================================================= */
-	public static function index($data) {
-		$fields = ['code|text', 'action|text'];
-		self::sanitizeParametersShort($data, $fields);
-		self::pw('page')->show_breadcrumbs = false;
-		self::pw('page')->headline = 'Lost Sales Reason';
-
-		if (empty($data->action) === false) {
-			return self::handleCRUD($data);
-		}
-		return self::list($data);
+	public static function _url() {
+		return Menu::lsmUrl();
 	}
 
-	public static function handleCRUD($data) {
-		$fields = ['code|text', 'action|text'];
-		self::sanitizeParametersShort($data, $fields);
-		$url  = self::lsmUrl();
-		$lsm  = self::getLsm();
-
-		if ($data->action) {
-			$lsm->processInput(self::pw('input'));
-			$url  = self::lsmUrl($data->code);
-		}
-		self::pw('session')->redirect($url, $http301 = false);
+	public static function getCodeFilter() {
+		return new Filters\Mso\LostSalesCode();
 	}
 
-	private static function list($data) {
-		$fields = ['q|text'];
-		self::sanitizeParametersShort($data, $fields);
-		$page   = self::pw('page');
-		$filter = new Filters\Mso\LostSalesCode();
-
-		$page->headline = "Lost Sales Reason";
-
-		if (empty($data->q) === false) {
-			$filter->search($data->q);
-			$page->headline = "LSM: Searching for '$data->q'";
-		}
-
-		$filter->sortby($page);
-		$input = self::pw('input');
-		$codes = $filter->query->paginate($input->pageNum, $input->get->offsetExists('print') ? 0 : self::SHOWONPAGE);
-		self::initHooks();
-
-		self::pw('config')->scripts->append(self::getFileHasher()->getHashUrl('scripts/code-tables/modal-events.js'));
-		$page->js .= self::pw('config')->twig->render('code-tables/mso/lsm/.js.twig', ['lsm' => self::getLsm()]);
-		$html = self::displayList($data, $codes);
-		self::getLsm()->deleteResponse();
-		return $html;
+	public static function getCodeTable() {
+		return Codes\Mso\Lsm::instance();
 	}
 
 /* =============================================================
-	URLs
+	Render HTML / JS
 ============================================================= */
-	public static function lsmUrl($code = '') {
-		if (empty($code)) {
-			return Menu::lsmUrl();
-		}
-		return self::lsmFocusUrl($code);
+	protected static function renderList(WireData $data, PropelModelPager $codes) {
+		$codeTable = static::getCodeTable();
+		return self::pw('config')->twig->render('code-tables/mso/lsm/list.twig', ['manager' => $codeTable, 'codes' => $codes]);
 	}
 
-	public static function lsmFocusUrl($focus) {
-		$filter = new Filters\Mso\LostSalesCode();
-		if ($filter->exists($focus) === false) {
-			return Menu::lsmUrl();
-		}
-		$position = $filter->positionQuick($focus);
-		$pagenbr = self::getPagenbrFromOffset($position, self::SHOWONPAGE);
-
-		$url = new Purl(Menu::lsmUrl());
-		$url->query->set('focus', $focus);
-		$url = self::pw('modules')->get('Dpurl')->paginate($url, 'lsm', $pagenbr);
-		return $url->getUrl();
-	}
-
-	public static function codeDeleteUrl($code) {
-		$url = new Purl(Menu::lsmUrl());
-		$url->query->set('code', $code);
-		$url->query->set('action', 'delete-code');
-		return $url->getUrl();
-	}
-
-/* =============================================================
-	Displays
-============================================================= */
-	private static function displayList($data, PropelModelPager $codes) {
-		$config = self::pw('config');
-		$lsm = self::getLsm();
-
-		$html  = '';
-		$html .= $config->twig->render('code-tables/bread-crumbs.twig');
-		$html .= self::displayResponse($data);
-		$html .= $config->twig->render('code-tables/mso/lsm/list.twig', ['manager' => $lsm, 'codes' => $codes]);
-		if (self::pw('input')->get->offsetExists('print') === false) {
-			$html .= $config->twig->render('util/paginator/propel.twig', ['pager'=> $codes]);
-		}
-		$html .= $config->twig->render('code-tables/mso/lsm/edit-modal.twig', ['manager' => $lsm]);
-		return $html;
-	}
-
-	public static function displayResponse($data) {
-		$lsm = self::getLsm();
-		$response = $lsm->getResponse();
-		if (empty($response)) {
-			return '';
-		}
-		return self::pw('config')->twig->render('code-tables/response.twig', ['response' => $response]);
-	}
-
-/* =============================================================
-	Hooks
-============================================================= */
-	public static function initHooks() {
-		$m = self::pw('modules')->get('Dpages');
-
-		$m->addHook('Page(pw_template=somain)::menuUrl', function($event) {
-			$event->return = Menu::menuUrl();
-		});
-
-		$m->addHook('Page(pw_template=somain)::menuTitle', function($event) {
-			$event->return = Menu::TITLE;
-		});
-
-		$m->addHook('Page(pw_template=somain)::codeDeleteUrl', function($event) {
-			$event->return = self::codeDeleteUrl($event->arguments(0));
-		});
-	}
-
-/* =============================================================
-	Supplemental
-============================================================= */
-	public static function getLsm() {
-		if (empty(self::$lsm)) {
-			self::$lsm = new LsmManager();
-		}
-		return self::$lsm;
+	protected static function renderModal(WireData $data) {
+		$codeTable = static::getCodeTable();
+		return self::pw('config')->twig->render('code-tables/mso/lsm/edit-modal.twig', ['manager' => $codeTable]);
 	}
 }
