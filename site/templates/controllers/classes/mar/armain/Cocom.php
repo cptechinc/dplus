1ec4c0145963db0824fb095d1d48444c9025e0af
Paul <pauldro952@outlook.com>
2022-09-06 10:33:20 -0500

    FEAT: extend AbstractCodeTableController

--- site/templates/controllers/classes/mar/armain/Cocom.php
+++ site/templates/controllers/classes/mar/armain/Cocom.php
@@ -1,157 +1,25 @@
 <?php namespace Controllers\Mar\Armain;
-// External Libraries, classes
-use Purl\Url as Purl;
-// Propel ORM Library
-use Propel\Runtime\Util\PropelModelPager;
 // Dplus Filters
 use Dplus\Filters;
 // Dplus CRUD
-use Dplus\Codes\Mar\Cocom as CocomManager;
-// Mvc Controllers
-use Controllers\Mar\AbstractController as Base;
+use Dplus\Codes;
 
-class Cocom extends Base {
+
+class Cocom extends AbstractCodeTableController {
 	const DPLUSPERMISSION = 'cocom';
+	const TITLE      = 'Country Code';
+	const SUMMARY    = 'View / Edit Country Codes';
 	const SHOWONPAGE = 25;
 
-	private static $cocom;
-
-/* =============================================================
-	Indexes
-============================================================= */
-	public static function index($data) {
-		$fields = ['code|text', 'action|text'];
-		self::sanitizeParametersShort($data, $fields);
-		self::pw('page')->show_breadcrumbs = false;
-		self::pw('page')->headline = 'Country / Currency Code';
-
-		if (empty($data->action) === false) {
-			return self::handleCRUD($data);
-		}
-		return self::list($data);
+	public static function _url() {
+		return Menu::cocomUrl();
 	}
 
-	public static function handleCRUD($data) {
-		$fields = ['code|text', 'action|text'];
-		self::sanitizeParametersShort($data, $fields);
-		$url  = self::cocomUrl();
-		$cocom = self::getCocom();
-
-		if ($data->action) {
-			$cocom->processInput(self::pw('input'));
-			$url = self::cocomUrl($data->code);
-		}
-		self::pw('session')->redirect($url, $http301 = false);
+	public static function getCodeFilter() {
+		return new Filters\Mar\CountryCode();
 	}
 
-	private static function list($data) {
-		$fields = ['q|text', 'col|text'];
-		self::sanitizeParametersShort($data, $fields);
-		$page   = self::pw('page');
-		$page->headline = "Country / Currency Code";
-
-		$filter = new Filters\Mar\CountryCode();
-
-		if (empty($data->q) === false) {
-			$filter->search($data->q, self::pw('sanitizer')->array($data->col, ['delimiter' => ',']));
-		}
-
-		$filter->sortby($page);
-		$input = self::pw('input');
-		$codes = $filter->query->paginate($input->pageNum, $input->get->offsetExists('print') ? 0 : self::SHOWONPAGE);
-		self::initHooks();
-		
-		self::pw('config')->scripts->append(self::getFileHasher()->getHashUrl('scripts/code-tables/modal-events.js'));
-		$page->js .= self::pw('config')->twig->render('code-tables/mar/cocom/.js.twig', ['cocom' => self::getCocom()]);
-		$html = self::displayList($data, $codes);
-		self::getCocom()->deleteResponse();
-		return $html;
-	}
-
-/* =============================================================
-	URLs
-============================================================= */
-	public static function cocomUrl($code = '') {
-		if (empty($code)) {
-			return Menu::cocomUrl();
-		}
-		return self::cocomFocusUrl($code);
-	}
-
-	public static function cocomFocusUrl($focus) {
-		$filter = new Filters\Mar\CountryCode();
-		if ($filter->exists($focus) === false) {
-			return Menu::cocomUrl();
-		}
-		$position = $filter->positionQuick($focus);
-		$pagenbr = self::getPagenbrFromOffset($position, self::SHOWONPAGE);
-
-		$url = new Purl(Menu::cocomUrl());
-		$url->query->set('focus', $focus);
-		$url = self::pw('modules')->get('Dpurl')->paginate($url, 'cocom', $pagenbr);
-		return $url->getUrl();
-	}
-
-	public static function codeDeleteUrl($code) {
-		$url = new Purl(Menu::cocomUrl());
-		$url->query->set('code', $code);
-		$url->query->set('action', 'delete-code');
-		return $url->getUrl();
-	}
-
-/* =============================================================
-	Displays
-============================================================= */
-	private static function displayList($data, PropelModelPager $codes) {
-		$config = self::pw('config');
-		$cocom = self::getCocom();
-
-		$html  = '';
-		$html .= $config->twig->render('code-tables/bread-crumbs.twig');
-		$html .= self::displayResponse($data);
-		$html .= $config->twig->render('code-tables/mar/cocom/list.twig', ['manager' => $cocom, 'codes' => $codes]);
-		if (self::pw('input')->get->offsetExists('print') === false) {
-			$html .= $config->twig->render('util/paginator/propel.twig', ['pager'=> $codes]);
-		}
-		$html .= $config->twig->render('code-tables/mar/cocom/edit-modal.twig', ['manager' => $cocom]);
-		return $html;
-	}
-
-	public static function displayResponse($data) {
-		$cocom = self::getCocom();
-		$response = $cocom->getResponse();
-		if (empty($response)) {
-			return '';
-		}
-		return self::pw('config')->twig->render('code-tables/response.twig', ['response' => $response]);
-	}
-
-/* =============================================================
-	Hooks
-============================================================= */
-	public static function initHooks() {
-		$m = self::pw('modules')->get('Dpages');
-
-		$m->addHook('Page(pw_template=armain)::menuUrl', function($event) {
-			$event->return = Menu::menuUrl();
-		});
-
-		$m->addHook('Page(pw_template=armain)::menuTitle', function($event) {
-			$event->return = Menu::TITLE;
-		});
-
-		$m->addHook('Page(pw_template=armain)::codeDeleteUrl', function($event) {
-			$event->return = self::codeDeleteUrl($event->arguments(0));
-		});
-	}
-
-/* =============================================================
-	Supplemental
-============================================================= */
-	public static function getCocom() {
-		if (empty(self::$cocom)) {
-			self::$cocom = new CocomManager();
-		}
-		return self::$cocom;
+	public static function getCodeTable() {
+		return Codes\Mar\Cocom::instance();
 	}
 }
