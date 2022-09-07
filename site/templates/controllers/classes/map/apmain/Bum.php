0e998b09ec9732efa2dcf5dea2a4479e85909384
Paul <pauldro952@outlook.com>
2022-09-07 13:38:16 -0500

    FEAT: extend AbstractCodeTableController

--- site/templates/controllers/classes/map/apmain/Bum.php
+++ site/templates/controllers/classes/map/apmain/Bum.php
@@ -1,156 +1,45 @@
 <?php namespace Controllers\Map\Apmain;
-// External Libraries, classes
-use Purl\Url as Purl;
+// ProcessWire 
+use ProcessWire\WireData;
 // Propel ORM Library
 use Propel\Runtime\Util\PropelModelPager;
-// Dplus Filters
+// Dplus
 use Dplus\Filters;
-// Dplus CRUD
-use Dplus\Codes\Map\Bum as BumManager;
-// Mvc Controllers
-use Controllers\Map\Apmain\Base;
+use Dplus\Codes;
 
-class Bum extends Base {
+/**
+ * Bum
+ * 
+ * Controller for handling HTTP Requests for the Bum Codetable
+ */
+class Bum extends AbstractCodeTableController {
 	const DPLUSPERMISSION = 'bum';
+	const TITLE      = 'Vendor Buyer Code"';
+	const SUMMARY    = 'View / Edit Vendor Buyer Code"s';
 	const SHOWONPAGE = 10;
 
-	private static $bum;
-
-/* =============================================================
-	Indexes
-============================================================= */
-	public static function index($data) {
-		$fields = ['code|text', 'action|text'];
-		self::sanitizeParametersShort($data, $fields);
-		self::pw('page')->show_breadcrumbs = false;
-
-		if (empty($data->action) === false) {
-			return self::handleCRUD($data);
-		}
-		return self::list($data);
+	public static function _url() {
+		return Menu::bumUrl();
 	}
 
-	public static function handleCRUD($data) {
-		$fields = ['code|text', 'action|text'];
-		self::sanitizeParametersShort($data, $fields);
-		$url  = self::bumUrl();
-		$bum  = self::getBum();
-
-		if ($data->action) {
-			$bum->processInput(self::pw('input'));
-			$url  = self::bumUrl($data->code);
-		}
-		self::pw('session')->redirect($url, $http301 = false);
+	public static function getCodeFilter() {
+		return new Filters\Map\ApBuyer();
 	}
 
-	private static function list($data) {
-		$fields = ['q|text', 'col|text'];
-		self::sanitizeParametersShort($data, $fields);
-		$page   = self::pw('page');
-		$filter = new Filters\Map\ApBuyer();
-
-		$page->headline = "Vendor Buyer Code";
-
-		if (empty($data->q) === false) {
-			$filter->search($data->q, self::pw('sanitizer')->array($data->col, ['delimiter' => ',']));
-		}
-
-		$filter->sortby($page);
-		$input = self::pw('input');
-		$codes = $filter->query->paginate($input->pageNum, $input->get->offsetExists('print') ? 0 : self::SHOWONPAGE);
-		self::initHooks();
-
-		self::pw('config')->scripts->append(self::getFileHasher()->getHashUrl('scripts/code-tables/modal-events.js'));
-		$page->js .= self::pw('config')->twig->render('code-tables/map/bum/.js.twig', ['bum' => self::getBum()]);
-		$html = self::displayList($data, $codes);
-		self::getBum()->deleteResponse();
-		return $html;
+	public static function getCodeTable() {
+		return Codes\Map\Bum::instance();
 	}
 
 /* =============================================================
-	URLs
+	Render HTML / JS
 ============================================================= */
-	public static function bumUrl($code = '') {
-		if (empty($code)) {
-			return Menu::bumUrl();
-		}
-		return self::bumFocusUrl($code);
+	protected static function renderList(WireData $data, PropelModelPager $codes) {
+		$codeTable = static::getCodeTable();
+		return self::pw('config')->twig->render('code-tables/map/bum/list.twig', ['manager' => $codeTable, 'codes' => $codes]);
 	}
 
-	public static function bumFocusUrl($focus) {
-		$filter = new Filters\Map\ApBuyer();
-		if ($filter->exists($focus) === false) {
-			return Menu::bumUrl();
-		}
-		$position = $filter->positionQuick($focus);
-		$pagenbr = self::getPagenbrFromOffset($position, self::SHOWONPAGE);
-
-		$url = new Purl(Menu::bumUrl());
-		$url->query->set('focus', $focus);
-		$url = self::pw('modules')->get('Dpurl')->paginate($url, 'bum', $pagenbr);
-		return $url->getUrl();
-	}
-
-	public static function codeDeleteUrl($code) {
-		$url = new Purl(Menu::bumUrl());
-		$url->query->set('code', $code);
-		$url->query->set('action', 'delete-code');
-		return $url->getUrl();
-	}
-
-/* =============================================================
-	Displays
-============================================================= */
-	private static function displayList($data, PropelModelPager $codes) {
-		$config = self::pw('config');
-		$bum = self::getBum();
-
-		$html  = '';
-		$html .= $config->twig->render('code-tables/bread-crumbs.twig');
-		$html .= self::displayResponse($data);
-		$html .= $config->twig->render('code-tables/map/bum/list.twig', ['manager' => $bum, 'codes' => $codes]);
-		if (self::pw('input')->get->offsetExists('print') === false) {
-			$html .= $config->twig->render('util/paginator/propel.twig', ['pager'=> $codes]);
-		}
-		$html .= $config->twig->render('code-tables/map/bum/edit-modal.twig', ['manager' => $bum]);
-		return $html;
-	}
-
-	public static function displayResponse($data) {
-		$bum = self::getBum();
-		$response = $bum->getResponse();
-		if (empty($response)) {
-			return '';
-		}
-		return self::pw('config')->twig->render('code-tables/response.twig', ['response' => $response]);
-	}
-
-/* =============================================================
-	Hooks
-============================================================= */
-	public static function initHooks() {
-		$m = self::pw('modules')->get('Dpages');
-
-		$m->addHook('Page(pw_template=apmain)::menuUrl', function($event) {
-			$event->return = Menu::menuUrl();
-		});
-
-		$m->addHook('Page(pw_template=apmain)::menuTitle', function($event) {
-			$event->return = Menu::TITLE;
-		});
-
-		$m->addHook('Page(pw_template=apmain)::codeDeleteUrl', function($event) {
-			$event->return = self::codeDeleteUrl($event->arguments(0));
-		});
-	}
-
-/* =============================================================
-	Supplemental
-============================================================= */
-	public static function getBum() {
-		if (empty(self::$bum)) {
-			self::$bum = new BumManager();
-		}
-		return self::$bum;
+	protected static function renderModal(WireData $data) {
+		$codeTable = static::getCodeTable();
+		return self::pw('config')->twig->render('code-tables/map/bum/edit-modal.twig', ['manager' => $codeTable]);
 	}
 }
