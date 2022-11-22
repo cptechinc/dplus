ca76e0c3f58fbda8c921a10faf998ca1f50c721e
Paul <pauldro952@outlook.com>
2022-11-22 08:30:48 -0600

    Use ajax-modal

--- site/templates/scripts/mar/armain/pty3/customer-list.js
+++ site/templates/scripts/mar/armain/pty3/customer-list.js
@@ -11,15 +11,6 @@ $(function() {
 /* =============================================================
 	Lookup Modal Functions
 ============================================================= */
-	ajaxModal.on('show.bs.modal', function (event) {
-		var button = $(event.relatedTarget); // Button that triggered the modal
-		var modal = $(this);
-		modal.attr('data-input', button.data('input'));
-
-		modal.find('.modal-title').text(button.attr('title'));
-		modal.resizeModal('xl');
-		modal.find('.modal-body').loadin(button.data('lookupurl'), function() {});
-	});
 
 	$("body").on('click', '.customer-link', function(e) {
 		e.preventDefault();
@@ -28,23 +19,5 @@ $(function() {
 		queryData.custID = button.data('custid');
 		uri.query(queryData);
 		window.location.href = uri.toString();
-	});
-
-	$("body").on('submit', '#ajax-modal form', function(e) {
-		e.preventDefault();
-		var form = $(this);
-		var query = form.serialize();
-		var action = form.attr('action');
-		var search = form.find('input[name=q]').val();
-		var url = action + '?' + query;
-		form.closest('.modal').find('.modal-title').text('Searching for ' + search);
-		form.closest('.modal').find('.modal-body').loadin(url, function() {});
-	});
-
-	$("body").on('click', '#ajax-modal .paginator-link', function(e) {
-		e.preventDefault();
-		var button = $(this);
-		var modal  = button.closest('.modal');
-		modal.find('.modal-body').load(button.attr('href'));
 	});
 });
