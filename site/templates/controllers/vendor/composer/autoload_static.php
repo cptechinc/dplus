65066ca7540d4653ea564a2cc0d75bfc7716a6c6
Paul <pauldro952@outlook.com>
2022-12-15 13:41:05 -0600

    add util class

--- site/templates/controllers/vendor/composer/autoload_static.php
+++ site/templates/controllers/vendor/composer/autoload_static.php
@@ -24,6 +24,7 @@ class ComposerStaticInit0137f8e60248be1bc1b37e9c1c6812dd
         'Controllers\\Ajax\\Json\\Mqo' => __DIR__ . '/../..' . '/classes/ajax/json/Mqo.php',
         'Controllers\\Ajax\\Json\\Msa' => __DIR__ . '/../..' . '/classes/ajax/json/Msa.php',
         'Controllers\\Ajax\\Json\\Mso' => __DIR__ . '/../..' . '/classes/ajax/json/Mso.php',
+        'Controllers\\Ajax\\Json\\Util' => __DIR__ . '/../..' . '/classes/ajax/json/Util.php',
         'Controllers\\Ajax\\Json\\Wm\\Receiving' => __DIR__ . '/../..' . '/classes/ajax/json/Wm/Receiving.php',
         'Controllers\\Ajax\\Lookup' => __DIR__ . '/../..' . '/classes/ajax/Lookup.php',
         'Controllers\\Ajax\\Lookup\\Lookup' => __DIR__ . '/../..' . '/classes/ajax/Lookup/Lookup.php',
