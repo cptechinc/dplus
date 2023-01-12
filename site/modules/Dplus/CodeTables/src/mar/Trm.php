c25bd4b3ed9bb5b499f38f9916e64acc8da0842f
Paul <pauldro952@outlook.com>
2023-01-12 14:08:55 -0600

    Update Trm.php

--- site/modules/Dplus/CodeTables/src/mar/Trm.php
+++ site/modules/Dplus/CodeTables/src/mar/Trm.php
@@ -17,9 +17,40 @@ class Trm extends AbstractCodeTableEditableSingleKey {
 	const RECORDLOCKER_FUNCTION = 'trm';
 	const DPLUS_TABLE           = 'TRM';
 	const FIELD_ATTRIBUTES = [
-		'code'        => ['type' => 'text', 'maxlength' => 4],
-		'description' => ['type' => 'text', 'maxlength' => 20],
+		'code'         => ['type' => 'text', 'maxlength' => 4],
+		'description'  => ['type' => 'text', 'maxlength' => 20],
+		'method'       => ['type' => 'text', 'default' => 'S', 'options' => ['S' => 'Standard Terms', 'E' => 'End of Menth Terms']],
+		'type'         => ['type' => 'text', 'default' => 'STD', 'optionsS' => ['STD' => 'Standard', 'CC' => 'Credit Card', 'CA' => 'Cash', 'COD' => 'COD', 'CDC' => 'COD Cash'], 'optionsE' => ['STD' => 'Standard', 'CA' => 'Cash', 'COD' => 'COD', 'CDC' => 'COD Cash' ]],
+		'hold'         => ['type' => 'text', 'default' => 'N', 'options' => ['Y' => 'Yes', 'N' => 'No']],
+		'expiredate'   => ['type' => 'text', 'default' => ''],
+		'country'      => ['type' => 'text', 'default' => ''],
+		'ccprefix'     => ['type' => 'text', 'default' => ''],
+		'freightallow' => ['type' => 'text', 'default' => 'N', 'options' => ['Y' => 'Yes', 'N' => 'No']],
+
+		// THESE ARE FOR THE SPLITS, USE BASE NAME
+		'eom_disc_percent' => ['type' => 'number', 'max' => 99.99, 'precision' => 2],
+		'eom_disc_day'    => ['type' => 'number', 'max' => 31],
+		'eom_disc_months' => ['type' => 'number', 'max' => 99],
+		'eom_due_day'     => ['type' => 'number', 'max' => 31, 'min' => 1],
+		'eom_plus_months'  => ['type' => 'number', 'max' => 99],
+		'eom_from_day'     => ['type' => 'number', 'max' => 98],
+		'eom_thru_day'     => ['type' => 'number', 'max' => 99],
+
+		'std_disc_date'    => ['type' => 'text', 'regex' => "(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])", 'dateformat' => 'm/d', 'placeholder' => 'mm/dd'],
+		'std_disc_percent' => ['type' => 'number', 'max' => 100],
+		'std_disc_days'    => ['type' => 'number', 'max' => 99],
+		'std_disc_day'     => ['type' => 'number', 'max' => 31],
+		'std_due_days'     => ['type' => 'number', 'max' => 990],
+		'std_due_day'      => ['type' => 'number', 'max' => 31],
+		'std_due_date'     => ['type' => 'text', 'regex' => "(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])", 'dateformat' => 'm/d', 'placeholder' => 'mm/dd'],
+		'std_plus_months'  => ['type' => 'number', 'max' => 99],
+		'std_plus_years'   => ['type' => 'number', 'max' => 9],
+		
 	];
+	const NBR_SPLITS_METHOD_S = 6;
+	const NBR_SPLITS_METHOD_E = 3;
+	const METHOD_STD = 'S';
+	const METHOD_EOM = 'E';
 
 	protected static $instance;
 
@@ -34,5 +65,34 @@ class Trm extends AbstractCodeTableEditableSingleKey {
 		$q = $this->query();
 		$q->select(ArTermsCode::aliasproperty('id'));
 		return $q->find()->toArray();
+	}
+
+/* =============================================================
+	CRUD Creates
+============================================================= */
+	/**
+	 * Return New Code
+	 * @param  string $id
+	 * @return Code
+	 */
+	public function new($id = '') {
+		$code = parent::new($id);
+		$code->method       = $this->fieldAttribute('method', 'default');
+		$code->type         = $this->fieldAttribute('type', 'default');
+		$code->hold         = $this->fieldAttribute('hold', 'default');
+		$code->expiredate   = $this->fieldAttribute('expiredate', 'default');
+		$code->country      = $this->fieldAttribute('country', 'default');
+		$code->ccprefix     = $this->fieldAttribute('ccprefix', 'default');
+		$code->freightallow = $this->fieldAttribute('freightallow', 'default');
+		$code->set_order_percent(1, 100.00);
+
+		$code->set_eom_from_day(1, 1);
+		$code->set_eom_thru_day(1, 99);
+
+		// DEBUG:
+		$code->method = self::METHOD_EOM;
+		$code->code = 'test';
+
+		return $code;
 	}
 }
