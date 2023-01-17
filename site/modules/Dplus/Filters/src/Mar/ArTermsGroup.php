8a92b998aaf564b5ce2a643f04612a3acbc2eb75
Paul <pauldro952@outlook.com>
2023-01-17 09:43:28 -0600

    FEAT: add ArTermsGroup filter

---
+++ site/modules/Dplus/Filters/src/Mar/ArTermsGroup.php
@@ -0,0 +1,12 @@
+<?php namespace Dplus\Filters\Mar;
+// Dplus Model
+use ArTermsGroupQuery, ArTermsGroup as Model;
+// Dplus Filters
+use Dplus\Filters\CodeFilter;
+
+/**
+ * Wrapper Class for ArTermsGroupQuery
+ */
+class ArTermsGroup extends CodeFilter {
+	const MODEL = 'ArTermsGroup';
+}
