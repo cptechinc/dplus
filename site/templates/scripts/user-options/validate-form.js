e498220ea659df281dafa7d37eebc54fd53290cf
Paul <pauldro952@outlook.com>
2023-02-14 13:47:03 -0600

    FEAT: add validations for date, whseid

--- site/templates/scripts/user-options/validate-form.js
+++ site/templates/scripts/user-options/validate-form.js
@@ -3,9 +3,25 @@ $(function() {
 	let alert    = UserOptionsAlerts.getInstance();
 	let server   = UserOptionsRequests.getInstance();
 
+	let momentJsFormats = {
+		'mm/dd/yyyy': 'MM/DD/YYYY',
+	}
+
 /* =============================================================
 	Form Validation
 ============================================================= */
+	function validateDate(date) {
+		if (date.length < 8) {
+			return true;
+		}
+		let expiredate = moment(date, momentJsFormats['mm/dd/yyyy']);
+		return expiredate.isValid();
+	}
+
+	jQuery.validator.addMethod("dateF", function(value, element) {
+		return this.optional(element) || validateDate(value);
+	}, "Date must be a valid date (MM/DD/YYYY)"); 
+
 	let validator = formUser.form.validate({
 		errorClass: "is-invalid",
 		validClass: "is-valid",
@@ -31,7 +47,36 @@ $(function() {
 			},
 		},
 		submitHandler: function(form) {
+			$('#loading-modal').modal('show');
 			form.submit();
 		}
+	});
+
+	$('input.whseid').each(function() {
+		let input = $(this);
+
+		input.rules("add", {
+			required: false,
+			remote: {
+				url: config.ajax.urls.json + 'min/validate/iwhm/code/',
+				type: "get",
+				data: {
+					jqv: 'true',
+					allowAll: 'true',
+					id: function() {
+						return input.val();
+					}
+				}
+			}
+		});
+	});
+
+	$('input.date').each(function() {
+		let input = $(this);
+
+		input.rules("add", {
+			required: false,
+			dateF: true,
+		});
 	});
 });
