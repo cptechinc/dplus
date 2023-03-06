12e913ee269a65dde4866ae03e9bfd22062a84ca
Paul <pauldro952@outlook.com>
2023-03-06 09:36:21 -0600

    FEAT: add validator class

---
+++ site/templates/scripts/classes/Validator.js
@@ -0,0 +1,29 @@
+class Validator {
+	static instance = null;
+
+	static getInstance() {
+		if (this.instance === null) {
+			this.instance = new Validator();
+		}
+		return this.instance;
+	}
+
+	dateMMYYSlash(date) {
+		return DateRegexes.getInstance().regexes['mm/dd'].test(date);
+	}
+
+	dateMMDDYYYYSlash(dateString) {
+		let date = new DateFormatter(dateString, 'mm/dd/yyyy');
+		return date.isValid();
+	}
+
+	dateIsInFuture(dateString, dateFormat = 'mm/dd/yyyy') {
+		let date = new DateFormatter(dateString, dateFormat);
+
+		if (date.isValid() === false) {
+			return false;
+		}
+		let today = new DateFormatter();
+		return parseInt(date.format('timestamp')) > parseInt(today.format('timestamp'));
+	}
+}
