e43450474334640c08a95db08cc5437e32182500
Paul <pauldro952@outlook.com>
2023-03-28 13:11:54 -0500

    FIX: hide breadcrumbs

--- site/templates/controllers/classes/mso/SalesOrder/SalesOrder.php
+++ site/templates/controllers/classes/mso/SalesOrder/SalesOrder.php
@@ -42,6 +42,7 @@ class SalesOrder extends Base {
 		if (empty($data->ordn) === false) {
 			return self::so($data);
 		}
+		self::pw('page')->show_breadcrumbs = false;
 		return self::lookupScreen($data);
 	}
 
