e785f8ee0a405f42319358880f264f410402d534
Paul <pauldro952@outlook.com>
2023-03-29 13:19:47 -0500

    Update Base.php

--- site/templates/controllers/classes/msa/Base.php
+++ site/templates/controllers/classes/msa/Base.php
@@ -1,31 +1,16 @@
 <?php namespace Controllers\Msa;
 // Mvc Controllers
-use Mvc\Controllers\Controller;
+use Controllers\AbstractController;
 
-abstract class Base extends Controller {
+abstract class Base extends AbstractController {
 	const DPLUSPERMISSION = 'msa';
 
 /* =============================================================
 	Displays
 ============================================================= */
-	protected static function displayUserNotPermitted() {
-		if (self::validateUserPermission()) {
-			return true;
-		}
-		$perm = static::DPLUSPERMISSION;
-		return self::pw('config')->twig->render('util/alert.twig', ['type' => 'danger', 'title' => "You don't have access to this function", 'iconclass' => 'fa fa-warning fa-2x', 'message' => "Permission: $perm"]);
-	}
-
+	
 /* =============================================================
 	Validator, Module Getters
 ============================================================= */
-	public static function validateUserPermission(User $user = null) {
-		if (empty(static::DPLUSPERMISSION)) {
-			return true;
-		}
-		if (empty($user)) {
-			$user = self::pw('user');
-		}
-		return $user->hasPermissionCode(static::DPLUSPERMISSION);
-	}
+	
 }
